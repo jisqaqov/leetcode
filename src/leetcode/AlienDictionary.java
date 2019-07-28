20bdf4d66502148701e85c820ca4f684c34414d9
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-07-28 10:46:53 +0300

    graph, topological sort: resolved problem 269. Alien Dictionary;

--- src/leetcode/AlienDictionary.java
+++ src/leetcode/AlienDictionary.java
@@ -6,9 +6,8 @@ import java.util.*;
  * @author Jandos Iskakov
  * problem: Alien Dictionary 269
  * algorithm: Graph, Topological Sort
- * time complexity: O(n + k)
- * space complexity: O(n + k)
- * notes: need to be submitted
+ * time complexity: O(V + E + |words|)
+ * space complexity: O(V + E + |words|)
  */
 public class AlienDictionary {
 
@@ -18,27 +17,44 @@ public class AlienDictionary {
     }
 
     private void test() {
-        String[] tc1dict = {"baa", "abcd", "abca", "cab", "cad"};
-        System.out.println(printOrder(tc1dict, 4));
+        String[] tc1dict = {"wrt", "wrf", "er", "ett", "rftt"};
+        System.out.println(alienOrder(tc1dict));
+
+        String[] tc2dict = {"z", "x", "z"};
+        System.out.println(alienOrder(tc2dict));
+
+        String[] tc3dict = {"z", "z"};
+        System.out.println(alienOrder(tc3dict));
     }
 
-    public String printOrder(String dict[], int k) {
+    public String alienOrder(String words[]) {
         Map<Character, Set<Character>> adjList = new HashMap<>();
 
-        for (int i = 1; i < dict.length; i++) {
-            int len = Math.min(dict[i - 1].length(), dict[i].length());
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                adjList.putIfAbsent(words[i].charAt(j), new HashSet<>());
+            }
+        }
+
+        for (int i = 1; i < words.length; i++) {
+            int len = Math.min(words[i - 1].length(), words[i].length());
 
             for (int t = 0; t < len; t++) {
-                char ch1 = dict[i - 1].charAt(t);
-                char ch2 = dict[i].charAt(t);
+                char ch1 = words[i - 1].charAt(t);
+                char ch2 = words[i].charAt(t);
+
+
 
                 if (ch1 != ch2) {
-                    adjList.putIfAbsent(ch1, new HashSet<>());
                     adjList.get(ch1).add(ch2);
 
                     break;
                 }
             }
+        }
+
+        if (isCyclic(adjList)) {
+            return "";
         }
 
         Set<Character> visited = new HashSet<>();
@@ -52,16 +68,6 @@ public class AlienDictionary {
             tsort(ch, visited, adjList, deque);
         }
 
-        int t = 0;
-
-        for (char ch = 'a'; ch <= 'z' && t < k; ch++, t++) {
-            if (visited.contains(ch)) {
-                continue;
-            }
-
-            deque.addLast(ch);
-        }
-
         StringBuilder sb = new StringBuilder();
 
         while (!deque.isEmpty()) {
@@ -71,7 +77,9 @@ public class AlienDictionary {
         return sb.toString();
     }
 
-    private void tsort(Character ch, Set<Character> visited, Map<Character, Set<Character>> adjList, Deque<Character> deque) {
+    private void tsort(Character ch, Set<Character> visited,
+                       Map<Character, Set<Character>> adjList,
+                       Deque<Character> deque) {
         visited.add(ch);
 
         if (adjList.containsKey(ch)) {
@@ -85,6 +93,40 @@ public class AlienDictionary {
         }
 
         deque.addFirst(ch);
+    }
+
+    private boolean isCyclic(Map<Character, Set<Character>> adjList)  {
+        Set<Character> visited = new HashSet<>();
+        Set<Character> explored = new HashSet<>();
+
+        for (Character node : adjList.keySet()) {
+            if (!visited.contains(node) && dfs(node, adjList, visited, explored)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(char s, Map<Character, Set<Character>> adjList,
+                        Set<Character> visited, Set<Character> explored) {
+        visited.add(s);
+
+        if (adjList.containsKey(s)) {
+            for (Character adj : adjList.get(s)) {
+                if (!explored.contains(adj) && visited.contains(adj)) {
+                    return true;
+                }
+
+                if (!visited.contains(adj) && dfs(adj, adjList, visited, explored)) {
+                    return true;
+                }
+            }
+        }
+
+        explored.add(s);
+
+        return false;
     }
 
 }
