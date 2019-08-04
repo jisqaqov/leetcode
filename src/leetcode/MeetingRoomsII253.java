3b106065f03f2964a2d4138da97c3aa5a98b1355
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-08-04 11:50:03 +0300

    heap: solved medium problem 253. Meeting Rooms II;

--- src/leetcode/MeetingRoomsII253.java
+++ src/leetcode/MeetingRoomsII253.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @author Jandos Iskakov
@@ -20,10 +20,14 @@ public class MeetingRoomsII253 {
         int[][] tc1intervals = {{0, 30}, {5, 10}, {15, 20}};
         int[][] tc2intervals = {{7, 10}, {2, 4}};
         int[][] tc3intervals = {{13, 15}, {1, 13}};
-
-        System.out.println(minMeetingRooms(tc1intervals));
-        System.out.println(minMeetingRooms(tc2intervals));
-        System.out.println(minMeetingRooms(tc3intervals));
+        int[][] tc4intervals = {{1293,2986},{848,3846},{4284,5907},{4466,4781},{518,2918},{300,5870}};
+        int[][] tc5intervals = {{1,5},{8,9},{8,9}};
+//
+        System.out.println(minMeetingRoomsWithHeap(tc1intervals));
+        System.out.println(minMeetingRoomsWithHeap(tc2intervals));
+        System.out.println(minMeetingRoomsWithHeap(tc3intervals));
+        System.out.println(minMeetingRoomsWithHeap(tc4intervals));
+        System.out.println(minMeetingRoomsWithHeap(tc5intervals));
     }
 
     public int minMeetingRooms(int[][] intervals) {
@@ -39,8 +43,8 @@ public class MeetingRoomsII253 {
         int meetings = 0;
         int max = 0;
 
-        for (int i = 0; i < times.length; i++) {
-            if (times[i].type == 2) {
+        for (Time time : times) {
+            if (time.type == 2) {
                 meetings++;
             } else {
                 meetings--;
@@ -62,5 +66,36 @@ public class MeetingRoomsII253 {
         }
     }
 
+    public int minMeetingRoomsWithHeap(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(t -> t[0]));
+
+        PriorityQueue<Integer> endTimes = new PriorityQueue<>();
+
+        for (int[] time : intervals) {
+            endTimes.add(time[1]);
+        }
+
+        int min = 0, meetings = 0;
+
+        for (int[] time : intervals) {
+            int startTime = time[0];
+            int endTime = endTimes.peek();
+
+            if (startTime < endTime) {
+                meetings++;
+            } else {
+                while (endTimes.peek() <= startTime) {
+                    meetings--;
+                    endTimes.poll();
+                }
+
+                meetings++;
+            }
+
+            min = Math.max(min, meetings);
+        }
+
+        return min;
+    }
 
 }
