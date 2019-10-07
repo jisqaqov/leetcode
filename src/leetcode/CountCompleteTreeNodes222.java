ad3f804899a487eeb973ac2ebb99ff10bd52235c
Zhandos Iskakov <zhandos.iskakov@iii.com>
2019-10-07 11:19:25 +0300

    tree: solved 222. Count Complete Tree Nodes;

---
+++ src/leetcode/CountCompleteTreeNodes222.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+/**
+ * @author Jandos Iskakov
+ * problem: 222. Count Complete Tree Nodes
+ * algorithm: Tree
+ * time complexity: O(n)
+ * space complexity: O(log(n))
+ */
+public class CountCompleteTreeNodes222 {
+
+  public int countNodes(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int l = countNodes(root.left);
+    int r = countNodes(root.right);
+
+    return l + r + 1;
+  }
+
+  /**
+   * Definition for a binary tree node.
+   */
+  public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+}
