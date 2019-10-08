00fdca262c4ff71228b0b44780c864836c8d91bb
Zhandos Iskakov <zhandos.iskakov@iii.com>
2019-10-08 17:23:22 +0300

    hash table: resolved 438. Find All Anagrams in a String;

---
+++ src/leetcode/FindAllAnagramsInAString438.java
@@ -0,0 +1,100 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Jandos Iskakov
+ * problem: 121. Best Time to Buy and Sell Stock
+ * time complexity: O(n)
+ * space complexity: O(n)
+ * algorithm: Hash Table
+ */
+@SuppressWarnings("DuplicatedCode")
+public class FindAllAnagramsInAString438 {
+
+  public static void main(String[] args) {
+    FindAllAnagramsInAString438 solution = new FindAllAnagramsInAString438();
+    solution.test();
+  }
+
+  public void test() {
+    System.out.println(findAnagrams("cbaebabacd", "abc"));
+    System.out.println(findAnagrams("abab", "ab"));
+  }
+
+  public List<Integer> findAnagrams(String s, String p) {
+    List<Integer> indexes = new ArrayList<>();
+
+    if (s == null || p == null || s.length() < p.length()) {
+      return indexes;
+    }
+
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < p.length(); i++) {
+      char ch = p.charAt(i);
+      map.put(ch, map.getOrDefault(ch, 0) + 1);
+    }
+
+    Set<Character> pattern = new HashSet<>(map.keySet());
+    Set<Character> balance = new HashSet<>(map.keySet());
+
+    for (int i = 0; i < p.length(); i++) {
+      char ch = s.charAt(i);
+
+      if (pattern.contains(ch)) {
+        map.put(ch, map.get(ch) - 1);
+        track(map, balance, ch);
+      } else {
+        map.put(ch, map.getOrDefault(ch, 0) + 1);
+        balance.add(ch);
+      }
+    }
+
+    if (balance.isEmpty()) {
+      indexes.add(0);
+    }
+
+    for (int i = p.length(); i < s.length(); i++) {
+      char charLast = s.charAt(i);
+      char charFirst = s.charAt(i - p.length());
+
+      if (charFirst != charLast) {
+
+        if (pattern.contains(charLast)) {
+          map.put(charLast, map.get(charLast) - 1);
+        } else {
+          map.put(charLast, map.getOrDefault(charLast, 0) + 1);
+        }
+
+        if (pattern.contains(charFirst)) {
+          map.put(charFirst, map.get(charFirst) + 1);
+        } else {
+          map.put(charFirst, map.getOrDefault(charFirst, 0) - 1);
+        }
+
+        track(map, balance, charLast);
+        track(map, balance, charFirst);
+      }
+
+      if (balance.isEmpty()) {
+        indexes.add(i - p.length() + 1);
+      }
+    }
+
+    return indexes;
+  }
+
+  private void track(Map<Character, Integer> map, Set<Character> balance, char ch) {
+    if (map.get(ch) == 0) {
+      balance.remove(ch);
+    } else {
+      balance.add(ch);
+    }
+  }
+
+}
