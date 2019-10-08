176f56a6447936dbf8241d3fe3b1410b53fba59b
Zhandos Iskakov <zhandos.iskakov@iii.com>
2019-10-08 20:25:08 +0300

    solution from discussions to be analyzed;

--- src/leetcode/FindAllAnagramsInAString438.java
+++ src/leetcode/FindAllAnagramsInAString438.java
@@ -23,8 +23,13 @@ public class FindAllAnagramsInAString438 {
   }
 
   public void test() {
-    System.out.println(findAnagrams("cbaebabacd", "abc"));
+    System.out.println(findAnagrams("xcbaebabacd", "abc"));
     System.out.println(findAnagrams("abab", "ab"));
+
+    DiscussionsApproach problem = new DiscussionsApproach();
+
+    System.out.println(problem.findAnagrams("aaaaaaaaaaxcbaebabacd", "abc"));
+    System.out.println(problem.findAnagrams("abab", "ab"));
   }
 
   public List<Integer> findAnagrams(String s, String p) {
@@ -87,6 +92,86 @@ public class FindAllAnagramsInAString438 {
     }
 
     return indexes;
+  }
+
+  private static class DiscussionsApproach {
+    public List<Integer> findAnagrams(String s, String p) {
+      List<Integer> indexes = new ArrayList<>();
+
+      if (s == null || p == null || s.length() < p.length()) {
+        return indexes;
+      }
+
+      Map<Character, Integer> map = new HashMap<>();
+      for (int i = 0; i < p.length(); i++) {
+        char ch = p.charAt(i);
+        map.put(ch, map.getOrDefault(ch, 0) + 1);
+      }
+
+      int start = 0, counter = p.length();
+
+      for (int i = 0; i < s.length(); i++) {
+        char ch = s.charAt(i);
+
+        if (map.containsKey(ch)) {
+          if (map.get(ch) >= 0) {
+            map.put(ch, map.get(ch) - 1);
+            counter--;
+          }
+        }
+
+        if (i - start == p.length()) {
+          if (map.containsKey(s.charAt(start))) {
+            map.put(s.charAt(start), map.get(s.charAt(start)) + 1);
+
+            if (map.get(s.charAt(start)) > 0) {
+              counter++;
+            }
+          }
+
+          start++;
+        }
+
+        if (counter == 0) {
+          indexes.add(start);
+        }
+      }
+
+      return indexes;
+    }
+
+
+    /*public List<Integer> findAnagrams(String s, String p) {
+      List<Integer> list = new ArrayList<>();
+      if (s == null || s.length() == 0 || p == null || p.length() == 0)
+        return list;
+      int[] hash = new int[256]; //character hash
+      //record each character in p to hash
+      for (char c : p.toCharArray()) {
+        hash[c]++;
+      }
+      //two points, initialize count to p's length
+      int left = 0, right = 0, count = p.length();
+      while (right < s.length()) {
+        //move right everytime, if the character exists in p's hash, decrease the count
+        //current hash value >= 1 means the character is existing in p
+        if (hash[s.charAt(right++)]-- >= 1)
+          count--;
+
+        //when the count is down to 0, means we found the right anagram
+        //then add window's left to result list
+        if (count == 0)
+          list.add(left);
+
+        //if we find the window's size equals to p, then we have to move left (narrow the window) to find the new match window
+        //++ to reset the hash because we kicked out the left
+        //only increase the count if the character is in p
+        //the count >= 0 indicate it was original in the hash, cuz it won't go below 0
+        if (right - left == p.length() && hash[s.charAt(left++)]++ >= 0)
+          count++;
+      }
+      return list;
+    }*/
   }
 
   private void track(Map<Character, Integer> map, Set<Character> balance, char ch) {
