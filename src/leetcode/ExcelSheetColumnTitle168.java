3c88b75508a75f07a91dce4096dc277171ae9a3a
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-10-12 12:36:00 +0300

    math: resolved 168. Excel Sheet Column Title;

---
+++ src/leetcode/ExcelSheetColumnTitle168.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/**
+ * @author Jandos Iskakov
+ * problem: 168. Excel Sheet Column Title
+ * algorithm: Math, String time
+ * complexity: O(n)
+ * space complexity: O(1)
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Excel Sheet Column Title.
+ * Memory Usage: 33.7 MB, less than 100.00% of Java online submissions for Excel Sheet Column Title.
+ */
+public class ExcelSheetColumnTitle168 {
+
+  public static void main(String[] args) {
+    ExcelSheetColumnTitle168 problem = new ExcelSheetColumnTitle168();
+    problem.test();
+  }
+
+  private void test() {
+    System.out.println(convertToTitle(26));
+    System.out.println(convertToTitle(731));
+    System.out.println(convertToTitle(52));
+  }
+
+  public String convertToTitle(int n) {
+    StringBuilder sb = new StringBuilder();
+
+    while (n > 0) {
+      int number = n % 26;
+      if (number == 0) {
+        number = 26;
+      }
+
+      n /= 26;
+      if (number == 26) {
+        n--;
+      }
+
+      char ch = (char) (number + 'A' - 1);
+
+      sb.append(ch);
+    }
+
+    return sb.reverse().toString();
+  }
+
+}
