cd1fe7020219f0674cb8235f3168b2bf9fbfbedc
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-10-12 19:31:38 +0300

    two pointer: resolved 19. Remove Nth Node From End of List;

--- src/leetcode/RemoveNthNodeFromEndOfList19.java
+++ src/leetcode/RemoveNthNodeFromEndOfList19.java
@@ -3,7 +3,7 @@ package leetcode;
 /**
  * @author Jandos Iskakov
  * problem: 19. Remove Nth Node From End of List
- * algorithm: Linked List
+ * algorithm: Linked List, Two Pointer
  * time complexity: O(n)
  * space complexity: O(1)
  */
@@ -12,6 +12,8 @@ public class RemoveNthNodeFromEndOfList19 {
   public static void main(String[] args) {
     RemoveNthNodeFromEndOfList19 problem = new RemoveNthNodeFromEndOfList19();
     problem.test();
+    problem.test2();
+    problem.test3();
   }
 
   private void test() {
@@ -29,32 +31,75 @@ public class RemoveNthNodeFromEndOfList19 {
     System.out.println(removeNthFromEnd(head, 2));
   }
 
-  public ListNode removeNthFromEnd(ListNode head, int k) {
-    int n = 0;
-    ListNode node = head;
+  private void test2() {
+    ListNode head = new ListNode(1);
 
-    while (node != null) {
-      n++;
-      node = node.next;
+    head.next = new ListNode(2);
+
+    System.out.println(removeNthFromEnd(head, 1));
+  }
+
+  private void test3() {
+    ListNode head = new ListNode(1);
+
+    System.out.println(removeNthFromEnd(head, 1));
+  }
+
+  public ListNode removeNthFromEnd(ListNode head, int k) {
+    ListNode q = head;
+
+    while (q.next != null && k > 0) {
+      q = q.next;
+      k--;
     }
 
-    if (k > n) {
-      return head;
-    } else if (k == n) {
+    if (k == 1) {
       return head.next;
     }
 
-    int i = 0, p = n - k - 1;
+    ListNode p = head;
 
-    node = head;
-    while (i < p) {
-      node = node.next;
-      i++;
+    while (q.next != null) {
+      p = p.next;
+      q = q.next;
     }
 
-    node.next = node.next.next;
+    if (p.next != null) {
+      p.next = p.next.next;
+    }
 
     return head;
+  }
+
+  private static class NaiveSolution {
+
+    public ListNode removeNthFromEnd(ListNode head, int k) {
+      int n = 0;
+      ListNode node = head;
+
+      while (node != null) {
+        n++;
+        node = node.next;
+      }
+
+      if (k > n) {
+        return head;
+      } else if (k == n) {
+        return head.next;
+      }
+
+      int i = 0, p = n - k - 1;
+
+      node = head;
+      while (i < p) {
+        node = node.next;
+        i++;
+      }
+
+      node.next = node.next.next;
+
+      return head;
+    }
   }
 
   /**
