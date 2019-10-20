9277113804a3028d7e2b04b434a762475174f91a
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-10-20 19:08:05 +0300

    dfs: resolved 721. Accounts Merge;

--- src/leetcode/AccountsMerge721.java
+++ src/leetcode/AccountsMerge721.java
@@ -38,42 +38,34 @@ public class AccountsMerge721 {
   }
 
   public List<List<String>> accountsMerge(List<List<String>> accounts) {
-    Map<String, Map<String, Set<Account>>> map = new HashMap<>();
+    Map<String, List<Account>> map = new HashMap<>();
 
     for (int i = 0; i < accounts.size(); i++) {
-      List<String> account = accounts.get(i);
+      List<String> emails = accounts.get(i);
 
-      Account acc = new Account();
-      acc.name = account.get(0);
-      acc.index = i;
-      acc.emails = new HashSet<>(account.subList(1, account.size()));
+      Account account = new Account();
+      account.name = emails.get(0);
+      account.index = i;
+      account.emails = new HashSet<>(emails.subList(1, emails.size()));
 
-      map.putIfAbsent(acc.name, new HashMap<>());
-
-      for (String email : acc.emails) {
-        map.get(acc.name).putIfAbsent(email, new HashSet<>());
-        map.get(acc.name).get(email).add(acc);
+      for (String email : account.emails) {
+        map.putIfAbsent(email, new ArrayList<>());
+        map.get(email).add(account);
       }
     }
 
     List<Account> sol = new ArrayList<>();
-    Set<Account> used = new HashSet<>();
+    Set<Integer> used = new HashSet<>();
 
-    for (String name : map.keySet()) {
-      Set<Account> accountsByName = map.get(name)
-        .values()
-        .stream()
-        .flatMap(Collection::stream)
-        .collect(Collectors.toSet());
-
-      for (Account account : accountsByName) {
-        if (used.contains(account)) {
+    for (String email : map.keySet()) {
+      for (Account account : map.get(email)) {
+        if (used.contains(account.index)) {
           continue;
         }
 
         sol.add(account);
 
-        dfs(account, account, map.get(name), used);
+        dfs(account, account, map, used);
       }
     }
 
@@ -89,13 +81,13 @@ public class AccountsMerge721 {
       }).collect(Collectors.toList());
   }
 
-  private void dfs(Account merge, Account adj, Map<String, Set<Account>> map,
-    Set<Account> used) {
-    if (used.contains(adj)) {
+  private void dfs(Account merge, Account adj, Map<String, List<Account>> map,
+    Set<Integer> used) {
+    if (used.contains(adj.index)) {
       return;
     }
 
-    used.add(adj);
+    used.add(adj.index);
 
     Set<Account> adjList = new HashSet<>();
 
@@ -105,13 +97,10 @@ public class AccountsMerge721 {
 
     adjList.remove(merge);
 
-    merge.emails.addAll(adjList.stream()
-      .map(account -> account.emails)
-      .flatMap(Collection::stream)
-      .collect(Collectors.toList()));
-
     for (Account account : adjList) {
-      if (!used.contains(account)) {
+      merge.emails.addAll(account.emails);
+
+      if (!used.contains(account.index)) {
         dfs(merge, account, map, used);
       }
     }
