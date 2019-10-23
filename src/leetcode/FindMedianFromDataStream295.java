906aa3d6808b1ab2d0438e1a633186478d815040
Zhandos Iskakov <zhandos.iskakov@iii.com>
2019-10-23 15:05:51 +0300

    heap: 295. Find Median from Data Stream;

---
+++ src/leetcode/FindMedianFromDataStream295.java
@@ -0,0 +1,84 @@
+package leetcode;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author Jandos Iskakov
+ * problem: 295. Find Median from Data Stream
+ * algorithm: Heap, Design
+ * time complexity: O(log(n))
+ * space complexity: O(n)
+ * Runtime: 117 ms, faster than 47.58% of Java online submissions for Find Median from Data Stream.
+ * Memory Usage: 66.7 MB, less than 54.24% of Java online submissions for Find Median from Data Stream.
+ */
+public class FindMedianFromDataStream295 {
+  public static void main(String[] args) {
+    FindMedianFromDataStream295 problem = new FindMedianFromDataStream295();
+    problem.test();
+  }
+
+  private void test() {
+    MedianFinder finder = new MedianFinder();
+    finder.addNum(1);
+    finder.addNum(2);
+    System.out.println(finder.findMedian());
+    finder.addNum(3);
+    System.out.println(finder.findMedian());
+    finder.addNum(1);
+    System.out.println(finder.findMedian());
+  }
+
+  class MedianFinder {
+    private int n = 0;
+    private double median = 0;
+    private PriorityQueue<Integer> left;
+    private PriorityQueue<Integer> right;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+      left = new PriorityQueue<>((a1, a2) -> a2 - a1);
+      right = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+      if (n == 0) {
+        median = num;
+      } else if (n == 1) {
+        int m = (int) median;
+        left.add(Math.min(num, m));
+        right.add(Math.max(num, m));
+        median = (left.peek() + right.peek()) / 2.0;
+      } else {
+        if (n % 2 == 0) {
+          if (num == median) {
+            median = num;
+          } else if (num > median) {
+            right.add(num);
+            median = right.poll();
+          } else if (num < median) {
+            left.add(num);
+            median = left.poll();
+          }
+        } else {
+          int m = (int) median;
+          if (num > median) {
+            right.add(num);
+            left.add(m);
+            median = (left.peek() + right.peek()) / 2.0;
+          } else if (num <= median) {
+            left.add(num);
+            right.add(m);
+            median = (left.peek() + right.peek()) / 2.0;
+          }
+        }
+      }
+
+      n++;
+    }
+
+    public double findMedian() {
+      return median;
+    }
+  }
+
+}
