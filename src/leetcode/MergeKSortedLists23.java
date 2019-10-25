df2b69eac77b415f8c55374cc9e0a6ded32c29a7
Zhandos Iskakov <zhandos.iskakov@iii.com>
2019-10-25 16:19:22 +0300

    refactoring;

--- src/leetcode/MergeKSortedLists23.java
+++ src/leetcode/MergeKSortedLists23.java
@@ -7,47 +7,58 @@ import java.util.PriorityQueue;
  * @author Jandos Iskakov
  * problem: 23. Merge k Sorted Lists
  * algorithm: Heap, Devide And Conquer, Linked List
- * time complexity: O(n*k*log(k))
+ * time complexity: O(n*log(k))
+ * space complexity: O(n)
+ * Runtime: 38 ms, faster than 22.60% of Java online submissions for Merge k Sorted Lists.
+ * Memory Usage: 41 MB, less than 44.81% of Java online submissions for Merge k Sorted Lists.
  */
 public class MergeKSortedLists23 {
 
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
+  public ListNode mergeKLists(ListNode[] lists) {
+    if (lists.length == 0) {
+      return null;
     }
 
-    public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0)
-            return null;
+    ListNode head = null, prev = null;
 
-        ListNode head = null, prev = null;
-
-        PriorityQueue<ListNode> heap = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
-        for (ListNode list : lists)
-            if (list != null)
-                heap.add(list);
-
-        while (!heap.isEmpty()) {
-            ListNode node = heap.peek();
-            ListNode newNode = new ListNode(node.val);
-
-            if (head == null) {
-                head = newNode;
-            } else {
-                prev.next = newNode;
-            }
-
-            prev = newNode;
-
-            heap.poll();
-
-            ListNode next = node.next;
-            if (next != null)
-                heap.add(next);
-        }
-
-        return head;
+    PriorityQueue<ListNode> heap = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+    for (ListNode list : lists) {
+      if (list != null) {
+        heap.add(list);
+      }
     }
+
+    while (!heap.isEmpty()) {
+      ListNode node = heap.poll();
+      ListNode newNode = new ListNode(node.val);
+
+      if (head == null) {
+        head = newNode;
+      } else {
+        prev.next = newNode;
+      }
+
+      prev = newNode;
+
+      if (node.next != null) {
+        heap.add(node.next);
+      }
+    }
+
+    return head;
+  }
+
+  /**
+  * Definition for singly-linked list.
+  */
+  public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+    }
+  }
 
 }
