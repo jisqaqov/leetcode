5e841db930cdfbbf9b143c8025ab20536e5f8c01
Zhandos Iskakov <zhandos.iskakov@iii.com>
2019-10-25 13:51:55 +0300

    refactoring;

--- src/leetcode/NumberOfIslands200.java
+++ src/leetcode/NumberOfIslands200.java
@@ -6,6 +6,8 @@ package leetcode;
  * algorithm: DFS
  * time complexity: O(N*M)
  * space complexity: O(N*M)
+ * Runtime: 1 ms, faster than 100.00% of Java online submissions for Number of Islands.
+ * Memory Usage: 41.3 MB, less than 58.14% of Java online submissions for Number of Islands.
  */
 public class NumberOfIslands200 {
 
@@ -16,9 +18,9 @@ public class NumberOfIslands200 {
 
   public void test() {
     char[][] tc1grid = {{'1', '1', '1', '1', '0'},
-        {'1', '1', '0', '1', '0'},
-        {'1', '1', '0', '0', '0'},
-        {'0', '0', '0', '1', '0'}};
+      {'1', '1', '0', '1', '0'},
+      {'1', '1', '0', '0', '0'},
+      {'0', '0', '0', '1', '0'}};
 
     System.out.println(numIslands(tc1grid));
   }
@@ -49,7 +51,8 @@ public class NumberOfIslands200 {
   }
 
   private void dfs(boolean[][] visited, char[][] grid, int i, int j) {
-    if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0' || visited[i][j]) {
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0'
+      || visited[i][j]) {
       return;
     }
 
