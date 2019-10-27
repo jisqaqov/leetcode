de046edc8adf20181e24b2054d7197ea4d2067f4
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-10-27 14:31:28 +0300

    recursion: resolved 140. Word Break II;

--- src/leetcode/WordBreakII140.java
+++ src/leetcode/WordBreakII140.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -31,6 +32,9 @@ public class WordBreakII140 {
 
     System.out.println(wordBreak("abcd", new ArrayList<>(Arrays.asList("a", "abc", "b", "cd"))));
 
+    AmazonProblem amazonProblem = new AmazonProblem();
+    System.out.println(amazonProblem.wordBreak("abcd", new ArrayList<>(Arrays.asList("a", "abc", "b", "cd"))));
+    
     BacktrackingApproach backtrackingApproach = new BacktrackingApproach();
     System.out.println(backtrackingApproach.wordBreak("abcd", new ArrayList<>(Arrays.asList("a", "abc", "b", "cd"))));
   }
@@ -40,38 +44,26 @@ public class WordBreakII140 {
 
     List<String> solution = new ArrayList<>();
 
-    Map<Integer, List<List<String>>> memo = new HashMap<>();
+    Map<Integer, List<String>> memo = new HashMap<>();
 
     wordBreak(s, 0, set, memo);
 
     if (memo.get(0) != null) {
-      for (List<String> list : memo.get(0)) {
-        StringBuilder sb = new StringBuilder();
-        for (String word : list) {
-          if (sb.length() > 0) {
-            sb.append(" ");
-          }
-
-          sb.append(word);
-        }
-        solution.add(sb.toString());
-      }
+        solution = memo.get(0);
     }
 
     return solution;
   }
 
-  private List<List<String>> wordBreak(String word, int index, Set<String> set,
-    Map<Integer, List<List<String>>> memo) {
+  private List<String> wordBreak(String word, int index, Set<String> set,
+    Map<Integer, List<String>> memo) {
 
     if (memo.containsKey(index)) {
       return memo.get(index);
     }
 
     if (index == word.length()) {
-      List<List<String>> temp = new ArrayList<>();
-      temp.add(new ArrayList<>());
-      return temp;
+      return new ArrayList<>(Collections.singletonList(""));
     }
 
     for (int i = index; i < word.length(); i++) {
@@ -81,16 +73,13 @@ public class WordBreakII140 {
         continue;
       }
 
-      List<List<String>> sub = wordBreak(word, i + 1, set, memo);
+      List<String> sub = wordBreak(word, i + 1, set, memo);
       if (sub != null) {
-        for (List<String> temp : sub) {
-          List<String> solution = new ArrayList<>();
-
-          solution.add(prefix);
-          solution.addAll(temp);
+        for (String temp : sub) {
+          String s = prefix + " " + temp;
 
           memo.putIfAbsent(index, new ArrayList<>());
-          memo.get(index).add(solution);
+          memo.get(index).add(s.trim());
         }
       } else {
         if (!memo.containsKey(index)) {
@@ -100,6 +89,75 @@ public class WordBreakII140 {
     }
 
     return memo.get(index);
+  }
+
+  private static class AmazonProblem {
+    public List<String> wordBreak(String s, List<String> words) {
+      Set<String> set = new HashSet<>(words);
+
+      List<String> solution = new ArrayList<>();
+
+      Map<Integer, List<List<String>>> memo = new HashMap<>();
+
+      wordBreak(s, 0, set, memo);
+
+      if (memo.get(0) != null) {
+        for (List<String> list : memo.get(0)) {
+          StringBuilder sb = new StringBuilder();
+          for (String word : list) {
+            if (sb.length() > 0) {
+              sb.append(" ");
+            }
+
+            sb.append(word);
+          }
+          solution.add(sb.toString());
+        }
+      }
+
+      return solution;
+    }
+
+    private List<List<String>> wordBreak(String word, int index, Set<String> set,
+      Map<Integer, List<List<String>>> memo) {
+
+      if (memo.containsKey(index)) {
+        return memo.get(index);
+      }
+
+      if (index == word.length()) {
+        List<List<String>> temp = new ArrayList<>();
+        temp.add(new ArrayList<>());
+        return temp;
+      }
+
+      for (int i = index; i < word.length(); i++) {
+        String prefix = word.substring(index, i + 1);
+
+        if (!set.contains(prefix)) {
+          continue;
+        }
+
+        List<List<String>> sub = wordBreak(word, i + 1, set, memo);
+        if (sub != null) {
+          for (List<String> temp : sub) {
+            List<String> solution = new ArrayList<>();
+
+            solution.add(prefix);
+            solution.addAll(temp);
+
+            memo.putIfAbsent(index, new ArrayList<>());
+            memo.get(index).add(solution);
+          }
+        } else {
+          if (!memo.containsKey(index)) {
+            memo.put(index, null);
+          }
+        }
+      }
+
+      return memo.get(index);
+    }
   }
 
   private static class BacktrackingApproach {
