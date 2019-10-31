0961c2c8c9f30aa743ac43c68de9f5153830a914
Zhandos Iskakov <zhandos.iskakov@iii.com>
2019-10-31 12:27:51 +0300

    string: 273. Integer to English Words;

---
+++ src/leetcode/IntegerToEnglishWords273.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+/**
+ * @author Jandos Iskakov
+ * problem: 273. Integer to English Words
+ * algorithm: Math, String
+ * time complexity: O(1)
+ * space complexity: O(1)
+ * Runtime: 1 ms, faster than 100.00% of Java online submissions for Integer to English Words.
+ * Memory Usage: 35.7 MB, less than 100.00% of Java online submissions for Integer to English Words.
+ */
+public class IntegerToEnglishWords273 {
+
+  private static final String[] NUMS = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven",
+    "Eight", "Nine", "Ten",
+    "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
+    "Nineteen"};
+
+  private static final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty",
+    "Seventy", "Eighty", "Ninety"};
+
+  private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+
+  public static void main(String[] args) {
+    IntegerToEnglishWords273 problem = new IntegerToEnglishWords273();
+    problem.test();
+  }
+
+  private void test() {
+    System.out.println(numberToWords(50868));
+  }
+
+  public String numberToWords(int num) {
+    if (num == 0) {
+      return "Zero";
+    }
+
+    String s = "";
+    int i = 0;
+
+    while (num > 0) {
+      if (num % 1000 != 0) {
+        s = convert(num % 1000).trim() + " " + THOUSANDS[i] + " " + s;
+      }
+
+      i++;
+      num /= 1000;
+    }
+
+    return s.trim();
+  }
+
+  private String convert(int n) {
+    if (n == 0) {
+      return "";
+    } else if (n < 20) {
+      return NUMS[n];
+    } else if (n < 100) {
+      return TENS[n / 10] + " " + convert(n % 10);
+    } else {
+      return NUMS[n / 100] + " Hundred " + convert(n % 100);
+    }
+  }
+
+
+}
