befc3138556b8c7cce68e30f899d40a22470901e
Zhandos Iskakov <zhandos.iskakov@iii.com>
2019-11-01 17:05:26 +0300

    Hash Table: 380. Insert Delete GetRandom O(1);

--- src/leetcode/InsertDeleteGetRandom380.java
+++ src/leetcode/InsertDeleteGetRandom380.java
@@ -13,7 +13,7 @@ import java.util.Random;
  * time complexity: O(1)
  * space complexity: O(N)
  * Runtime: 53 ms, faster than 81.50% of Java online submissions for Insert Delete GetRandom O(1).
- * Memory Usage: 45.1 MB, less than 84.00% of Java online submissions for Insert Delete GetRandom O(1).
+ * Memory Usage: 44.9 MB, less than 84.00% of Java online submissions for Insert Delete GetRandom O(1).
  */
 public class InsertDeleteGetRandom380 {
 
@@ -41,19 +41,23 @@ public class InsertDeleteGetRandom380 {
   }
 
   class RandomizedSet {
-    private Random random = new Random();
-    private static final double THRESHOLD = 0.7;
 
     Map<Integer, Integer> valueMap;
     List<Integer> indexes;
+    private Random random = new Random();
 
-    /** Initialize your data structure here. */
+    /**
+     * Initialize your data structure here.
+     */
     public RandomizedSet() {
       valueMap = new HashMap<>();
       indexes = new ArrayList<>();
     }
 
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    /**
+     * Inserts a value to the set. Returns true if the set did not already contain the specified
+     * element.
+     */
     public boolean insert(int val) {
       if (valueMap.containsKey(val)) {
         return false;
@@ -65,54 +69,33 @@ public class InsertDeleteGetRandom380 {
       return true;
     }
 
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    /**
+     * Removes a value from the set. Returns true if the set contained the specified element.
+     */
     public boolean remove(int val) {
       if (!valueMap.containsKey(val)) {
         return false;
       }
 
       int valIndex = valueMap.get(val);
+      if (valIndex < indexes.size() - 1) {
+        int valueLast = indexes.get(indexes.size() - 1);
+
+        indexes.set(valIndex, valueLast);
+        valueMap.put(valueLast, valIndex);
+      }
 
       valueMap.remove(val);
-      indexes.set(valIndex, null);
-
-      reIndex();
+      indexes.remove(indexes.size() - 1);
 
       return true;
     }
 
-    private void reIndex() {
-      if (valueMap.size() == 0) {
-        indexes.clear();
-
-        return;
-      }
-
-      int mod = (indexes.size() - valueMap.size() - 1) / valueMap.size();
-      if (mod < THRESHOLD) {
-        return;
-      }
-
-      indexes.clear();
-
-      for (int val : valueMap.keySet()) {
-        valueMap.put(val, indexes.size());
-        indexes.add(val);
-      }
-    }
-
-    /** Get a random element from the set. */
+    /**
+     * Get a random element from the set.
+     */
     public int getRandom() {
-      if (valueMap.size() == 0) {
-        return 0;
-      }
-
-      int randomIndex = -1;
-      do {
-        randomIndex = random.nextInt(indexes.size());
-      } while (indexes.get(randomIndex) == null);
-
-      return indexes.get(randomIndex);
+      return indexes.get(random.nextInt(indexes.size()));
     }
   }
 
