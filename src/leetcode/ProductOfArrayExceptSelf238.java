09fcfe42c5d649266062248aacf62b5c494f16a5
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-03 16:16:25 +0300

    array: solved 238. Product of Array Except Self;

--- src/leetcode/ProductOfArrayExceptSelf238.java
+++ src/leetcode/ProductOfArrayExceptSelf238.java
@@ -21,9 +21,11 @@ public class ProductOfArrayExceptSelf238 {
   private void test() {
     int[] tc1a = {1, 2, 3, 4};
     TestUtils.printArray(productExceptSelf(tc1a));
+    TestUtils.printArray(productExceptSelfV2(tc1a));
 
     int[] tc2a = {2, 3};
     TestUtils.printArray(productExceptSelf(tc2a));
+    TestUtils.printArray(productExceptSelfV2(tc2a));
   }
 
   public int[] productExceptSelf(int[] nums) {
@@ -42,6 +44,34 @@ public class ProductOfArrayExceptSelf238 {
       output[i] = left * right;
 
       left = left * nums[i];
+    }
+
+    return output;
+  }
+
+  public int[] productExceptSelfV2(int[] nums) {
+    int n = nums.length;
+
+    int[] left = new int[n];
+    left[0] = nums[0];
+
+    for (int i = 1; i < n; i++) {
+      left[i] = left[i - 1] * nums[i];
+    }
+
+    int[] right = new int[n];
+    right[n - 1] = nums[n - 1];
+
+    for (int i = n - 2; i >= 0; i--) {
+      right[i] = right[i + 1] * nums[i];
+    }
+
+    int[] output = new int[n];
+    for (int i = 0; i < n; i++) {
+      int l = i > 0? left[i - 1]: 1;
+      int r = i < n - 1? right[i + 1]: 1;
+
+      output[i] = l * r;
     }
 
     return output;
