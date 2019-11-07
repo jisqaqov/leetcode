7193f148aa5e39f794fb7144151be82fa17c3a82
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-07 18:49:47 +0300

    hash table: 953. Verifying an Alien Dictionary;

--- src/leetcode/VerifyingAnAlienDictionary953.java
+++ src/leetcode/VerifyingAnAlienDictionary953.java
@@ -1,9 +1,7 @@
 package leetcode;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author Jandos Iskakov
@@ -11,8 +9,8 @@ import java.util.Set;
  * algorithm: Hash Table
  * time complexity: O(|order| + |words| + 26^26)
  * space complexity: O(N)
- * Runtime: 3 ms, faster than 7.50% of Java online submissions for Verifying an Alien Dictionary.
- * Memory Usage: 36.1 MB, less than 100.00% of Java online submissions for Verifying an Alien Dictionary.
+ * Runtime: 1 ms, faster than 40.42% of Java online submissions for Verifying an Alien Dictionary.
+ * Memory Usage: 35.8 MB, less than 100.00% of Java online submissions for Verifying an Alien Dictionary.
  */
 public class VerifyingAnAlienDictionary953 {
 
@@ -31,38 +29,30 @@ public class VerifyingAnAlienDictionary953 {
   }
 
   public boolean isAlienSorted(String[] words, String order) {
-    Map<Character, Set<Character>> adjList = new HashMap<>();
-
-    for (int i = 1; i < words.length; i++) {
-      int n = Math.min(words[i].length(), words[i - 1].length());
-
-      for (int t = 0; t < n; t++) {
-        if (words[i].charAt(t) == words[i - 1].charAt(t)) {
-          continue;
-        }
-
-        adjList.putIfAbsent(words[i - 1].charAt(t), new HashSet<>());
-        adjList.get(words[i - 1].charAt(t)).add(words[i].charAt(t));
-
-        break;
-      }
-    }
-
-    if (adjList.isEmpty()) {
-      return false;
-    }
-
     Map<Character, Integer> indexes = new HashMap<>();
 
     for (int i = 0; i < order.length(); i++) {
       indexes.put(order.charAt(i), i);
     }
 
-    for (Character ch : adjList.keySet()) {
-      for (Character adj : adjList.get(ch)) {
-        if (indexes.get(ch) > indexes.get(adj)) {
+    for (int i = 0; i < words.length - 1; i++) {
+      int n = Math.min(words[i].length(), words[i + 1].length());
+
+      int t = 0;
+      for (; t < n; t++) {
+        if (words[i].charAt(t) == words[i + 1].charAt(t)) {
+          continue;
+        }
+
+        if (indexes.get(words[i].charAt(t)) > indexes.get(words[i + 1].charAt(t))) {
           return false;
         }
+
+        break;
+      }
+
+      if (t == n && words[i].length() > words[i + 1].length()) {
+        return false;
       }
     }
 
