474f7f589b7115152040a301a8c7792fbd12fc4f
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-15 16:24:31 +0600

    array: 896. Monotonic Array;

--- src/leetcode/MonotonicArray896.java
+++ src/leetcode/MonotonicArray896.java
@@ -6,8 +6,8 @@ package leetcode;
  * algorithm: Array
  * time complexity: O(n)
  * space complexity: O(1)
- * Runtime: 2 ms, faster than 76.31% of Java online submissions for Monotonic Array.
- * Memory Usage: 49.5 MB, less than 100.00% of Java online submissions for Monotonic Array.
+ * Runtime: 1 ms, faster than 100.00% of Java online submissions for Monotonic Array.
+ * Memory Usage: 48.8 MB, less than 100.00% of Java online submissions for Monotonic Array.
  */
 public class MonotonicArray896 {
 
@@ -25,24 +25,60 @@ public class MonotonicArray896 {
   }
 
   public boolean isMonotonic(int[] a) {
-    if (a.length <= 1) {
+    boolean increasing = true;
+    boolean decreasing = true;
+
+    for (int i = 0; i < a.length - 1; i++) {
+      if (a[i] < a[i + 1]) {
+        decreasing = false;
+      } else if (a[i] > a[i + 1]) {
+        increasing = false;
+      }
+    }
+
+    return increasing || decreasing;
+  }
+
+  private static class V3 {
+    public boolean isMonotonic(int[] a) {
+      int oldDiff = 0;
+
+      for (int i = 0; i < a.length - 1; i++) {
+        int diff = a[i] - a[i + 1];
+        if (diff != 0) {
+          if ((diff > 0 && oldDiff < 0) || (diff < 0 && oldDiff > 0)) {
+            return false;
+          }
+
+          oldDiff = diff;
+        }
+      }
+
       return true;
     }
+  }
 
-    int inc = 0;
-    int dec = 0;
-
-    for (int i = 1; i < a.length; i++) {
-      if (a[i] >= a[i - 1]) {
-        inc++;
+  private static class V2 {
+    public boolean isMonotonic(int[] a) {
+      if (a.length <= 1) {
+        return true;
       }
 
-      if (a[i] <= a[i - 1]) {
-        dec++;
+      int inc = 0;
+      int dec = 0;
+
+      for (int i = 1; i < a.length; i++) {
+        if (a[i] >= a[i - 1]) {
+          inc++;
+        }
+
+        if (a[i] <= a[i - 1]) {
+          dec++;
+        }
       }
+
+      return inc == a.length - 1 || dec == a.length - 1;
     }
-
-    return inc == a.length - 1 || dec == a.length - 1;
   }
 
 }
