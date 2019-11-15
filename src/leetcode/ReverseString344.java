2fc517510b894cb696fce210b134dd3656c4a8f8
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-15 19:34:54 +0600

    string: resolved 344. Reverse String;

---
+++ src/leetcode/ReverseString344.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * @author Jandos Iskakov
+ * problem: 344. Reverse String
+ * algorithm: String
+ * time complexity: O(N)
+ * space complexity: O(1)
+ * Runtime: 1 ms, faster than 100.00% of Java online submissions for Reverse String.
+ * Memory Usage: 52.4 MB, less than 6.51% of Java online submissions for Reverse String.
+ */
+public class ReverseString344 {
+
+  public void reverseString(char[] s) {
+    int l = 0;
+    int r = s.length - 1;
+
+    while (l < r) {
+      char temp = s[l];
+      s[l] = s[r];
+      s[r] = temp;
+
+      l++;
+      r--;
+    }
+  }
+
+}
