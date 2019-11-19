00f3aefad0798412793f5eef4010a03c3c783928
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-19 17:06:38 +0300

    two pointers: resolved 88. Merge Sorted Array;

--- src/leetcode/MergeSortedArray88.java
+++ src/leetcode/MergeSortedArray88.java
@@ -27,37 +27,71 @@ public class MergeSortedArray88 {
   }
 
   public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int k = nums1.length - 1;
     int i = m - 1;
-    for (int j = nums1.length - 1; i >= 0; j--, i--) {
-      nums1[j] = nums1[i];
+    int j = n - 1;
+
+    while (i >= 0 && j >= 0) {
+      if (nums1[i] >= nums2[j]) {
+        nums1[k] = nums1[i];
+
+        i--;
+        k--;
+      } else if (nums1[i] < nums2[j]) {
+        nums1[k] = nums2[j];
+
+        j--;
+        k--;
+      }
     }
 
-    int p = nums1.length - m;
-    int q = 0;
-    int k = 0;
+    while (i >= 0) {
+      nums1[k] = nums1[i];
+      k--;
+      i--;
+    }
 
-    while (p < nums1.length && q < nums2.length) {
-      if (nums1[p] <= nums2[q]) {
+    while (j >= 0) {
+      nums1[k] = nums2[j];
+      k--;
+      j--;
+    }
+  }
+
+  private static class V2 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+      int i = m - 1;
+      for (int j = nums1.length - 1; i >= 0; j--, i--) {
+        nums1[j] = nums1[i];
+      }
+
+      int p = nums1.length - m;
+      int q = 0;
+      int k = 0;
+
+      while (p < nums1.length && q < nums2.length) {
+        if (nums1[p] <= nums2[q]) {
+          nums1[k] = nums1[p];
+          p++;
+          k++;
+        } else if (nums2[q] < nums1[p]) {
+          nums1[k] = nums2[q];
+          q++;
+          k++;
+        }
+      }
+
+      while (p < nums1.length) {
         nums1[k] = nums1[p];
         p++;
         k++;
-      } else if (nums2[q] < nums1[p]) {
+      }
+
+      while (q < nums2.length) {
         nums1[k] = nums2[q];
         q++;
         k++;
       }
-    }
-
-    while (p < nums1.length) {
-      nums1[k] = nums1[p];
-      p++;
-      k++;
-    }
-
-    while (q < nums2.length) {
-      nums1[k] = nums2[q];
-      q++;
-      k++;
     }
   }
 
