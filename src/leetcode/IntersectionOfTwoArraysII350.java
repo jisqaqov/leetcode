982c1fd08bf3d2953933ac05e31f22d233dedf1e
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-19 14:30:42 +0300

    hash table: resolved 350. Intersection of Two Arrays II;

--- src/leetcode/IntersectionOfTwoArraysII350.java
+++ src/leetcode/IntersectionOfTwoArraysII350.java
@@ -9,10 +9,10 @@ import utils.TestUtils;
  * @author Jandos Iskakov
  * problem: 350. Intersection of Two Arrays II
  * algorithm: Hash Table
- * time complexity: O(N + M)
- * space complexity: O(N + M)
- * Runtime: 4 ms, faster than 22.41% of Java online submissions for Intersection of Two Arrays II.
- * Memory Usage: 36.6 MB, less than 83.87% of Java online submissions for Intersection of Two Arrays II.
+ * time complexity: O(nlogn + mlogm)
+ * space complexity: O(n + m)
+ * Runtime: 1 ms, faster than 100.00% of Java online submissions for Intersection of Two Arrays II.
+ * Memory Usage: 36.2 MB, less than 83.87% of Java online submissions for Intersection of Two Arrays II.
  */
 public class IntersectionOfTwoArraysII350 {
 
@@ -29,29 +29,58 @@ public class IntersectionOfTwoArraysII350 {
   }
 
   public int[] intersect(int[] nums1, int[] nums2) {
-    Map<Integer, Integer> c1 = new HashMap<>();
-    for (int num : nums1) {
-      c1.put(num, c1.getOrDefault(num, 0) + 1);
-    }
+    Arrays.sort(nums1);
+    Arrays.sort(nums2);
 
-    Map<Integer, Integer> c2 = new HashMap<>();
-    for (int num : nums2) {
-      c2.put(num, c2.getOrDefault(num, 0) + 1);
-    }
+    int i = 0;
+    int j = 0;
+    int k = 0;
 
-    int n = 0;
+    int[] sol = new int[nums1.length + nums2.length];
 
-    int[] list = new int[nums1.length + nums2.length];
+    while (i < nums1.length && j < nums2.length) {
+      if (nums1[i] < nums2[j]) {
+        i++;
+      } else if (nums1[i] > nums2[j]) {
+        j++;
+      } else {
+        sol[k] = nums1[i];
 
-    for (int num : c1.keySet()) {
-      int m = Math.min(c1.get(num), c2.getOrDefault(num, 0));
-
-      for (int k = 0; k < m; k++) {
-        list[n++] = num;
+        k++;
+        i++;
+        j++;
       }
     }
 
-    return Arrays.copyOfRange(list, 0, n);
+    return Arrays.copyOfRange(sol, 0, k);
+  }
+
+  private static class V2 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+      Map<Integer, Integer> c1 = new HashMap<>();
+      for (int num : nums1) {
+        c1.put(num, c1.getOrDefault(num, 0) + 1);
+      }
+
+      Map<Integer, Integer> c2 = new HashMap<>();
+      for (int num : nums2) {
+        c2.put(num, c2.getOrDefault(num, 0) + 1);
+      }
+
+      int n = 0;
+
+      int[] list = new int[nums1.length + nums2.length];
+
+      for (int num : c1.keySet()) {
+        int m = Math.min(c1.get(num), c2.getOrDefault(num, 0));
+
+        for (int k = 0; k < m; k++) {
+          list[n++] = num;
+        }
+      }
+
+      return Arrays.copyOfRange(list, 0, n);
+    }
   }
 
 }
