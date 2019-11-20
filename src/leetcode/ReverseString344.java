95c0b3e255f8108472cf3e48ee878bc8a67f97ac
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-20 12:30:49 +0300

    string: resolved 344. Reverse String;

--- src/leetcode/ReverseString344.java
+++ src/leetcode/ReverseString344.java
@@ -25,4 +25,23 @@ public class ReverseString344 {
     }
   }
 
+  private static class V2 {
+    public void reverseString(char[] s) {
+      helper(s, 0, s.length - 1);
+    }
+
+    public void helper(char[] s, int l, int r) {
+      if (l >= r) {
+        return;
+      }
+
+      char temp = s[l];
+      s[l] = s[r];
+      s[r] = temp;
+
+      helper(s, l + 1, r - 1);
+
+    }
+  }
+
 }
