c82477dee677122f6a379b670e8cbd8be7a583fa
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-22 15:29:32 +0300

    sort: resolved 973. K Closest Points to Origin

--- src/leetcode/KClosestPointsToOrigin973.java
+++ src/leetcode/KClosestPointsToOrigin973.java
@@ -1,14 +1,17 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.PriorityQueue;
 
 /**
  * @author Jandos Iskakov
  * problem: 973. K Closest Points to Origin
- * algorithm: Heap
+ * algorithm: Sort, Heap, Divide And Conquer
  * time complexity: O(nlog(k))
  * space complexity: O(k)
+ * Runtime: 4 ms, faster than 99.72% of Java online submissions for K Closest Points to Origin.
+ * Memory Usage: 58.6 MB, less than 77.02% of Java online submissions for K Closest Points to Origin.
  */
 public class KClosestPointsToOrigin973 {
 
@@ -18,7 +21,7 @@ public class KClosestPointsToOrigin973 {
   }
 
   private void test() {
-    SortApproach problem = new SortApproach();
+    SortVersion problem = new SortVersion();
 
     int[][] tc1a = {{-95,76},{17,7},{-55,-58},{53,20},{-69,-8},{-57,87},{-2,-42},{-10,-87},{-36,-57},{97,-39},{97,49}};
 
@@ -33,39 +36,105 @@ public class KClosestPointsToOrigin973 {
   }
 
   public int[][] kClosest(int[][] points, int k) {
-    PriorityQueue<double[]> heap =
-      new PriorityQueue<>((o1, o2) -> Double.compare(o2[2], o1[2]));
+    int len = k - 1;
 
-    for (int[] point : points) {
-      int x = point[0], y = point[1];
-      double d = Math.sqrt(x * x + y * y);
+    int l = 0;
+    int r = points.length - 1;
 
-      if (heap.size() < k) {
-        heap.add(new double[]{x, y, d});
+    while (l <= r) {
+      int pivotIndex = partition(l, r, points);
+
+      if (pivotIndex == len) {
+        break;
+      } else if (pivotIndex < len) {
+        l = pivotIndex + 1;
       } else {
-        double[] peek = heap.peek();
-        if (peek[2] > d) {
-          heap.poll();
-          heap.add(new double[]{x, y, d});
-        }
+        r = pivotIndex - 1;
       }
     }
 
-    int[][] result = new int[k][2];
-
-    Iterator<double[]> it = heap.iterator();
-    for (int i = 0; it.hasNext() && i < k; i++) {
-      result[i] = new int[2];
-      double[] items = it.next();
-
-      result[i][0] = (int)items[0];
-      result[i][1] = (int)items[1];
-    }
-
-    return result;
+    return Arrays.copyOf(points, k);
   }
 
-  private static class SortApproach {
+  private int partition(int l, int r, int[][] points) {
+    int i = l + 1;
+    int j = r;
+
+    while (true) {
+      while (i < r && dist(i, points) <= dist(l, points)) {
+        i++;
+      }
+
+      while (j > l && dist(j, points) >= dist(l, points)) {
+        j--;
+      }
+
+      if (i >= j) {
+        break;
+      }
+
+      swap(i, j, points);
+
+      i++;
+      j--;
+    }
+
+    swap(l, j, points);
+
+    return j;
+  }
+
+  private void swap(int i, int j, int[][] points) {
+    int tempX = points[j][0];
+    int tempY = points[j][1];
+
+    points[j][0] = points[i][0];
+    points[j][1] = points[i][1];
+
+    points[i][0] = tempX;
+    points[i][1] = tempY;
+  }
+
+  private int dist(int index, int[][] points) {
+    return points[index][0] * points[index][0] + points[index][1] * points[index][1];
+  }
+
+  private static class HeapVersion {
+    public int[][] kClosest(int[][] points, int k) {
+      PriorityQueue<double[]> heap =
+        new PriorityQueue<>((o1, o2) -> Double.compare(o2[2], o1[2]));
+
+      for (int[] point : points) {
+        int x = point[0], y = point[1];
+        double d = Math.sqrt(x * x + y * y);
+
+        if (heap.size() < k) {
+          heap.add(new double[]{x, y, d});
+        } else {
+          double[] peek = heap.peek();
+          if (peek[2] > d) {
+            heap.poll();
+            heap.add(new double[]{x, y, d});
+          }
+        }
+      }
+
+      int[][] result = new int[k][2];
+
+      Iterator<double[]> it = heap.iterator();
+      for (int i = 0; it.hasNext() && i < k; i++) {
+        result[i] = new int[2];
+        double[] items = it.next();
+
+        result[i][0] = (int)items[0];
+        result[i][1] = (int)items[1];
+      }
+
+      return result;
+    }
+  }
+
+  private static class SortVersion {
     public int[][] kClosest(int[][] points, int k) {
       sort(0, points.length - 1, k, points);
 
@@ -97,14 +166,15 @@ public class KClosestPointsToOrigin973 {
     private int partition(int[][] points, int start, int end) {
       int pivot = dist(points, start);
 
-      int i = start + 1, j = end;
+      int i = start + 1;
+      int j = end;
 
       while (true) {
         while (i < end && dist(points, i) < pivot) {
           i++;
         }
 
-        while (j >start && dist(points, j) > pivot) {
+        while (j > start && dist(points, j) > pivot) {
           j--;
         }
 
