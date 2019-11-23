017791ce5b209373416a820b55705e0b07dd1ed7
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-23 10:04:33 +0300

    sort: 56. Merge Intervals;

--- src/leetcode/MergeIntervals56.java
+++ src/leetcode/MergeIntervals56.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import utils.TestUtils;
 
 /**
  * @author Jandos Iskakov
@@ -9,6 +10,8 @@ import java.util.Comparator;
  * algorithm: Sort, Array
  * time complexity: O(nlog(n))
  * space complexity: O(n)
+ * Runtime: 9 ms, faster than 56.10% of Java online submissions for Merge Intervals.
+ * Memory Usage: 38.8 MB, less than 91.30% of Java online submissions for Merge Intervals.
  */
 public class MergeIntervals56 {
 
@@ -19,48 +22,40 @@ public class MergeIntervals56 {
 
   private void test() {
     int[][] tc1a = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-    printArray(merge(tc1a));
-  }
+    TestUtils.printArray(merge(tc1a));
 
-  private void printArray(int[][] array) {
-    for (int i = 0; i < array.length; i++) {
-      for (int j = 0; j < array[i].length; j++) {
-        System.out.print(array[i][j] + " ");
-      }
-      System.out.println();
-    }
+    int[][] tc2a = {{1, 4}, {4, 5}};
+    TestUtils.printArray(merge(tc2a));
+
+    int[][] tc3a = {{1, 4}, {0, 4}};
+    TestUtils.printArray(merge(tc3a));
   }
 
   public int[][] merge(int[][] intervals) {
-    int n = intervals.length;
-    if (n == 0) {
-      return intervals;
+    int[][] output = new int[intervals.length][2];
+
+    if (intervals.length == 0) {
+      return output;
     }
 
-    int[][] solution = new int[n][2];
+    Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
 
-    Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+    int k = 0;
+    output[0][0] = intervals[0][0];
+    output[0][1] = intervals[0][1];
 
-    solution[0][0] = intervals[0][0];
-    solution[0][1] = intervals[0][1];
-
-    int j = 0;
-
-    for (int i = 1; i < n; i++) {
-      int start = intervals[i][0];
-      int end = intervals[i][1];
-
-      if (start >= solution[j][0] && start <= solution[j][1]) {
-        solution[j][1] = Math.max(end, solution[j][1]);
+    for (int i = 1; i < intervals.length; i++) {
+      if (intervals[i][0] >= output[k][0] && intervals[i][0] <= output[k][1]) {
+        output[k][1] = Math.max(output[k][1], intervals[i][1]);
       } else {
-        j++;
-
-        solution[j][0] = start;
-        solution[j][1] = end;
+        k++;
+        output[k][0] = intervals[i][0];
+        output[k][1] = intervals[i][1];
       }
     }
 
-    return Arrays.copyOfRange(solution, 0, j + 1);
+    int len = k + 1;
+    return Arrays.copyOf(output, len);
   }
 
 
