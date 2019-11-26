1d469db5748afca792b8e460a86aa0c31a33a9d3
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-26 16:54:03 +0300

    dfs: resolved 721. Accounts Merge;

--- src/leetcode/AccountsMerge721.java
+++ src/leetcode/AccountsMerge721.java
@@ -2,14 +2,12 @@ package leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * @author Jandos Iskakov
@@ -17,8 +15,8 @@ import java.util.stream.Collectors;
  * algorithm: DFS, Graph
  * time complexity: O(V+E)
  * space complexity: O(V+E)
- * Runtime: 158 ms, faster than 6.28% of Java online submissions for Accounts Merge.
- * Memory Usage: 60.1 MB, less than 5.88% of Java online submissions for Accounts Merge.
+ * Runtime: 44 ms, faster than 58.44% of Java online submissions for Accounts Merge.
+ * Memory Usage: 47.8 MB, less than 35.29% of Java online submissions for Accounts Merge.
  */
 public class AccountsMerge721 {
 
@@ -38,96 +36,58 @@ public class AccountsMerge721 {
   }
 
   public List<List<String>> accountsMerge(List<List<String>> accounts) {
-    Map<String, List<Account>> map = new HashMap<>();
+    Map<String, Set<String>> graph = new HashMap<>();
+    Map<String, String> emailName = new HashMap<>();
 
-    for (int i = 0; i < accounts.size(); i++) {
-      List<String> emails = accounts.get(i);
+    for (List<String> account : accounts) {
+      String username = account.get(0);
 
-      Account account = new Account();
-      account.name = emails.get(0);
-      account.index = i;
-      account.emails = new HashSet<>(emails.subList(1, emails.size()));
+      String rootEmail = account.get(1);
 
-      for (String email : account.emails) {
-        map.putIfAbsent(email, new ArrayList<>());
-        map.get(email).add(account);
+      for (int i = 1; i < account.size(); i++) {
+        String email = account.get(i);
+
+        graph.putIfAbsent(email, new HashSet<>());
+
+        graph.get(email).add(rootEmail);
+        graph.get(rootEmail).add(email);
+
+        emailName.putIfAbsent(email, username);
       }
     }
 
-    List<Account> sol = new ArrayList<>();
-    Set<Integer> used = new HashSet<>();
+    Set<String> used = new HashSet<>();
+    List<List<String>> output = new ArrayList<>();
 
-    for (String email : map.keySet()) {
-      for (Account account : map.get(email)) {
-        if (used.contains(account.index)) {
-          continue;
-        }
-
-        sol.add(account);
-
-        dfs(account, account, map, used);
+    for (String email : graph.keySet()) {
+      if (used.contains(email)) {
+        continue;
       }
+
+      List<String> emails = new ArrayList<>();
+      dfs(emails, graph, used, email);
+      Collections.sort(emails);
+
+      List<String> account = new ArrayList<>();
+      account.add(emailName.get(email));
+      account.addAll(emails);
+
+      output.add(account);
     }
 
-    return sol.stream()
-      .map(account -> {
-        List<String> emails = new ArrayList<>();
-        emails.add(account.name);
-        emails.addAll(account.emails
-          .stream()
-          .sorted()
-          .collect(Collectors.toList()));
-        return emails;
-      }).collect(Collectors.toList());
+    return output;
   }
 
-  private void dfs(Account merge, Account adj, Map<String, List<Account>> map,
-    Set<Integer> used) {
-    if (used.contains(adj.index)) {
-      return;
-    }
+  private void dfs(List<String> list, Map<String, Set<String>> graph, Set<String> used,
+    String email) {
+    list.add(email);
 
-    used.add(adj.index);
+    used.add(email);
 
-    Set<Account> adjList = new HashSet<>();
-
-    for (String email : adj.emails) {
-      adjList.addAll(map.get(email));
-    }
-
-    adjList.remove(merge);
-
-    for (Account account : adjList) {
-      merge.emails.addAll(account.emails);
-
-      if (!used.contains(account.index)) {
-        dfs(merge, account, map, used);
+    for (String adj : graph.get(email)) {
+      if (!used.contains(adj)) {
+        dfs(list, graph, used, adj);
       }
-    }
-  }
-
-  private static class Account {
-
-    String name;
-    Set<String> emails;
-    int index;
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (o == null || getClass() != o.getClass()) {
-        return false;
-      }
-      Account account = (Account) o;
-      return index == account.index &&
-        Objects.equals(name, account.name);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(name, index);
     }
   }
 
