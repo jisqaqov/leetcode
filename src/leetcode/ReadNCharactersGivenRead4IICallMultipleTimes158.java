b57fd41c032c85d12eaf18eb5aa0977b541724cf
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-29 17:36:38 +0300

    solved problem 158. Read N Characters Given Read4 II - Call multiple times;

--- src/leetcode/ReadNCharactersGivenRead4IICallMultipleTimes158.java
+++ src/leetcode/ReadNCharactersGivenRead4IICallMultipleTimes158.java
@@ -18,49 +18,49 @@ public class ReadNCharactersGivenRead4IICallMultipleTimes158 {
    * The read4 API is defined in the parent class Reader4.
    *     int read4(char[] buf);
    */
-    public class Solution extends Reader4 {
-      private boolean isEOF = false;
-      private Queue<Character> queue = new LinkedList<>();
+  public class Solution extends Reader4 {
+    private boolean isEOF = false;
+    private Queue<Character> queue = new LinkedList<>();
 
-      /**
-       * @param buf Destination buffer
-       * @param n   Number of characters to read
-       * @return    The number of actual characters read
-       */
-      public int read(char[] buf, int n) {
-        if (isEOF && queue.isEmpty()) {
-          return 0;
-        }
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    public int read(char[] buf, int n) {
+      if (isEOF && queue.isEmpty()) {
+        return 0;
+      }
 
-        int size = 0;
+      int size = 0;
 
-        while (size < n) {
-          if (!queue.isEmpty()) {
-            buf[size] = queue.poll();
-            size++;
-          } else {
-            char[] buf4 = new char[4];
-            int n4 = read4(buf4);
+      while (size < n) {
+        if (!queue.isEmpty()) {
+          buf[size] = queue.poll();
+          size++;
+        } else {
+          char[] buf4 = new char[4];
+          int n4 = read4(buf4);
 
-            for (int i = 0; i < n4; i++) {
-              if (size < n) {
-                buf[size] = buf4[i];
-                size++;
-              } else {
-                queue.add(buf4[i]);
-              }
-            }
-
-            if (n4 == 0) {
-              isEOF = true;
-              break;
+          for (int i = 0; i < n4; i++) {
+            if (size < n) {
+              buf[size] = buf4[i];
+              size++;
+            } else {
+              queue.add(buf4[i]);
             }
           }
-        }
 
-        return size;
+          if (n4 == 0) {
+            isEOF = true;
+            break;
+          }
+        }
       }
+
+      return size;
     }
+  }
 
   private class Reader4 {
     int read4(char[] buf) {
