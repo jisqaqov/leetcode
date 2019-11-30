8579fb67a520f2507121e2f968700a36aea98a12
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-11-30 15:36:31 +0300

    linked list: solved 708. Insert into a Sorted Circular Linked List;

--- src/leetcode/InsertIntoSortedCircularLinkedList708.java
+++ src/leetcode/InsertIntoSortedCircularLinkedList708.java
@@ -1,13 +1,25 @@
 package leetcode;
 
 /**
- * @author Jandos Iskakov
+ * @author https://leetcode.com/problems/insert-into-a-sorted-circular-linked-list/discuss/149374/Java-5ms-One-Pass-and-Two-Pass-Traverse-With-Detailed-Comments-and-Edge-cases!/221648
  * problem: 708. Insert into a Sorted Circular Linked List
  * algorithm: Linked List
  * time complexity: O(N)
  * space complexity: O(1)
  * Runtime: 0 ms, faster than 100.00% of Java online submissions for Insert into a Sorted Circular Linked List.
  * Memory Usage: 36.2 MB, less than 10.00% of Java online submissions for Insert into a Sorted Circular Linked List.
+ * 3 cases
+ * case 1: insertVal is between 2 nodes
+ *  e.g. 1->2->4, insert 3
+ *  condition: insertVal >= n.val && insertVal <= n.next.val
+ *
+ * case 2: insertVal is >= largest node value or <= smalles node value
+ *  e.g. 1->2->4, insert 0 or 1->2->4, insert 5
+ *  condition: n.next.val < n.val && (insertVal >= n.val || insertVal <= n.next.val)
+ *
+ * case 3: all the nodes in the tree have same value
+ *  e.g. 1->1->1, insert 2
+ *  condition: n.next == head
  */
 public class InsertIntoSortedCircularLinkedList708 {
 
@@ -29,52 +41,33 @@ public class InsertIntoSortedCircularLinkedList708 {
   }
 
   public Node insert(Node head, int insertVal) {
-    Node newNode = new Node();
-    newNode.val = insertVal;
+    Node newNode = new Node(insertVal, null);
 
     if (head == null) {
       newNode.next = newNode;
       return newNode;
     }
 
-    boolean isFound = false;
-
     Node currNode = head;
-    Node tail = null;
 
     while (true) {
-      if (insertVal >= currNode.val && insertVal <= currNode.next.val) {
-        isFound = true;
-        break;
-      }
+      if ((insertVal >= currNode.val && insertVal <= currNode.next.val) ||
+          (currNode.val > currNode.next.val && (insertVal >= currNode.val || insertVal <= currNode.next.val)) ||
+          (currNode.next == head)) {
+        newNode.next = currNode.next;
+        currNode.next = newNode;
 
-      if (currNode.val > currNode.next.val) {
-        tail = currNode;
+        break;
       }
 
       currNode = currNode.next;
-
-      if (currNode == head) {
-        break;
-      }
-    }
-
-    if (isFound) {
-      newNode.next = currNode.next;
-      currNode.next = newNode;
-    } else if (tail != null) {
-      newNode.next = tail.next;
-      tail.next = newNode;
-    } else {
-      newNode.next = head;
-      head.next = newNode;
     }
 
     return head;
   }
 
   // Definition for a Node.
-  class Node {
+  static class Node {
 
     public int val;
     public Node next;
