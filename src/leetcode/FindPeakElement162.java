c06a9d51a538ff1623f2daf52c405eeeea3652d3
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-01 11:03:17 +0300

    binary search: resolved 162. Find Peak Element;

--- src/leetcode/FindPeakElement162.java
+++ src/leetcode/FindPeakElement162.java
@@ -28,12 +28,6 @@ public class FindPeakElement162 {
     V2 v2 = new V2();
     System.out.println(v2.findPeakElement(tc1a));
     System.out.println(v2.findPeakElement(tc2a));
-
-    System.out.println("v3:");
-
-    V3 v3 = new V3();
-    System.out.println(v3.findPeakElement(tc1a));
-    System.out.println(v3.findPeakElement(tc2a));
   }
 
   public int findPeakElement(int[] nums) {
@@ -42,35 +36,14 @@ public class FindPeakElement162 {
 
     while (l < r) {
       int mid = l + (r - l) / 2;
-
-      if (mid > 0 && nums[mid - 1] > nums[mid]) {
-        r = mid - 1;
-      } else if (mid < nums.length - 1 && nums[mid + 1] > nums[mid]) {
+      if (nums[mid] < nums[mid + 1]) {
         l = mid + 1;
       } else {
-        return mid;
+        r = mid;
       }
     }
 
-    return -1;
-  }
-
-  private static class V3 {
-    public int findPeakElement(int[] nums) {
-      int l = 0;
-      int r = nums.length - 1;
-
-      while (l < r) {
-        int mid = l + (r - l) / 2;
-        if (nums[mid] < nums[mid + 1]) {
-          l = mid + 1;
-        } else {
-          r = mid;
-        }
-      }
-
-      return l;
-    }
+    return l;
   }
 
   private static class V2 {
