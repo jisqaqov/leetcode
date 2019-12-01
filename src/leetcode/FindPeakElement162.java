52e0dccfc051158a13e5aae0d8ee45036d68793b
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-01 10:54:55 +0300

    binary search: resolved 162. Find Peak Element;

--- src/leetcode/FindPeakElement162.java
+++ src/leetcode/FindPeakElement162.java
@@ -17,15 +17,30 @@ public class FindPeakElement162 {
   }
 
   private void test() {
-    System.out.println(findPeakElement(new int[]{1, 2, 3, 1}));
-    System.out.println(findPeakElement(new int[]{1, 2, 1, 3, 5, 6, 4}));
+    int[] tc1a = new int[]{1, 2, 3, 1};
+    int[] tc2a = new int[]{1, 2, 1, 3, 5, 6, 4};
+
+    System.out.println(findPeakElement(tc1a));
+    System.out.println(findPeakElement(tc2a));
+
+    System.out.println("v2:");
+
+    V2 v2 = new V2();
+    System.out.println(v2.findPeakElement(tc1a));
+    System.out.println(v2.findPeakElement(tc2a));
+
+    System.out.println("v3:");
+
+    V3 v3 = new V3();
+    System.out.println(v3.findPeakElement(tc1a));
+    System.out.println(v3.findPeakElement(tc2a));
   }
 
   public int findPeakElement(int[] nums) {
     int l = 0;
     int r = nums.length - 1;
 
-    while (l <= r) {
+    while (l < r) {
       int mid = l + (r - l) / 2;
 
       if (mid > 0 && nums[mid - 1] > nums[mid]) {
@@ -38,6 +53,43 @@ public class FindPeakElement162 {
     }
 
     return -1;
+  }
+
+  private static class V3 {
+    public int findPeakElement(int[] nums) {
+      int l = 0;
+      int r = nums.length - 1;
+
+      while (l < r) {
+        int mid = l + (r - l) / 2;
+        if (nums[mid] < nums[mid + 1]) {
+          l = mid + 1;
+        } else {
+          r = mid;
+        }
+      }
+
+      return l;
+    }
+  }
+
+  private static class V2 {
+    public int findPeakElement(int[] nums) {
+      return findPeakElement(nums, 0, nums.length - 1);
+    }
+
+    public int findPeakElement(int[] nums, int l, int r) {
+      if (l == r) {
+        return l;
+      }
+
+      int mid = l + (r - l) / 2;
+      if (nums[mid] > nums[mid + 1]) {
+        return findPeakElement(nums, l, mid);
+      }
+
+      return findPeakElement(nums, mid + 1, r);
+    }
   }
 
 }
