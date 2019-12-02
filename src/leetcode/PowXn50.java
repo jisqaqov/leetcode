0d3f88fd9b44c941f3f022c050238221a0b3f730
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-02 17:57:48 +0300

    binary search: resolved 50. Pow(x, n);

--- src/leetcode/PowXn50.java
+++ src/leetcode/PowXn50.java
@@ -4,67 +4,56 @@ package leetcode;
  * @author Jandos Iskakov
  * problem: 50. Pow(x, n)
  * algorithm: Binary Search
- * notes: search for the number it's root n equals x
+ * https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+ * time complexity: O(log(n))
+ * space complexity: O(log(n))
+ * Runtime: 1 ms, faster than 61.30% of Java online submissions for Pow(x, n).
+ * Memory Usage: 33.5 MB, less than 5.88% of Java online submissions for Pow(x, n).
  */
 public class PowXn50 {
 
-    public static void main(String[] args) {
-        PowXn50 solution = new PowXn50();
-        solution.test();
+  public static void main(String[] args) {
+    PowXn50 problem = new PowXn50();
+    problem.test();
+  }
+
+  public void test() {
+    System.out.println(myPow(2.00000, -2147483648));
+    System.out.println(myPow(0.44528, 0));
+    System.out.println(myPow(2, 3));
+    System.out.println(myPow(2, 10));
+    System.out.println(myPow(2.1, 3));
+    System.out.println(myPow(2, -2));
+    System.out.println(myPow(-2, 3));
+
+    System.out.println(myPow(0.00001, 2147483647));
+    System.out.println(myPow(-1, 3));
+    System.out.println(myPow(-1, 4));
+    System.out.println(myPow(1, 3));
+    System.out.println(myPow(1, 2));
+  }
+
+  public double myPow(double x, int n) {
+    if (n < 0) {
+      x = 1 / x;
+      n = -1 * n;
     }
 
-    public void test() {
-        System.out.println(pow(2.00000,-2147483648));
-        System.out.println(pow(0.44528, 0));
-        System.out.println(pow(2, 3));
-        System.out.println(pow(2, 10));
-        System.out.println(pow(2.1, 3));
-        System.out.println(pow(2, -2));
-        System.out.println(pow(-2, 3));
+    return squaring(x, n);
+  }
 
-        System.out.println(pow(0.00001, 2147483647));
-        System.out.println(pow(-1, 3));
-        System.out.println(pow(-1, 4));
-        System.out.println(pow(1, 3));
-        System.out.println(pow(1, 2));
+  private double squaring(double x, int n) {
+    if (n == 0) {
+      return 1;
     }
 
-    public double pow(double x, int n) {
-        if (n == 0)
-            return 1;
+    double h = squaring(x, n / 2);
 
-        if (Math.abs(x) == 1)
-            return n % 2 > 0? x: 1;
-
-        double l = 0, r = Double.MAX_VALUE;
-
-        double result = 0, prev = 0;
-
-        while (l <= r) {
-            double mid = l + (r - l) / 2;
-            double root = Math.pow(mid, 1.0/Math.abs(n));
-
-            if (mid == 0 || root == 0 || root == prev) {
-                return 0;
-            } else if (root == Math.abs(x)) {
-                result = mid;
-                break;
-            } else if (root > Math.abs(x)) {
-                r = mid;
-            } else {
-                l = mid;
-            }
-
-            prev = root;
-        }
-
-        if (n < 0)
-            result = 1 / result;
-
-        if (x < 0 && n % 2 > 0)
-            result *= -1;
-
-        return result;
+    if (n % 2 == 0) {
+      return h * h;
+    } else {
+      return h * h * x;
     }
+  }
 
 }
--- src/prep/Prep.java
+++ src/prep/Prep.java
@@ -1,8 +1,5 @@
 package prep;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Prep {
 
   public static void main(String[] args) {
@@ -11,38 +8,43 @@ public class Prep {
   }
 
   private void test() {
-    System.out.println(isStrobogrammatic("0"));//true
-    System.out.println(isStrobogrammatic("1"));//true
-    System.out.println(isStrobogrammatic("69"));//true
-    System.out.println(isStrobogrammatic("88"));//true
-    System.out.println(isStrobogrammatic("962"));//false
+    System.out.println(myPow(2.00000,-2147483648));
+    System.out.println(myPow(0.44528, 0));
+    System.out.println(myPow(2, 3));
+    System.out.println(myPow(2, 10));
+    System.out.println(myPow(2.1, 3));
+    System.out.println(myPow(2, -2));
+    System.out.println(myPow(-2, 3));
+
+    System.out.println(myPow(0.00001, 2147483647));
+    System.out.println(myPow(-1, 3));
+    System.out.println(myPow(-1, 4));
+    System.out.println(myPow(1, 3));
+    System.out.println(myPow(1, 2));
   }
 
-  public boolean isStrobogrammatic(String num) {
-    Map<Integer, Integer> map = new HashMap<>();
-
-    map.put(0, 0);
-    map.put(1, 1);
-    map.put(6, 9);
-    map.put(9, 6);
-    map.put(8, 8);
-
-    int i = 0;
-    int j = num.length() - 1;
-
-    while (i <= j) {
-      int startNum = Character.getNumericValue(num.charAt(i));
-      int endNum = Character.getNumericValue(num.charAt(j));
-
-      if (startNum != map.getOrDefault(endNum, -1)) {
-        return false;
-      }
-
-      i++;
-      j--;
+  public double myPow(double x, int n) {
+    if (n < 0) {
+      x = 1 / x;
+      n = -1 * n;
     }
 
-    return true;
+    return squaring(x, n);
   }
+
+  private double squaring(double x, int n) {
+    if (n == 0) {
+      return 1;
+    }
+
+    double h = squaring(x, n / 2);
+
+    if (n % 2 == 0) {
+      return h * h;
+    } else {
+      return h * h * x;
+    }
+  }
+
 
 }
