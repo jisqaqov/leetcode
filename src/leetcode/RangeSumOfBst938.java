b692da178be9a36dfd1d21706cf47e48ea7897cb
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-06 21:10:55 +0300

    bst: resolved old problem 938. Range Sum of BST;

---
+++ src/leetcode/RangeSumOfBst938.java
@@ -0,0 +1,75 @@
+package leetcode;
+
+/**
+ * @author Jandos Iskakov
+ * problem: 938. Range Sum of BST
+ * algorithm: BST
+ * time complexity: O(N)
+ * space complexity: O(N)
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Range Sum of BST.
+ * Memory Usage: 45.6 MB, less than 97.67% of Java online submissions for Range Sum of BST.
+ */
+public class RangeSumOfBst938 {
+
+  public static void main(String[] args) {
+    RangeSumOfBst938 problem = new RangeSumOfBst938();
+    problem.test();
+  }
+
+  private void test() {
+    TreeNode root = new TreeNode(10);
+    TreeNode node5 = new TreeNode(5);
+    TreeNode node3 = new TreeNode(3);
+    TreeNode node7 = new TreeNode(7);
+    TreeNode node15 = new TreeNode(15);
+    TreeNode node18 = new TreeNode(18);
+
+    root.left = node5;
+    root.right = node15;
+    node5.left = node3;
+    node5.right = node7;
+    node15.right = node18;
+
+    System.out.println(rangeSumBST(root, 7, 15));
+  }
+
+  public int rangeSumBST(TreeNode root, int l, int r) {
+    if (root == null) {
+      return 0;
+    }
+
+    int s = 0;
+
+    if (root.val > l) {
+      s += rangeSumBST(root.left, l, r);
+    }
+
+    if (root.val < r) {
+      s += rangeSumBST(root.right, l, r);
+    }
+
+    if (root.val >= l && root.val <= r) {
+      s += root.val;
+    }
+
+    return s;
+  }
+
+  /**
+   * Definition for a binary tree node.
+   */
+  public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+
+}
+
+
