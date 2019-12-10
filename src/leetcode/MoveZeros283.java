8767ffd7d28a8b772203616f4f746a27a2a8fe49
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-10 21:10:49 +0300

    array: resolved 283. Move Zeroes;

--- src/leetcode/MoveZeros283.java
+++ src/leetcode/MoveZeros283.java
@@ -9,7 +9,7 @@ import utils.TestUtils;
  * time complexity: O(n)
  * space complexity: O(1)
  * Runtime: 0 ms, faster than 100.00% of Java online submissions for Move Zeroes.
- * Memory Usage: 37.5 MB, less than 100.00% of Java online submissions for Move Zeroes.
+ * Memory Usage: 38.2 MB, less than 81.12% of Java online submissions for Move Zeroes.
  */
 public class MoveZeros283 {
 
@@ -26,23 +26,25 @@ public class MoveZeros283 {
 
     System.out.println("v2:");
     int[] tc2a = {0, 1, 0, 3, 12};
-    moveZeroes(tc2a);
+    new V2().moveZeroes(tc2a);
 
     TestUtils.printArray(tc2a);
+
+    System.out.println("v3:");
+    int[] tc3a = {0, 1, 0, 3, 12};
+    new V3().moveZeroes(tc3a);
+
+    TestUtils.printArray(tc3a);
   }
 
   public void moveZeroes(int[] nums) {
-    int j = 0;
-
+    int zeros = 0;
     for (int i = 0; i < nums.length; i++) {
-      if (nums[i] != 0) {
-        if (i != j) {
-          int temp = nums[j];
-          nums[j] = nums[i];
-          nums[i] = temp;
-        }
-
-        j++;
+      if (nums[i] == 0) {
+        zeros++;
+      } else if (zeros > 0) {
+        nums[i - zeros] = nums[i];
+        nums[i] = 0;
       }
     }
   }
@@ -63,6 +65,24 @@ public class MoveZeros283 {
 
       while (j < nums.length) {
         nums[j++] = 0;
+      }
+    }
+  }
+
+  private static class V3 {
+    public void moveZeroes(int[] nums) {
+      int j = 0;
+
+      for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+          if (i != j) {
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+          }
+
+          j++;
+        }
       }
     }
   }
