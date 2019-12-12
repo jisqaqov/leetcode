9e62349ced8c8466e44970ce2b88cbe932dbe302
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-13 01:16:28 +0300

    binary search: 852. Peak Index in a Mountain Array;

--- src/leetcode/PeakIndexInMountainArray852.java
+++ src/leetcode/PeakIndexInMountainArray852.java
@@ -19,11 +19,6 @@ public class PeakIndexInMountainArray852 {
   private void test() {
     System.out.println(peakIndexInMountainArray(new int[]{0, 1, 0}));//1
     System.out.println(peakIndexInMountainArray(new int[]{0, 2, 1, 0}));//1
-
-    System.out.println("v2:");
-    V2 v2 = new V2();
-    System.out.println(v2.peakIndexInMountainArray(new int[]{0, 1, 0}));//1
-    System.out.println(v2.peakIndexInMountainArray(new int[]{0, 2, 1, 0}));//1
   }
 
   public int peakIndexInMountainArray(int[] a) {
@@ -41,29 +36,6 @@ public class PeakIndexInMountainArray852 {
     }
 
     return l;
-  }
-
-  private static class V2 {
-
-    public int peakIndexInMountainArray(int[] a) {
-      int l = 0;
-      int r = a.length - 1;
-
-      while (l <= r) {
-        int m = l + (r - l) / 2;
-
-        if (a[m] > a[m - 1] && a[m] > a[m + 1]) {
-          return m;
-        } else if (a[m] < a[m + 1]) {
-          l = m + 1;
-        } else {
-          r = m - 1;
-        }
-      }
-
-      return -1;
-    }
-
   }
 
 }
