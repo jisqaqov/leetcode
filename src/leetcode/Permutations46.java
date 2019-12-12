9d1dfca4528c1286c1c67c00b683b4f8d7d94b6e
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-12 12:10:58 +0300

    backtracking: resolved 46. Permutations;

--- src/leetcode/Permutations46.java
+++ src/leetcode/Permutations46.java
@@ -7,7 +7,7 @@ import java.util.List;
  * @author Jandos Iskakov
  * problem: 46. Permutations
  * algorithm: Backtracking
- * time complexity: O()
+ * time complexity: O((N!)
  * space complexity: O(N!)
  * Runtime: 1 ms, faster than 97.75% of Java online submissions for Permutations.
  * Memory Usage: 36.6 MB, less than 97.16% of Java online submissions for Permutations.
