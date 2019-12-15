c0ca0cf8d2f43fe1981596f14cd3a9ffb274f24b
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-15 10:38:34 +0300

    binary search: resolved 278. First Bad Version;

--- src/leetcode/FirstBadVersion278.java
+++ src/leetcode/FirstBadVersion278.java
@@ -12,22 +12,19 @@ package leetcode;
 public class FirstBadVersion278 {
 
   public int firstBadVersion(int n) {
-    int l = 1, r = n;
-    int version = -1;
+    int l = 1;
+    int r = n;
 
-    while (l <= r) {
+    while (l < r) {
       int mid = l + (r - l) / 2;
-
-      boolean isBad = isBadVersion(mid);
-      if (isBad) {
-        r = mid - 1;
-        version = mid;
+      if (isBadVersion(mid)) {
+        r = mid;
       } else {
         l = mid + 1;
       }
     }
 
-    return version;
+    return r;
   }
 
   private boolean isBadVersion(int n) {
