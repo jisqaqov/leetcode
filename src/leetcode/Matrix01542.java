532143db0378c552d107ff67bc65947226dc77ea
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-21 18:24:50 +0300

    refactoring;

--- src/leetcode/Matrix01542.java
+++ src/leetcode/Matrix01542.java
@@ -5,13 +5,17 @@ import java.util.Queue;
 import utils.TestUtils;
 
 /**
- * @author Jandos Iskakov problem: 542. 01 Matrix algorithm: BFS time complexity: O(N*M) space
- * complexity: O(N*M) Runtime: 19 ms, faster than 35.66% of Java online submissions Memory Usage: 54
- * MB, less than 95.83% of Java online submissions
+ * @author Jandos Iskakov
+ * problem: 542. 01 Matrix
+ * algorithm: BFS
+ * time complexity: O(N*M)
+ * space complexity: O(N*M)
+ * Runtime: 19 ms, faster than 35.66% of Java online submissions
+ * Memory Usage: 54 MB, less than 95.83% of Java online submissions
  */
 public class Matrix01542 {
 
-  private static int[][] DIMS = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+  private static int[][] DIRS = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
   public static void main(String[] args) {
     Matrix01542 problem = new Matrix01542();
@@ -19,19 +23,17 @@ public class Matrix01542 {
   }
 
   private void test() {
-    V2 v2 = new V2();
-
     int[][] tc1a = {{0, 0, 0},
       {0, 1, 0},
       {1, 1, 1}};
 
-    TestUtils.printArray(v2.updateMatrix(tc1a));
+    TestUtils.printArray(updateMatrix(tc1a));
 
     int[][] tc2a = {{0, 0, 0},
       {0, 1, 0},
       {0, 0, 0}};
 
-    TestUtils.printArray(v2.updateMatrix(tc2a));
+    TestUtils.printArray(updateMatrix(tc2a));
   }
 
   public int[][] updateMatrix(int[][] matrix) {
@@ -55,9 +57,9 @@ public class Matrix01542 {
     while (!queue.isEmpty()) {
       int[] node = queue.poll();
 
-      for (int[] dim : DIMS) {
-        int i = dim[0] + node[0];
-        int j = dim[1] + node[1];
+      for (int[] dir : DIRS) {
+        int i = dir[0] + node[0];
+        int j = dir[1] + node[1];
 
         if (i < 0 || j < 0 || i >= n || j >= m || matrix[i][j] == 0 || visited[i][j]) {
           continue;
@@ -70,74 +72,6 @@ public class Matrix01542 {
     }
 
     return dis;
-  }
-
-  private static class V2 {
-
-    public int[][] updateMatrix(int[][] matrix) {
-      int n = matrix.length;
-      int m = matrix[0].length;
-
-      boolean[][] visited = new boolean[n][m];
-
-      int[][] dis = new int[n][m];
-
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-          if (matrix[i][j] == 0) {
-            visited[i][j] = true;
-          } else {
-            dis[i][j] = Integer.MAX_VALUE;
-          }
-        }
-      }
-
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-          if (dis[i][j] == 0) {
-            dfs(i, j, matrix, dis, visited);
-          }
-        }
-      }
-
-      return dis;
-    }
-
-    private void dfs(int i, int j, int[][] matrix, int[][] dis, boolean[][] visited) {
-      visited[i][j] = true;
-
-      if (matrix[i][j] == 0) {
-        for (int[] dim : DIMS) {
-          int x = i + dim[0];
-          int y = j + dim[1];
-
-          if (x < 0 || y < 0 || x >= matrix.length || y >= matrix[0].length || matrix[x][y] == 0) {
-            continue;
-          }
-
-          visited[x][y] = true;
-          dis[x][y] = 1;
-        }
-      } else {
-        for (int[] dim : DIMS) {
-          int x = i + dim[0];
-          int y = j + dim[1];
-
-          if (x < 0 || y < 0 || x >= matrix.length || y >= matrix[0].length) {
-            continue;
-          }
-
-          if (!visited[x][y]) {
-            dfs(x, y, matrix, dis, visited);
-          }
-
-          if (dis[x][y] != Integer.MAX_VALUE) {
-            dis[i][j] = Math.min(dis[i][j], dis[x][y] + 1);
-          }
-        }
-      }
-    }
-
   }
 
 }
--- src/prep/Prep.java
+++ src/prep/Prep.java
@@ -1,9 +1,5 @@
 package prep;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Prep {
 
   public static void main(String[] args) {
@@ -12,51 +8,9 @@ public class Prep {
   }
 
   private void test() {
-    System.out.println(
-      suggestedProducts(new String[]{"mobile", "mouse", "moneypot", "monitor", "mousepad"},
-        "mouse"));
-    System.out.println(suggestedProducts(new String[]{"havana"}, "havana"));
-    System.out.println(
-      suggestedProducts(new String[]{"bags", "baggage", "banner", "box", "cloths"}, "bags"));
-    System.out.println(
-      suggestedProducts(new String[]{"havana"}, "tatiana"));
+
   }
 
-  public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-    Arrays.sort(products);
 
-    List<List<String>> output = new ArrayList<>();
-
-    for (int n = 1; n <= searchWord.length(); n++) {
-      String t = searchWord.substring(0, n);
-
-      int l = 0;
-      int r = products.length - 1;
-
-      while (l < r) {
-        int mid = l + (r - l) / 2;
-        String sub = products[mid].substring(0, Math.min(n, products[mid].length()));
-
-        if (sub.compareTo(t) < 0) {
-          l = mid + 1;
-        } else {
-          r = mid;
-        }
-      }
-
-      List<String> list = new ArrayList<>();
-      for (int k = l; k < l + 3 && k < products.length; k++) {
-        String sub = products[k].substring(0, Math.min(n, products[k].length()));
-
-        if (sub.equals(t)) {
-          list.add(products[k]);
-        }
-      }
-
-      output.add(list);
-    }
-
-    return output;
-  }
 
 }
