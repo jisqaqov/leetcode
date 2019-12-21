a6f25cfa44e6b4485d3e098328f49eedbc1552fc
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-21 11:38:21 +0300

    greedy: 1167. Minimum Cost to Connect Sticks;

--- src/amazon/MinimumCostToConnectSticks1167.java
+++ src/amazon/MinimumCostToConnectSticks1167.java
@@ -1,26 +1,31 @@
-package prep;
+package amazon;
 
 import java.util.PriorityQueue;
 
-public class Prep {
+/**
+ * @author Jandos Iskakov
+ * problem: 1167. Minimum Cost to Connect Sticks
+ * algorithm: Greedy
+ * time complexity: O(nlog(n))
+ * space complexity: O(n)
+ * Runtime: 41 ms, faster than 80.32% of Java online submissions
+ * Memory Usage: 39.6 MB, less than 100.00% of Java online submissions
+ */
+public class MinimumCostToConnectSticks1167 {
 
   public static void main(String[] args) {
-    Prep problem = new Prep();
+    MinimumCostToConnectSticks1167 problem = new MinimumCostToConnectSticks1167();
     problem.test();
   }
 
   private void test() {
     System.out.println(connectSticks(new int[]{2, 4, 3}));//14
     System.out.println(connectSticks(new int[]{1, 8, 3, 5}));//30
+    System.out.println(connectSticks(new int[]{8, 4, 6, 12}));//58
+    System.out.println(connectSticks(new int[]{20, 4, 8, 2}));//54
   }
 
   public int connectSticks(int[] sticks) {
-    if (sticks.length == 0) {
-      return 0;
-    } else if (sticks.length <= 1) {
-      return sticks[0];
-    }
-
     PriorityQueue<Integer> pq = new PriorityQueue<>();
 
     for (int len : sticks) {
@@ -29,7 +34,7 @@ public class Prep {
 
     int cost = 0;
 
-    while (!pq.isEmpty()) {
+    while (pq.size() >= 2) {
       int a = pq.poll();
       int b = pq.poll();
 
