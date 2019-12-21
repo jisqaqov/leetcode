789c23f5b38d7c109d481b535ba1d1cbf254be6c
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-21 18:19:11 +0300

    binary search: solved 1268. Search Suggestions System;

--- src/leetcode/SearchSuggestionsSystem1268.java
+++ src/leetcode/SearchSuggestionsSystem1268.java
@@ -10,8 +10,8 @@ import java.util.List;
  * algorithm: Binary Search
  * time complexity: O(log(|p|) * (|s| + |p|))
  * space complexity: O(3 * |s|)
- * Runtime: 13 ms, faster than 87.08% of Java online submissions
- * Memory Usage: 49.8 MB, less than 100.00% of Java online submissions
+ * Runtime: 11 ms, faster than 91.73% of Java online submissions
+ * Memory Usage: 50 MB, less than 100.00% of Java online submissions
  */
 public class SearchSuggestionsSystem1268 {
 
@@ -44,7 +44,9 @@ public class SearchSuggestionsSystem1268 {
 
       while (l < r) {
         int mid = l + (r - l) / 2;
-        String sub = products[mid].substring(0, Math.min(n, products[mid].length()));
+
+        int len = Math.min(n, products[mid].length());
+        String sub = products[mid].substring(0, len);
 
         if (sub.compareTo(t) < 0) {
           l = mid + 1;
@@ -55,9 +57,10 @@ public class SearchSuggestionsSystem1268 {
 
       List<String> list = new ArrayList<>();
       for (int k = l; k < l + 3 && k < products.length; k++) {
-        String sub = products[k].substring(0, Math.min(n, products[k].length()));
+        int len = Math.min(n, products[k].length());
+        String sub = products[k].substring(0, len);
 
-        if (sub.equals(t)) {
+        if (!sub.equals(t)) {
           list.add(products[k]);
         }
       }
