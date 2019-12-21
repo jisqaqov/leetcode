471d0769b8c2b26825902aef60d8d13d4c0793b2
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-21 13:05:02 +0300

    bfs: resolved 994. Rotting Oranges;

--- src/leetcode/RottingOranges994.java
+++ src/leetcode/RottingOranges994.java
@@ -6,91 +6,70 @@ import java.util.Queue;
 /**
  * @author Jandos Iskakov
  * problem: 994. Rotting Oranges
- * algorithm: BFS,
+ * algorithm: BFS
  * time complexity: O(n*m)
  * space complexity: O(n*m)
+ * Runtime: 1 ms, faster than 100.00% of Java online submissions
+ * Memory Usage: 39.1 MB, less than 81.25% of Java online submissions
  */
 public class RottingOranges994 {
 
-    public static void main(String[] args) {
-        RottingOranges994 solution = new RottingOranges994();
-        solution.test();
+  private static int[][] DIRS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+  public static void main(String[] args) {
+    RottingOranges994 solution = new RottingOranges994();
+    solution.test();
+  }
+
+  public void test() {
+    System.out.println(orangesRotting(new int[][]{{2, 1, 1}, {1, 1, 0}, {0, 1, 1}}));
+    System.out.println(orangesRotting(new int[][]{{2, 1, 1}, {0, 1, 1}, {1, 0, 1}}));
+    System.out.println(orangesRotting(new int[][]{{0, 2}}));
+  }
+
+  public int orangesRotting(int[][] grid) {
+    int n = grid.length;
+    int m = grid[0].length;
+
+    Queue<int[]> queue = new LinkedList<>();
+
+    int freshes = 0;
+
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] == 2) {
+          queue.add(new int[]{i, j});
+        } else if (grid[i][j] == 1) {
+          freshes++;
+        }
+      }
     }
 
-    public void test() {
-        int[][] tc1a = {{2,1,1}, {1,1,0}, {0,1,1}};
-        int[][] tc2a = {{2,1,1}, {0,1,1}, {1,0,1}};
-        int[][] tc3a = {{0,2}};
+    int mins = 0;
 
-        System.out.println(orangesRotting(tc1a));
-        System.out.println(orangesRotting(tc2a));
-        System.out.println(orangesRotting(tc3a));
+    for (; !queue.isEmpty() && freshes > 0; mins++) {
+      int size = queue.size();
+
+      for (int k = 0; k < size; k++) {
+        int[] node = queue.poll();
+
+        for (int[] dir : DIRS) {
+          int i = dir[0] + node[0];
+          int j = dir[1] + node[1];
+
+          if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != 1) {
+            continue;
+          }
+
+          freshes--;
+
+          grid[i][j] = 2;
+          queue.add(new int[]{i, j});
+        }
+      }
     }
 
-    public int orangesRotting(int[][] grid) {
-        if (grid.length == 0) {
-            return 0;
-        }
-
-        int n = grid.length, m = grid[0].length;
-
-        Queue<int[]> queue = new LinkedList<>();
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 2) {
-                    queue.add(new int[] {i, j});
-                }
-            }
-        }
-
-        int minutes = 0;
-        boolean[][] visited = new boolean[n][m];
-
-        while (!queue.isEmpty()) {
-            Queue<int[]> rottens = new LinkedList<>();
-
-            while (!queue.isEmpty()) {
-                int[] coords = queue.poll();
-                int i = coords[0], j = coords[1];
-
-                if (i < n - 1 && !visited[i + 1][j] && grid[i + 1][j] == 1) {
-                    rottens.add(new int[] {i + 1, j});
-                    visited[i + 1][j] = true;
-                }
-
-                if (i > 0 && !visited[i - 1][j] && grid[i - 1][j] == 1) {
-                    rottens.add(new int[] {i - 1, j});
-                    visited[i - 1][j] = true;
-                }
-
-                if (j > 0 && !visited[i][j - 1] && grid[i][j - 1] == 1) {
-                    rottens.add(new int[] {i, j - 1});
-                    visited[i][j - 1] = true;
-                }
-
-                if (j < m - 1 && !visited[i][j + 1] && grid[i][j + 1] == 1) {
-                    rottens.add(new int[] {i, j + 1});
-                    visited[i][j + 1] = true;
-                }
-            }
-
-            if (!rottens.isEmpty()) {
-                minutes++;
-            }
-
-            queue = rottens;
-        }
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (!visited[i][j] && grid[i][j] == 1) {
-                    return -1;
-                }
-            }
-        }
-
-        return minutes;
-    }
+    return freshes > 0? -1: mins;
+  }
 
 }
