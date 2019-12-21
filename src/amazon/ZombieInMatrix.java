033e2925a92d40abd59581b663b23f6e7e29170c
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-21 13:21:02 +0300

    solved Amazon | OA 2019 | 🧟‍♀️ Zombie in Matrix;

---
+++ src/amazon/ZombieInMatrix.java
@@ -0,0 +1,102 @@
+package amazon;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Jandos Iskakov
+ * Given a 2D grid, each cell is either a zombie 1 or a human 0.
+ * Zombies can turn adjacent (up/down/left/right) human beings into
+ * zombies every hour. Find out how many hours does it take to infect all humans?
+ *
+ * Example:
+ *
+ * Input:
+ * [[0, 1, 1, 0, 1],
+ *  [0, 1, 0, 1, 0],
+ *  [0, 0, 0, 0, 1],
+ *  [0, 1, 0, 0, 0]]
+ *
+ * Output: 2
+ *
+ * Explanation:
+ * At the end of the 1st hour, the status of the grid:
+ * [[1, 1, 1, 1, 1],
+ *  [1, 1, 1, 1, 1],
+ *  [0, 1, 0, 1, 1],
+ *  [1, 1, 1, 0, 1]]
+ *
+ * At the end of the 2nd hour, the status of the grid:
+ * [[1, 1, 1, 1, 1],
+ *  [1, 1, 1, 1, 1],
+ *  [1, 1, 1, 1, 1],
+ *  [1, 1, 1, 1, 1]]
+ *
+ *
+ * algorithm: BFS
+ * time complexity: O(n*m)
+ * space complexity: O(n*m)
+ */
+public class ZombieInMatrix {
+
+  private static int[][] DIRS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+  public static void main(String[] args) {
+    ZombieInMatrix solution = new ZombieInMatrix();
+    solution.test();
+  }
+
+  public void test() {
+    System.out.println(zombieInMatrix(
+      new int[][]{{0, 1, 1, 0, 1},
+        {0, 1, 0, 1, 0},
+        {0, 0, 0, 0, 1},
+        {0, 1, 0, 0, 0}}));//2
+  }
+
+  public int zombieInMatrix(int[][] grid) {
+    int n = grid.length;
+    int m = grid[0].length;
+
+    Queue<int[]> queue = new LinkedList<>();
+
+    int humans = 0;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (grid[i][j] == 1) {
+          queue.add(new int[]{i, j});
+        } else if (grid[i][j] == 0) {
+          humans++;
+        }
+      }
+    }
+
+    int hours = 0;
+
+    for (; !queue.isEmpty() && humans > 0; hours++) {
+      int size = queue.size();
+
+      for (int k = 0; k < size; k++) {
+        int[] node = queue.poll();
+
+        for (int[] dir : DIRS) {
+          int i = dir[0] + node[0];
+          int j = dir[1] + node[1];
+
+          if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != 0) {
+            continue;
+          }
+
+          humans--;
+
+          grid[i][j] = 1;
+          queue.add(new int[]{i, j});
+        }
+      }
+    }
+
+    return hours;
+  }
+
+}
