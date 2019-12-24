8fc34761cff26156ddf355b72abb5a4e63ef3bca
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-24 13:42:39 +0300

    reservoir sampling: solved 398. Random Pick Index;

--- src/leetcode/RandomPickIndex398.java
+++ src/leetcode/RandomPickIndex398.java
@@ -1,24 +1,31 @@
-package prep;
+package leetcode;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
-public class Prep {
+/**
+ * @author Jandos Iskakov
+ * problem: 398. Random Pick Index
+ * algorithm: Reservoir Sampling
+ * time complexity: O(N)
+ * space complexity: O(1)
+ * Runtime: 60 ms, faster than 73.15% of Java online submissions
+ * Memory Usage: 56.8 MB, less than 5.88% of Java online submissions
+ */
+public class RandomPickIndex398 {
 
   public static void main(String[] args) {
-    Prep problem = new Prep();
+    RandomPickIndex398 problem = new RandomPickIndex398();
     problem.test();
   }
 
   private void test() {
-    //TODO:
-
+    Solution solution = new Solution(new int[]{1, 2, 3, 3, 3});
+    System.out.println(solution.pick(3));
+    System.out.println(solution.pick(1));
   }
 
   private class Solution {
+
     private Random random = new Random();
     private int[] nums;
 
@@ -45,7 +52,5 @@ public class Prep {
       return index;
     }
   }
-
-
 
 }
