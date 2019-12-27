295abf7b945be811d01258e53ecdaa4b8a6f0673
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-27 10:49:35 +0300

    tree: resolved 863. All Nodes Distance K in Binary Tree;

--- src/leetcode/AllNodesDistanceKInBinaryTree863.java
+++ src/leetcode/AllNodesDistanceKInBinaryTree863.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 /**
  * @author Jandos Iskakov
- * problem: 863. All Nodes Distance K in Binary Tree time complexity: O(N)
- * space complexity: O(1)
- * Runtime: 1 ms, faster than 100.00% of Java online submissions for All Nodes Distance K in Binary Tree.
- * Memory Usage: 36.9 MB, less than 100.00% of Java online submissions for All Nodes Distance K in Binary Tree.
+ * problem: 863. All Nodes Distance K in Binary Tree
+ * time complexity: O(N)
+ * space complexity: O(N)
+ * Runtime: 1 ms, faster than 100.00% of Java online submissions
+ * Memory Usage: 36.9 MB, less than 100.00% of Java online submissions
  */
 public class AllNodesDistanceKInBinaryTree863 {
 
@@ -17,14 +18,14 @@ public class AllNodesDistanceKInBinaryTree863 {
     solution.test();
   }
 
-  public void test() {
+  private void test() {
     TreeNode root = new TreeNode(3);
     TreeNode node5 = new TreeNode(5);
-    TreeNode node1 = new TreeNode(1);
     TreeNode node6 = new TreeNode(6);
     TreeNode node2 = new TreeNode(2);
-    TreeNode node7 = new TreeNode(7);
     TreeNode node4 = new TreeNode(4);
+    TreeNode node7 = new TreeNode(7);
+    TreeNode node1 = new TreeNode(1);
     TreeNode node0 = new TreeNode(0);
     TreeNode node8 = new TreeNode(8);
 
@@ -37,77 +38,59 @@ public class AllNodesDistanceKInBinaryTree863 {
     node1.left = node0;
     node1.right = node8;
 
-    System.out.println(distanceK(root, node5, 3));
+    System.out.println(distanceK(root, node5, 2));
   }
 
   public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-    List<Integer> values = new ArrayList<>();
+    List<Integer> list = new ArrayList<>();
 
-    traverse(root, target, k, values);
-    find(target, k, 0, values);
+    search(root, target, k, list);
 
-    return values;
+    return list;
   }
 
-  private int traverse(TreeNode node, TreeNode target, int k, List<Integer> values) {
-    if (node == null) {
+  private int search(TreeNode root, TreeNode target, int k, List<Integer> list) {
+    if (root == null) {
       return -1;
     }
 
-    if (node == target) {
-      return 0;
-    }
+    int d = -1;
 
-    int disLeft = traverse(node.left, target, k, values);
-    if (disLeft >= 0) {
-      disLeft++;
-    }
+    if (root == target) {
+      d = 0;
+      collect(root, 0, list, k);
+    } else {
+      int dl = search(root.left, target, k, list);
+      int dr = search(root.right, target, k, list);
 
-    if (disLeft >= 0 && disLeft <= k) {
-      if (disLeft == k) {
-        values.add(node.val);
-      } else {
-        find(node.right, k, disLeft + 1, values);
+      if (dl >= 0 || dr >= 0) {
+        d = Math.max(dl, dr) + 1;
       }
 
-      return disLeft;
-    }
-
-    int disRight = traverse(node.right, target, k, values);
-    if (disRight >= 0) {
-      disRight++;
-    }
-
-    if (disRight >= 0 && disRight <= k) {
-      if (disRight == k) {
-        values.add(node.val);
-      } else {
-        find(node.left, k, disRight + 1, values);
+      if (d == k) {
+        list.add(root.val);
+      } else if (dl >= 0) {
+        collect(root.right, d + 1, list, k);
+      } else if (dr >= 0) {
+        collect(root.left, d + 1, list, k);
       }
-
-      return disRight;
     }
 
-    return -2;
+    return d;
   }
 
-  private void find(TreeNode node, int k, int path, List<Integer> values) {
-    if (node == null) {
+  private void collect(TreeNode root, int d, List<Integer> list, int k) {
+    if (root == null || d > k) {
       return;
     }
 
-    if (path == k) {
-      values.add(node.val);
+    if (d == k) {
+      list.add(root.val);
       return;
     }
 
-    if (node.left != null) {
-      find(node.left, k, path + 1, values);
-    }
-
-    if (node.right != null) {
-      find(node.right, k, path + 1, values);
-    }
+    collect(root.left, d + 1, list, k);
+    collect(root.right, d + 1, list, k);
   }
 
   /**
--- src/prep/Prep.java
+++ src/prep/Prep.java
@@ -1,8 +1,7 @@
 package prep;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Prep {
 
@@ -12,72 +11,47 @@ public class Prep {
   }
 
   private void test() {
-    char[] tasks = {'A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'C', 'C', 'D'};
-    //System.out.println(leastInterval(tasks, 6));//29
-    System.out.println(leastInterval(tasks, 2));//13
+    TreeNode root = new TreeNode(3);
+    TreeNode node5 = new TreeNode(5);
+    TreeNode node6 = new TreeNode(6);
+    TreeNode node2 = new TreeNode(2);
+    TreeNode node4 = new TreeNode(4);
+    TreeNode node7 = new TreeNode(7);
+    TreeNode node1 = new TreeNode(1);
+    TreeNode node0 = new TreeNode(0);
+    TreeNode node8 = new TreeNode(8);
+
+    root.left = node5;
+    root.right = node1;
+    node5.left = node6;
+    node5.right = node2;
+    node2.left = node7;
+    node2.right = node4;
+    node1.left = node0;
+    node1.right = node8;
+
+    System.out.println(distanceK(root, node5, 2));
   }
 
-  public int leastInterval(char[] tasks, int n) {
-    Map<Character, Integer> freq = new HashMap<>();
-    for (char task : tasks) {
-      freq.put(task, freq.getOrDefault(task, 0) + 1);
-    }
+  public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+    
 
-    int t = 0;
-
-    Task[] list = new Task[freq.size()];
-    for (char task : freq.keySet()) {
-      list[t++] = new Task(task, freq.get(task));
-    }
-
-    int intervals = 0;
-
-    Arrays.sort(list, (t1, t2) -> t2.count - t1.count);
-
-    int index = 0;
-    int start = 0;
-    int end = list.length - 1;
-    int count = tasks.length;
-
-    while (count > 0) {
-      count--;
-      intervals++;
-
-      if (list[index].pos > 0 && intervals - list[index].pos - 1 < n) {
-        intervals += n - (intervals - list[index].pos - 1);
-      }
-
-      list[index].count--;
-      list[index].pos = intervals;
-
-      if (list[end].count == 0) {
-        end--;
-      }
-
-      if (list[start].count == 0) {
-        start++;
-      }
-
-      if (intervals - list[start].pos >= n || index == end) {
-        index = start;
-      } else {
-        index++;
-      }
-    }
-
-    return intervals;
+    return null;
   }
 
-  private static class Task {
+  /**
+   * Definition for a binary tree node.
+   */
+  public class TreeNode {
 
-    private int pos = -1;
-    private char id;
-    private int count;
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-    public Task(char id, int count) {
-      this.id = id;
-      this.count = count;
+    TreeNode(int x) {
+      val = x;
     }
   }
+
 
 }
