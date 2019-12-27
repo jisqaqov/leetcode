e45a42ce02077fc48826d6e6296347a98058dff4
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-27 19:30:13 +0300

    math: resolved 43. Multiply Strings;

--- src/leetcode/MultiplyStrings43.java
+++ src/leetcode/MultiplyStrings43.java
@@ -9,8 +9,8 @@ import java.util.List;
  * algorithm: Math, String
  * time complexity: O(N*M)
  * space complexity: O(1)
- * Runtime: 12 ms, faster than 20.66% of Java online submissions
- * Memory Usage: 37 MB, less than 100.00% of Java online submissions
+ * Runtime: 4 ms, faster than 86.37% of Java online submissions
+ * Memory Usage: 36 MB, less than 100.00% of Java online submissions
  */
 public class MultiplyStrings43 {
 
@@ -29,56 +29,88 @@ public class MultiplyStrings43 {
   }
 
   public String multiply(String num1, String num2) {
-    if (num1.equals("0") || num2.equals("0")) {
-      return "0";
-    }
+    int n = num1.length();
+    int m = num2.length();
 
-    List<Integer> list = new ArrayList<>();
+    int[] nums = new int[n + m];
 
-    int carry = 0;
-    int index = 0;
+    for (int i = n - 1; i >= 0; i--) {
+      for (int j = m - 1; j >= 0; j--) {
+        int a = num1.charAt(i) - '0';
+        int b = num2.charAt(j) - '0';
 
-    for (int i = num1.length() - 1; i >= 0; i--) {
-      int k = index;
+        int p = a * b + nums[i + j + 1];
 
-      for (int j = num2.length() - 1; j >= 0; j--) {
-        int a = Character.getNumericValue(num1.charAt(i));
-        int b = Character.getNumericValue(num2.charAt(j));
-
-        if (k >= list.size()) {
-          list.add(0);
-        }
-
-        int c = a * b + carry + list.get(k);
-
-        carry = c / 10;
-        c = c % 10;
-
-        list.set(k, c);
-
-        k++;
+        nums[i + j + 1] = p % 10;
+        nums[i + j] += p / 10;
       }
-
-      if (carry > 0) {
-
-        if (k >= list.size()) {
-          list.add(0);
-        }
-
-        list.set(k, carry);
-
-        carry = 0;
-      }
-
-      index++;
     }
 
     StringBuilder output = new StringBuilder();
-    for (int i = list.size() - 1; i >= 0; i--) {
-      output.append(list.get(i));
+
+    for (int num : nums) {
+      if (output.length() != 0 || num != 0) {
+        output.append(num);
+      }
     }
 
-    return output.toString();
+    return output.length() == 0? "0": output.toString();
+  }
+
+  private static class V2 {
+
+    public String multiply(String num1, String num2) {
+      if (num1.equals("0") || num2.equals("0")) {
+        return "0";
+      }
+
+      List<Integer> list = new ArrayList<>();
+
+      int carry = 0;
+      int index = 0;
+
+      for (int i = num1.length() - 1; i >= 0; i--) {
+        int k = index;
+
+        for (int j = num2.length() - 1; j >= 0; j--) {
+          int a = Character.getNumericValue(num1.charAt(i));
+          int b = Character.getNumericValue(num2.charAt(j));
+
+          if (k >= list.size()) {
+            list.add(0);
+          }
+
+          int c = a * b + carry + list.get(k);
+
+          carry = c / 10;
+          c = c % 10;
+
+          list.set(k, c);
+
+          k++;
+        }
+
+        if (carry > 0) {
+
+          if (k >= list.size()) {
+            list.add(0);
+          }
+
+          list.set(k, carry);
+
+          carry = 0;
+        }
+
+        index++;
+      }
+
+      StringBuilder output = new StringBuilder();
+      for (int i = list.size() - 1; i >= 0; i--) {
+        output.append(list.get(i));
+      }
+
+      return output.toString();
+    }
   }
 
 }
