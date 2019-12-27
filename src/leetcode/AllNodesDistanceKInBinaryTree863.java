a13a028fd9331219b5e3cff1a7e8bc05164be901
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-27 14:37:27 +0300

    tree: resolved 863. All Nodes Distance K in Binary Tree;

--- src/leetcode/AllNodesDistanceKInBinaryTree863.java
+++ src/leetcode/AllNodesDistanceKInBinaryTree863.java
@@ -1,15 +1,21 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * @author Jandos Iskakov
  * problem: 863. All Nodes Distance K in Binary Tree
  * time complexity: O(N)
  * space complexity: O(N)
- * Runtime: 1 ms, faster than 100.00% of Java online submissions
- * Memory Usage: 36.9 MB, less than 100.00% of Java online submissions
+ * Runtime: 3 ms, faster than 62.38% of Java online submissions
+ * Memory Usage: 36.2 MB, less than 100.00% of Java online submissions
  */
 public class AllNodesDistanceKInBinaryTree863 {
 
@@ -56,51 +62,110 @@ public class AllNodesDistanceKInBinaryTree863 {
   }
 
   public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+
+    Map<TreeNode, TreeNode> parents = new HashMap<>();
+    parents.put(root, null);
+
+    while (!queue.isEmpty()) {
+      TreeNode node = queue.poll();
+
+      if (node.left != null) {
+        parents.put(node.left, node);
+        queue.add(node.left);
+      }
+
+      if (node.right != null) {
+        parents.put(node.right, node);
+        queue.add(node.right);
+      }
+    }
+
+    Set<TreeNode> visited = new HashSet<>();
+
+    queue.add(target);
+    visited.add(target);
+
+    while (k > 0 && !queue.isEmpty()) {
+      for (int sz = queue.size(); sz > 0; sz--) {
+        TreeNode node = queue.poll();
+
+        if (node.left != null && visited.add(node.left)) {
+          queue.add(node.left);
+        }
+
+        if (node.right != null && visited.add(node.right)) {
+          queue.add(node.right);
+        }
+
+        TreeNode parent = parents.get(node);
+        if (parent != null && visited.add(parent)) {
+          queue.add(parent);
+        }
+      }
+
+      k--;
+    }
+
     List<Integer> output = new ArrayList<>();
 
-    postorder(root, target, k, output);
+    while (!queue.isEmpty()) {
+      output.add(queue.poll().val);
+    }
 
     return output;
   }
 
-  private int postorder(TreeNode root, TreeNode target, int k, List<Integer> output) {
-    int dis = -1;
+  private static class V2 {
 
-    if (root == target) {
-      dis = 0;
-      collect(root, 0, output, k);
-    } else if (root != null) {
-      int dl = postorder(root.left, target, k, output);
-      int dr = postorder(root.right, target, k, output);
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+      List<Integer> output = new ArrayList<>();
 
-      if (dl >= 0 || dr >= 0) {
-        dis = Math.max(dl, dr) + 1;
+      postorder(root, target, k, output);
+
+      return output;
+    }
+
+    private int postorder(TreeNode root, TreeNode target, int k, List<Integer> output) {
+      int dis = -1;
+
+      if (root == target) {
+        dis = 0;
+        collect(root, 0, output, k);
+      } else if (root != null) {
+        int dl = postorder(root.left, target, k, output);
+        int dr = postorder(root.right, target, k, output);
+
+        if (dl >= 0 || dr >= 0) {
+          dis = Math.max(dl, dr) + 1;
+        }
+
+        if (dis == k) {
+          output.add(root.val);
+        } else if (dl >= 0) {
+          collect(root.right, dis + 1, output, k);
+        } else if (dr >= 0) {
+          collect(root.left, dis + 1, output, k);
+        }
+      }
+
+      return dis;
+    }
+
+    private void collect(TreeNode root, int dis, List<Integer> output, int k) {
+      if (root == null || dis > k) {
+        return;
       }
 
       if (dis == k) {
         output.add(root.val);
-      } else if (dl >= 0) {
-        collect(root.right, dis + 1, output, k);
-      } else if (dr >= 0) {
-        collect(root.left, dis + 1, output, k);
+        return;
       }
+
+      collect(root.left, dis + 1, output, k);
+      collect(root.right, dis + 1, output, k);
     }
-
-    return dis;
-  }
-
-  private void collect(TreeNode root, int dis, List<Integer> output, int k) {
-    if (root == null || dis > k) {
-      return;
-    }
-
-    if (dis == k) {
-      output.add(root.val);
-      return;
-    }
-
-    collect(root.left, dis + 1, output, k);
-    collect(root.right, dis + 1, output, k);
   }
 
   /**
