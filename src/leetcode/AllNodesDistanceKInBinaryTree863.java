eb62cd83f269a2c02e3fc8617752fc18822f3176
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-27 13:51:55 +0300

    tree: resolved 863. All Nodes Distance K in Binary Tree;

--- src/leetcode/AllNodesDistanceKInBinaryTree863.java
+++ src/leetcode/AllNodesDistanceKInBinaryTree863.java
@@ -1,12 +1,7 @@
 package leetcode;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Queue;
 
 /**
  * @author Jandos Iskakov
@@ -48,8 +43,6 @@ public class AllNodesDistanceKInBinaryTree863 {
   }
 
   private void test2() {
-    IterativeVersion problem = new IterativeVersion();
-
     TreeNode node3 = new TreeNode(3);
     TreeNode node2 = new TreeNode(2);
     TreeNode node1 = new TreeNode(1);
@@ -59,7 +52,7 @@ public class AllNodesDistanceKInBinaryTree863 {
     node0.right = node1;
     node1.left = node3;
 
-    System.out.println(problem.distanceK(node0, node3, 3));
+    System.out.println(distanceK(node0, node3, 3));
   }
 
   public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
@@ -112,96 +105,6 @@ public class AllNodesDistanceKInBinaryTree863 {
 
     collect(root.left, dis + 1, output, k);
     collect(root.right, dis + 1, output, k);
-  }
-
-  private static class IterativeVersion {
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-      List<Integer> output = new ArrayList<>();
-
-      collect(target, 0, output, k);
-
-      Map<TreeNode, TreeNode> map = fillParents(root, target);
-
-      int dis = 1;
-
-      TreeNode node = target;
-
-      while (dis <= k && map.get(node) != null) {
-        TreeNode parent = map.get(node);
-
-        if (dis < k) {
-          if (parent.right == node) {
-            collect(parent.left, dis + 1, output, k);
-          } else {
-            collect(parent.right, dis + 1, output, k);
-          }
-        } else {
-          output.add(parent.val);
-        }
-
-        dis++;
-        node = parent;
-      }
-
-      return output;
-    }
-
-    private Map<TreeNode, TreeNode> fillParents(TreeNode root, TreeNode target) {
-      Map<TreeNode, TreeNode> map = new HashMap<>();
-      map.put(root, null);
-
-      Queue<TreeNode> queue = new LinkedList<>();
-      queue.add(root);
-
-      while (!queue.isEmpty()) {
-        TreeNode node = queue.poll();
-
-        if (node == target) {
-          break;
-        }
-
-        if (node.left != null) {
-          map.put(node.left, node);
-          queue.add(node.left);
-        }
-
-        if (node.right != null) {
-          map.put(node.right, node);
-          queue.add(node.right);
-        }
-      }
-
-      return map;
-    }
-
-    private void collect(TreeNode root, int dis, List<Integer> output, int k) {
-      if (root == null || dis > k) {
-        return;
-      }
-
-      Queue<TreeNode> queue = new ArrayDeque<>();
-      queue.add(root);
-
-      while (!queue.isEmpty() && dis <= k) {
-        for (int sz = queue.size(); sz > 0; sz--) {
-          TreeNode node = queue.poll();
-
-          if (dis == k) {
-            output.add(node.val);
-          } else {
-            if (node.left != null) {
-              queue.add(node.left);
-            }
-
-            if (node.right != null) {
-              queue.add(node.right);
-            }
-          }
-        }
-
-        dis++;
-      }
-    }
   }
 
   /**
