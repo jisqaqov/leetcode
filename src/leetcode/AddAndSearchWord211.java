92bfd225ba767c6c63d50c6bf8a1edfcae846584
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-29 12:35:24 +0300

    trie: resolved 211. Add and Search Word - Data structure design;

--- src/leetcode/AddAndSearchWord211.java
+++ src/leetcode/AddAndSearchWord211.java
@@ -7,78 +7,99 @@ import java.util.Map;
  * @author Jandos Iskakov
  * problem: 211. Add and Search Word - Data structure design
  * algorithm: Backtracking, Design, Trie
+ * Runtime: 50 ms, faster than 65.98% of Java online submissions
+ * Memory Usage: 55.2 MB, less than 81.82% of Java online submissions
  */
 public class AddAndSearchWord211 {
-    class WordDictionary {
-        private TrieNode root;
 
-        /** Initialize your data structure here. */
-        public WordDictionary() {
-            this.root = new TrieNode();
-        }
+  public static void main(String[] args) {
+    AddAndSearchWord211 problem = new AddAndSearchWord211();
+    problem.test();
+  }
 
-        /** Adds a word into the data structure. */
-        public void addWord(String word) {
-            TrieNode node = root;
+  private void test() {
+    WordDictionary wd = new WordDictionary();
+    wd.addWord("bad");
+    wd.addWord("dad");
+    wd.addWord("mad");
 
-            for (int i = 0; i < word.length(); i++) {
-                char ch = word.charAt(i);
+    System.out.println(wd.search("m.d"));//true
+    System.out.println(wd.search("bad"));//true
+    System.out.println(wd.search("dad"));//true
+    System.out.println(wd.search("baz"));//false
+    System.out.println(wd.search("baddy"));//false
+  }
 
-                if (node.map.containsKey(ch)) {
-                    node = node.map.get(ch);
-                } else {
-                    TrieNode newNode = new TrieNode();
-                    node.map.put(ch, newNode);
-                    node = newNode;
-                }
-            }
+  class WordDictionary {
 
-            node.isWordEnd = true;
-        }
+    private TrieNode root;
 
-        /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
-        public boolean search(String word) {
-            return search(word, 0, root);
-        }
-
-        private boolean search(String word, int i, TrieNode node) {
-            if (i == word.length())
-                return node.isWordEnd;
-
-            char ch = word.charAt(i);
-
-            if (ch == '.') {
-                for (Character character : node.map.keySet()) {
-                    if (search(word, i + 1, node.map.get(character)))
-                        return true;
-                }
-
-                return false;
-            } else {
-                if (node.map.containsKey(ch)) {
-                    return search(word, i + 1, node.map.get(ch));
-                }
-            }
-
-            return false;
-        }
-
-        class TrieNode {
-            Map<Character, TrieNode> map = new HashMap<>();
-            boolean isWordEnd = false;
-
-            TrieNode() {
-            }
-        }
-
+    /**
+     * Initialize your data structure here.
+     */
+    public WordDictionary() {
+      root = new TrieNode();
     }
 
     /**
-     * Your WordDictionary object will be instantiated and called as such:
-     * WordDictionary obj = new WordDictionary();
-     * obj.addWord(word);
-     * boolean param_2 = obj.search(word);
+     * Adds a word into the data structure.
      */
+    public void addWord(String word) {
+      TrieNode node = root;
+
+      for (int i = 0; i < word.length(); i++) {
+        char ch = word.charAt(i);
+        if (node.map.containsKey(ch)) {
+          node = node.map.get(ch);
+        } else {
+          node.map.put(ch, new TrieNode());
+          node = node.map.get(ch);
+        }
+      }
+
+      node.isWord = true;
+    }
+
+    /**
+     * Returns if the word is in the data structure. A word could contain the dot character '.' to
+     * represent any one letter.
+     */
+    public boolean search(String word) {
+      return search(word, 0, root);
+    }
+
+    private boolean search(String word, int index, TrieNode root) {
+      TrieNode node = root;
+
+      for (; index < word.length(); index++) {
+        char ch = word.charAt(index);
+
+        if (ch != '.') {
+          if (!node.map.containsKey(ch)) {
+            return false;
+          }
+
+          node = node.map.get(ch);
+        } else {
+          for (TrieNode adj : node.map.values()) {
+            if (search(word, index + 1, adj)) {
+              return true;
+            }
+          }
+
+          return false;
+        }
+      }
+
+      return node.isWord && index == word.length();
+    }
     
-    
+  }
+
+  private class TrieNode {
+
+    private Map<Character, TrieNode> map = new HashMap<>();
+    private boolean isWord = false;
+  }
+
 }
--- src/prep/Prep.java
+++ src/prep/Prep.java
@@ -1,13 +1,6 @@
 package prep;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import utils.TestUtils;
-
 public class Prep {
-
-  private static final int INF = Integer.MAX_VALUE;
-  private static final int[][] DIRS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
   public static void main(String[] args) {
     Prep problem = new Prep();
@@ -15,46 +8,7 @@ public class Prep {
   }
 
   private void test() {
-    int[][] tc1grid = {{INF, -1, 0, INF},
-      {INF, INF, INF, -1},
-      {INF, -1, INF, -1},
-      {0, -1, INF, INF}};
 
-    wallsAndGates(tc1grid);
-    TestUtils.printArray(tc1grid);
-  }
-
-  public void wallsAndGates(int[][] grid) {
-    Queue<int[]> queue = new LinkedList<>();
-
-    for (int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[i].length; j++) {
-        if (grid[i][j] == 0) {
-          queue.add(new int[]{i, j});
-        }
-      }
-    }
-
-    while (!queue.isEmpty()) {
-      for (int sz = queue.size(); sz > 0; sz--) {
-        int[] node = queue.poll();
-
-        for (int[] dir : DIRS) {
-          int i = dir[0] + node[0];
-          int j = dir[1] + node[1];
-
-          if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == -1) {
-            continue;
-          }
-
-          int dis = grid[node[0]][node[1]] + 1;
-          if (grid[i][j] > dis) {
-            grid[i][j] = dis;
-            queue.add(new int[]{i, j});
-          }
-        }
-      }
-    }
   }
 
 
