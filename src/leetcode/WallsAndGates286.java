d7993248817a28fe917ed3c42a74dec0ff974677
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-29 10:38:30 +0300

    bfs: resolved 286. Walls and Gates;

--- src/leetcode/WallsAndGates286.java
+++ src/leetcode/WallsAndGates286.java
@@ -1,111 +1,67 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
+import utils.TestUtils;
 
 /**
- * @author Jandos Iskakov problem: 286. Walls and Gates algorithm: BFS time complexity: O(N*M) space
- * complexity: O(N*M)
+ * @author Jandos Iskakov
+ * problem: 286. Walls and Gates
+ * algorithm: BFS
+ * time complexity: O(N*M)
+ * space complexity: O(N*M)
+ * Runtime: 10 ms, faster than 31.40% of Java online submissions
+ * Memory Usage: 41.2 MB, less than 100.00% of Java online submissions
  */
 public class WallsAndGates286 {
 
+  private static final int INF = Integer.MAX_VALUE;
+  private static final int[][] DIRS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
   public static void main(String[] args) {
-    WallsAndGates286 solution = new WallsAndGates286();
-    solution.test();
+    WallsAndGates286 problem = new WallsAndGates286();
+    problem.test();
   }
 
-  public void test() {
-    int[][] tc1rooms = {{Integer.MAX_VALUE, -1, 0, Integer.MAX_VALUE},
-      {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, -1},
-      {Integer.MAX_VALUE, -1, Integer.MAX_VALUE, -1},
-      {0, -1, Integer.MAX_VALUE, Integer.MAX_VALUE}
-    };
+  private void test() {
+    int[][] tc1grid = {{INF, -1, 0, INF},
+      {INF, INF, INF, -1},
+      {INF, -1, INF, -1},
+      {0, -1, INF, INF}};
 
-    wallsAndGates(tc1rooms);
+    wallsAndGates(tc1grid);
 
-    int[][] tc1solution = {{3, -1, 0, 1},
-      {2, 2, 1, -1},
-      {1, -1, 2, -1},
-      {0, -1, 3, 4}};
+    TestUtils.printArray(tc1grid);
+  }
 
-    print(tc1rooms);
+  public void wallsAndGates(int[][] grid) {
+    Queue<int[]> queue = new LinkedList<>();
 
-    for (int i = 0; i < tc1rooms.length; i++) {
-      for (int j = 0; j < tc1rooms[i].length; j++) {
-        if (tc1rooms[i][j] != tc1solution[i][j]) {
-          System.out.println("i = " + i + ", j = " + j);
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] == 0) {
+          queue.add(new int[]{i, j});
         }
       }
     }
-  }
-
-  private void print(int[][] rooms) {
-    for (int i = 0; i < rooms.length; i++) {
-      for (int j = 0; j < rooms[i].length; j++) {
-        System.out.print(rooms[i][j] + " ");
-      }
-      System.out.println();
-    }
-  }
-
-  public void wallsAndGates(int[][] rooms) {
-    int n = rooms.length;
-    if (n == 0) {
-      return;
-    }
-
-    int m = rooms[0].length;
-
-    Queue<Integer[]> queue = new LinkedList<>();
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (rooms[i][j] == 0) {
-          queue.add(new Integer[]{i, j});
-        }
-      }
-    }
-
-    Map<Integer, Set<Integer>> visited = new HashMap<>();
 
     while (!queue.isEmpty()) {
-      Integer[] node = queue.poll();
+      for (int sz = queue.size(); sz > 0; sz--) {
+        int[] node = queue.poll();
 
-      int si = node[0];
-      int sj = node[1];
+        for (int[] dir : DIRS) {
+          int i = dir[0] + node[0];
+          int j = dir[1] + node[1];
 
-      visited.putIfAbsent(si, new HashSet<>());
-      visited.get(si).add(sj);
+          if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == -1) {
+            continue;
+          }
 
-      List<Integer[]> adjList = new ArrayList<>();
-
-      adjList.add(new Integer[]{si + 1, sj});
-      adjList.add(new Integer[]{si - 1, sj});
-      adjList.add(new Integer[]{si, sj + 1});
-      adjList.add(new Integer[]{si, sj - 1});
-
-      for (Integer[] adj : adjList) {
-        int ti = adj[0];
-        int tj = adj[1];
-
-        if (ti < 0 || tj < 0 || ti >= n || tj >= m) {
-          continue;
-        }
-
-        if (rooms[ti][tj] <= 0 || (visited.containsKey(ti) && visited.get(ti).contains(tj))) {
-          continue;
-        }
-
-        queue.add(new Integer[]{ti, tj});
-
-        if (rooms[si][sj] < rooms[ti][tj]) {
-          rooms[ti][tj] = Math.min(rooms[si][sj] + 1, rooms[ti][tj]);
+          int dis = grid[node[0]][node[1]] + 1;
+          if (grid[i][j] > dis) {
+            grid[i][j] = dis;
+            queue.add(new int[]{i, j});
+          }
         }
       }
     }
--- src/prep/Prep.java
+++ src/prep/Prep.java
@@ -1,6 +1,13 @@
 package prep;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import utils.TestUtils;
+
 public class Prep {
+
+  private static final int INF = Integer.MAX_VALUE;
+  private static final int[][] DIRS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
   public static void main(String[] args) {
     Prep problem = new Prep();
@@ -8,30 +15,47 @@ public class Prep {
   }
 
   private void test() {
-    System.out.println(reverse(123));//321
-    System.out.println(reverse(-123));//-321
-    System.out.println(reverse(120));//21
-    System.out.println(reverse(1534236469));//0
-    System.out.println(reverse(-2147483648));//0
+    int[][] tc1grid = {{INF, -1, 0, INF},
+      {INF, INF, INF, -1},
+      {INF, -1, INF, -1},
+      {0, -1, INF, INF}};
+
+    wallsAndGates(tc1grid);
+    TestUtils.printArray(tc1grid);
   }
 
-  public int reverse(int x) {
-    int output = 0;
+  public void wallsAndGates(int[][] grid) {
+    Queue<int[]> queue = new LinkedList<>();
 
-    while (x != 0) {
-      int d = x % 10;
-
-      if ((d > 0 && output > (Integer.MAX_VALUE - d) / 10) ||
-          (d < 0 && output < (Integer.MIN_VALUE - d) / 10)) {
-        return 0;
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] == 0) {
+          queue.add(new int[]{i, j});
+        }
       }
-
-      output = output * 10 + d;
-
-      x /= 10;
     }
 
-    return output;
+    while (!queue.isEmpty()) {
+      for (int sz = queue.size(); sz > 0; sz--) {
+        int[] node = queue.poll();
+
+        for (int[] dir : DIRS) {
+          int i = dir[0] + node[0];
+          int j = dir[1] + node[1];
+
+          if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == -1) {
+            continue;
+          }
+
+          int dis = grid[node[0]][node[1]] + 1;
+          if (grid[i][j] > dis) {
+            grid[i][j] = dis;
+            queue.add(new int[]{i, j});
+          }
+        }
+      }
+    }
   }
+
 
 }
