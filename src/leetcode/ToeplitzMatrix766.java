c1d16ca1717ea626185a6cdae4ecedfa182a208e
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-30 11:54:47 +0300

    array: solved 766. Toeplitz Matrix;

--- src/leetcode/ToeplitzMatrix766.java
+++ src/leetcode/ToeplitzMatrix766.java
@@ -27,15 +27,8 @@ public class ToeplitzMatrix766 {
   }
 
   public boolean isToeplitzMatrix(int[][] matrix) {
-    if (matrix.length == 0) {
-      return true;
-    }
-
-    int n = matrix.length;
-    int m = matrix[0].length;
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
         if (i > 0 && j > 0 && matrix[i][j] != matrix[i - 1][j - 1]) {
           return false;
         }
