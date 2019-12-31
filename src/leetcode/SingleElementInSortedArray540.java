eb304b049582afacc927b82918bdadc3380ec456
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-12-31 11:37:22 +0300

    array: solved 540. Single Element in a Sorted Array;

--- src/leetcode/SingleElementInSortedArray540.java
+++ src/leetcode/SingleElementInSortedArray540.java
@@ -17,6 +17,7 @@ public class SingleElementInSortedArray540 {
   }
 
   private void test() {
+    System.out.println(singleNonDuplicate(new int[]{0}));//0
     System.out.println(singleNonDuplicate(new int[]{1, 2, 2}));//1
     System.out.println(singleNonDuplicate(new int[]{1, 1, 2, 3, 3, 4, 4, 8, 8}));//2
     System.out.println(singleNonDuplicate(new int[]{3, 3, 7, 7, 10, 11, 11}));//10
@@ -27,27 +28,60 @@ public class SingleElementInSortedArray540 {
     int l = 0;
     int r = nums.length - 1;
 
-    while (l <= r) {
-      int mid = l + (r - l) / 2;
+    if (nums.length == 1) {
+      return nums[0];
+    }
 
-      if (mid == 0 || mid == nums.length - 1 ||
-        (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])) {
-        return nums[mid];
+    while (l + 2 < r) {
+      int m = l + (r - l) / 2;
+
+      if (nums[m] == nums[m - 1]) {
+        m--;
       }
 
-      if (nums[mid] == nums[mid - 1]) {
-        mid--;
-      }
+      int len = r - m + 1;
 
-      int len = r - mid + 1;
       if (len % 2 > 0) {
-        l = mid + 2;
+        l = m;
       } else {
-        r = mid - 1;
+        r = m - 1;
       }
     }
 
-    return -1;
+    if (nums[l] != nums[l + 1]) {
+      return nums[l];
+    }
+
+    return nums[r];
+  }
+
+  private static class V2 {
+    public int singleNonDuplicate(int[] nums) {
+      int n = nums.length;
+
+      int l = 0, r = n - 1;
+
+      while (l <= r) {
+        int m = l + (r - l) / 2;
+
+        if (m == 0 || m == n - 1 || (nums[m] != nums[m - 1] && nums[m] != nums[m + 1])) {
+          return nums[m];
+        }
+
+        if (nums[m] == nums[m - 1]) {
+          m--;
+        }
+
+        int len = r - m + 1;
+        if (len % 2 > 0) {
+          l = m + 2;
+        } else {
+          r = m - 1;
+        }
+      }
+
+      return -1;
+    }
   }
 
 }
