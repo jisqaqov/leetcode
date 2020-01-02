d05853ea188d26687dd0739bf033720f948a3c1b
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-02 12:30:44 +0300

    facebook: solved Facebook | Onsite | Matrix Antidiagonal Traverse;

--- src/facebook/MatrixAntidiagonalTraverse.java
+++ src/facebook/MatrixAntidiagonalTraverse.java
@@ -1,12 +1,54 @@
-package prep;
+package facebook;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Prep {
+/**
+ * @author Jandos Iskakov
+ * algorithm: Array
+ * https://leetcode.com/discuss/interview-question/346342/Facebook-or-Onsite-or-Matrix-Antidiagonal-Traverse
+ * Given a matrix, return all elements of the matrix in antidiagonal order as shown in the below image.
+ * Example 1:
+ *
+ * Input:
+ * [[12, 7, 21, 31, 11],
+ *  [45, -2, 14, 27, 19],
+ *  [-3, 15, 36, 71, 26],
+ *  [4, -13, 55, 34, 15]]
+ *
+ * Output:
+ * [
+ * [12],
+ * [7, 45],
+ * [21, -2, -3],
+ * [31, 14, 15, 4],
+ * [11, 27, 36, -13],
+ * [19, 71, 55],
+ * [26, 34],
+ * [15]
+ * ]
+ * Example 2:
+ *
+ * Input:
+ * [[1, 2, 3],
+ *  [4, 5, 6],
+ *  [7, 8, 9]]
+ *
+ * Output:
+ * [
+ * [1],
+ * [2, 4],
+ * [3, 5, 7],
+ * [6, 8],
+ * [9]
+ * ]
+ * time complexity: O(N*M)
+ * space complexity: O(N*M)
+ */
+public class MatrixAntidiagonalTraverse {
 
   public static void main(String[] args) {
-    Prep problem = new Prep();
+    MatrixAntidiagonalTraverse problem = new MatrixAntidiagonalTraverse();
     problem.test();
   }
 
