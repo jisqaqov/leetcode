f3adb0f34daf9134bf3f3f13564f085028614932
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-02 23:07:59 +0300

    Facebook | Phone screen | Mth smallest & Left most index of 1;

--- src/facebook/KthSmallestInSortedMatrix.java
+++ src/facebook/KthSmallestInSortedMatrix.java
@@ -3,6 +3,8 @@ package facebook;
 import java.util.PriorityQueue;
 
 /**
+ * @author Jandos Iskakov
+ * algorithm: Heap
  * Given k sorted arrays of possibly different sizes, find m-th smallest value in the merged array.
  *
  * Example 1:
@@ -11,6 +13,9 @@ import java.util.PriorityQueue;
  * Output: 4
  * Explanation: The merged array would be [0, 1, 2, 3, 4, 6, 9, 10, 11].
  * The 5-th smallest element in this merged array is 4.
+ *
+ * time complexity: O(r*c*log(k))
+ * space complexity: O(k)
  */
 public class KthSmallestInSortedMatrix {
 
@@ -29,20 +34,46 @@ public class KthSmallestInSortedMatrix {
     System.out.println(kthSmallest(tc2a, 2));//2
   }
 
-  public int kthSmallest(int[][] a, int k) {
-    PriorityQueue<Integer> pq = new PriorityQueue<>((t1, t2) -> t2 - t1);
+  public int kthSmallest(int[][] matrix, int k) {
+    PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> matrix[x[0]][x[1]] - matrix[y[0]][y[1]]);
 
-    for (int i = 0; i < a.length; i++) {
-      for (int j = 0; j < a[i].length; j++) {
-        pq.add(a[i][j]);
-
-        if (pq.size() > k) {
-          pq.poll();
-        }
-      }
+    for (int i = 0; i < matrix.length; i++) {
+      pq.add(new int[] {i, 0});
     }
 
-    return pq.poll();
+    int min = -1;
+
+    while (k > 0) {
+      int[] rc = pq.poll();
+
+      if (rc[1] < matrix[rc[0]].length - 1) {
+        pq.add(new int[] {rc[0], rc[1] + 1});
+      }
+
+      min = matrix[rc[0]][rc[1]];
+
+      k--;
+    }
+
+    return min;
+  }
+
+  private static class V2 {
+    public int kthSmallest(int[][] a, int k) {
+      PriorityQueue<Integer> pq = new PriorityQueue<>((t1, t2) -> t2 - t1);
+
+      for (int i = 0; i < a.length; i++) {
+        for (int j = 0; j < a[i].length; j++) {
+          pq.add(a[i][j]);
+
+          if (pq.size() > k) {
+            pq.poll();
+          }
+        }
+      }
+
+      return pq.poll();
+    }
   }
 
 }
