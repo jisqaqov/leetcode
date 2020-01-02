069c1e8118b13959fce98624218ef3f5d2148224
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-02 17:02:25 +0300

    stack: resolved 341. Flatten Nested List Iterator;

--- src/leetcode/FlattenNestedListIterator341.java
+++ src/leetcode/FlattenNestedListIterator341.java
@@ -3,9 +3,7 @@ package leetcode;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * @author Jandos Iskakov
@@ -13,8 +11,8 @@ import java.util.Queue;
  * algorithm: Stack, Design
  * time complexity: O(N)
  * space complexity: O(N)
- * Runtime: 4 ms, faster than 49.96% of Java online submissions
- * Memory Usage: 37 MB, less than 100.00% of Java online submissions
+ * Runtime: 2 ms, faster than 100.00% of Java online submissions
+ * Memory Usage: 36.9 MB, less than 100.00% of Java online submissions
  */
 public class FlattenNestedListIterator341 {
 
@@ -40,80 +38,19 @@ public class FlattenNestedListIterator341 {
     List<NestedInteger> getList();
   }
 
-  public class NestedIterator implements Iterator<Integer> {
-    private Queue<Integer> queue = new LinkedList<>();
+  public class NestedIteratorV2 implements Iterator<Integer> {
 
-    public NestedIterator(List<NestedInteger> nestedList) {
-      flatten(nestedList);
-    }
+    private Deque<NestedInteger> stack = new ArrayDeque<>();
 
-    private void flatten(List<NestedInteger> nestedList) {
-      Deque<List<NestedInteger>> stack = new ArrayDeque<>();
-      Deque<Integer> indexes = new ArrayDeque<>();
-
-      stack.push(nestedList);
-      indexes.push(0);
-
-      while (!stack.isEmpty()) {
-        List<NestedInteger> list = stack.peek();
-        Integer pos = indexes.peek();
-
-        for (; pos < list.size(); pos++) {
-          NestedInteger ni = list.get(pos);
-
-          if (ni.isInteger()) {
-            queue.add(ni.getInteger());
-          } else {
-            indexes.pop();
-            indexes.push(pos);
-
-            stack.push(ni.getList());
-            indexes.push(0);
-            
-            break;
-          }
-        }
-
-        if (pos == list.size()) {
-          stack.pop();
-          indexes.pop();
-
-          if (!stack.isEmpty()) {
-            indexes.push(indexes.poll() + 1);
-          }
-        }
+    public NestedIteratorV2(List<NestedInteger> nestedList) {
+      for (int i = nestedList.size() - 1; i >= 0; i--) {
+        stack.push(nestedList.get(i));
       }
     }
 
     @Override
     public Integer next() {
-      return queue.poll();
-    }
-
-    @Override
-    public boolean hasNext() {
-      return !queue.isEmpty();
-    }
-  }
-
-  public class NestedIteratorV2 implements Iterator<Integer> {
-
-    private Deque<List<NestedInteger>> stack = new ArrayDeque<>();
-    private Deque<Integer> indexes = new ArrayDeque<>();
-
-    public NestedIteratorV2(List<NestedInteger> nestedList) {
-      stack.push(nestedList);
-      indexes.push(-1);
-    }
-
-    @Override
-    public Integer next() {
-      List<NestedInteger> list = stack.peek();
-
-      int index = indexes.poll() + 1;
-      indexes.push(index);
-
-      return list.get(index).getInteger();
+      return stack.pollFirst().getInteger();
     }
 
     @Override
@@ -123,24 +60,16 @@ public class FlattenNestedListIterator341 {
       }
 
       while (!stack.isEmpty()) {
-        List<NestedInteger> list = stack.peek();
-        int index = indexes.peek();
+        NestedInteger node = stack.peekFirst();
 
-        if (index == list.size() - 1) {
-          stack.pop();
-          indexes.pop();
+        if (!node.isInteger()) {
+          List<NestedInteger> list = stack.pollFirst().getList();
 
-          if (!stack.isEmpty()) {
-            indexes.push(indexes.pop() + 1);
+          for (int i = list.size() - 1; i >= 0; i--) {
+            stack.push(list.get(i));
           }
         } else {
-          NestedInteger ni = list.get(index + 1);
-          if (ni.isInteger()) {
-            return true;
-          } else {
-            stack.push(ni.getList());
-            indexes.push(-1);
-          }
+          return true;
         }
       }
 
