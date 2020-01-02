b88d96ad7d4baa05361ffaaa99343f9ba8863465
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-02 20:30:41 +0300

    Facebook | Leftmost column index of 1;

--- src/facebook/LeftMostIndex.java
+++ src/facebook/LeftMostIndex.java
@@ -1,9 +1,28 @@
-package prep;
+package facebook;
 
-public class Prep {
+/**
+ * @author Jandos Iskakov
+ * algorithm: Two Pointers
+ * https://leetcode.com/discuss/interview-question/341247/Facebook-or-Leftmost-column-index-of-1
+ * In a binary matrix (all elements are 0 and 1), every row is sorted in ascending order
+ * (0 to the left of 1). Find the leftmost column index with a 1 in it.
+ *
+ * Example 1:
+ * Input: [[0, 0, 0, 1], [0, 0, 1, 1], [0, 1, 1, 1], [0, 0, 0, 0]]
+ * Output: 1
+ *
+ * Example 2:
+ * Input: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+ * Output: -1
+ * Expected solution better than O(r * c).
+ *
+ * time complexity: O(N + M)
+ * space complexity: O(1)
+ */
+public class LeftMostIndex {
 
   public static void main(String[] args) {
-    Prep problem = new Prep();
+    LeftMostIndex problem = new LeftMostIndex();
     problem.test();
   }
 
