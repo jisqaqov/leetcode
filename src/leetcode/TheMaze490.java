c841593d4763ab15d3a1f28ef681c68fa145f720
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-04 18:50:26 +0300

    dfs: resolved 490. The Maze;

--- src/leetcode/TheMaze490.java
+++ src/leetcode/TheMaze490.java
@@ -1,184 +1,74 @@
 package leetcode;
 
-import java.util.*;
-
 /**
  * @author Jandos Iskakov
  * problem: 490. The Maze
  * algorithm: DFS
  * time complexity: O(V+E)
  * space complexity: O(V+E)
+ * Runtime: 2 ms, faster than 96.64% of Java online submissions
+ * Memory Usage: 45.9 MB, less than 91.67% of Java online submissions
  */
 public class TheMaze490 {
 
-    public static void main(String[] args) {
-        TheMaze490 solution = new TheMaze490();
-        solution.test();
+  public static void main(String[] args) {
+    TheMaze490 problem = new TheMaze490();
+    problem.test();
+  }
+
+  private void test() {
+    int[][] tc1a = {
+      {0, 0, 1, 0, 0},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 1, 0},
+      {1, 1, 0, 1, 1},
+      {0, 0, 0, 0, 0}};
+
+    System.out.println(hasPath(tc1a, new int[]{0, 4}, new int[]{4, 4}));
+    System.out.println(hasPath(tc1a, new int[]{0, 4}, new int[]{3, 2}));
+    System.out.println(hasPath(tc1a, new int[]{0, 4}, new int[]{1, 2}));
+  }
+
+  public boolean hasPath(int[][] maze, int[] start, int[] dst) {
+    boolean[][] visited = new boolean[maze.length][maze[0].length];
+
+    return hasPath(start[0], start[1], 0, 0, maze, dst, visited);
+  }
+
+  private boolean hasPath(int row, int col, int dirRow, int dirCol,
+    int[][] grid, int[] dst, boolean[][] visited) {
+
+    int n = grid.length;
+    int m = grid[0].length;
+
+    if (row < 0 || col < 0 || row >= n || col >= m || grid[row][col] == 1) {
+      return false;
     }
 
-    public void test() {
-        int[][] tc1maze = {{0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0},
-                {1, 1, 0, 1, 1},
-                {0, 0, 0, 0, 0}};
+    if (dirCol != dirRow) {
+      while (row >= 0 && col >= 0 && row < n && col < m && grid[row][col] == 0) {
+        row += dirRow;
+        col += dirCol;
+      }
 
-        int[] tc1start = {0, 4};
-        int[] tc1dest = {4, 4};
-
-        System.out.println(hasPath(tc1maze, tc1start, tc1dest));
-
-        int[][] tc2maze = {{0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0},
-                {1, 1, 0, 1, 1},
-                {0, 0, 0, 0, 0}};
-
-        int[] tc2start = {0, 4};
-        int[] tc2dest = {3, 2};
-
-        System.out.println(hasPath(tc2maze, tc2start, tc2dest));
-
-        int[][] tc3maze = {{0,0,0,0,1,0,0},
-                {0,0,1,0,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,1},
-                {0,1,0,0,0,0,0},
-                {0,0,0,1,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,1,0,0,0,1},
-                {0,0,0,0,1,0,0}};
-
-        int[] tc3start = {0, 0};
-        int[] tc3dest = {8, 6};
-
-        System.out.println(hasPath(tc3maze, tc3start, tc3dest));
+      row -= dirRow;
+      col -= dirCol;
     }
 
-    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        Set<Node> visited = new HashSet<>();
-
-        Node startNode = new Node();
-        startNode.x = start[0];
-        startNode.y = start[1];
-
-        visited.add(startNode);
-
-        for (Node adjNode : getAdjNodes(startNode)) {
-            if (hasPath(adjNode, maze, destination, visited)) {
-                return true;
-            }
-        }
-
-        return false;
+    if (visited[row][col]) {
+      return false;
     }
 
-    private boolean hasPath(Node currNode, int[][] maze, int[] dest, Set<Node> visited) {
-        int n = maze.length, m = maze[0].length;
-
-        if (!isInMaze(currNode.x, currNode.y, n, m)) {
-            return false;
-        }
-
-        if (visited.contains(currNode)) {
-            return false;
-        }
-
-        if (maze[currNode.x][currNode.y] == 1) {
-            return false;
-        }
-
-        visited.add(currNode);
-
-        Node nextNode = getNextNode(currNode);
-
-        boolean isWall = !isInMaze(nextNode.x, nextNode.y, n, m) ||
-                maze[nextNode.x][nextNode.y] == 1;
-
-        if (isWall) {
-            if (currNode.x == dest[0] && currNode.y == dest[1]) {
-                return true;
-            }
-
-            for (Node adjNode : getAdjNodes(currNode)) {
-                if (!isInMaze(adjNode.x, adjNode.y, n, m)) {
-                    continue;
-                }
-
-                if (hasPath(adjNode, maze, dest, visited)) {
-                    return true;
-                }
-            }
-        } else if (isInMaze(nextNode.x, nextNode.y, n, m)) {
-            return hasPath(nextNode, maze, dest, visited);
-        }
-
-        return false;
+    if (dst[0] == row && dst[1] == col) {
+      return true;
     }
 
-    private List<Node> getAdjNodes(Node node) {
-        List<Node> adjList = new ArrayList<>();
+    visited[row][col] = true;
 
-        Node leftNode = new Node();
-        leftNode.x = node.x - 1;
-        leftNode.y = node.y;
-        leftNode.stepX = -1;
-        adjList.add(leftNode);
-
-        Node rightNode = new Node();
-        rightNode.x = node.x + 1;
-        rightNode.y = node.y;
-        rightNode.stepX = 1;
-        adjList.add(rightNode);
-
-        Node upNode = new Node();
-        upNode.x = node.x;
-        upNode.y = node.y - 1;
-        upNode.stepY = -1;
-        adjList.add(upNode);
-
-        Node downNode = new Node();
-        downNode.x = node.x;
-        downNode.y = node.y + 1;
-        downNode.stepY = 1;
-        adjList.add(downNode);
-
-        return adjList;
-    }
-
-    private Node getNextNode(Node node) {
-        Node adjNode = new Node();
-        adjNode.x = node.x + node.stepX;
-        adjNode.y = node.y + node.stepY;
-        adjNode.stepX = node.stepX;
-        adjNode.stepY = node.stepY;
-        return adjNode;
-    }
-
-    private boolean isInMaze(int x, int y, int n, int m) {
-        return x < n && x >= 0 && y >= 0 && y < m;
-    }
-
-    private static class Node {
-        int x, y;
-        int stepX = 0, stepY = 0;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Node node = (Node) o;
-            return x == node.x &&
-                    y == node.y &&
-                    stepX == node.stepX &&
-                    stepY == node.stepY;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y, stepX, stepY);
-        }
-    }
-
+    return hasPath(row, col, -1, 0, grid, dst, visited) ||
+      hasPath(row, col, 1, 0, grid, dst, visited) ||
+      hasPath(row, col, 0, -1, grid, dst, visited) ||
+      hasPath(row, col, 0, 1, grid, dst, visited);
+  }
 
 }
--- src/prep/Prep.java
+++ src/prep/Prep.java
@@ -1,7 +1,5 @@
 package prep;
 
-import java.util.Arrays;
-
 public class Prep {
 
   public static void main(String[] args) {
@@ -10,13 +8,59 @@ public class Prep {
   }
 
   private void test() {
-    //TODO:
+    int[][] tc1a = {
+      {0, 0, 1, 0, 0},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 1, 0},
+      {1, 1, 0, 1, 1},
+      {0, 0, 0, 0, 0}};
 
+    System.out.println(hasPath(tc1a, new int[]{0, 4}, new int[]{4, 4}));
+    System.out.println(hasPath(tc1a, new int[]{0, 4}, new int[]{3, 2}));
+    System.out.println(hasPath(tc1a, new int[]{0, 4}, new int[]{1, 2}));
   }
 
+  public boolean hasPath(int[][] maze, int[] start, int[] dst) {
+    boolean[][] visited = new boolean[maze.length][maze[0].length];
 
+    return hasPath(start[0], start[1], 0, 0, maze, dst, visited);
+  }
 
+  private boolean hasPath(int row, int col, int dirRow, int dirCol,
+    int[][] grid, int[] dst, boolean[][] visited) {
 
+    int n = grid.length;
+    int m = grid[0].length;
+
+    if (row < 0 || col < 0 || row >= n || col >= m || grid[row][col] == 1) {
+      return false;
+    }
+
+    if (dirCol != dirRow) {
+      while (row >= 0 && col >= 0 && row < n && col < m && grid[row][col] == 0) {
+        row += dirRow;
+        col += dirCol;
+      }
+
+      row -= dirRow;
+      col -= dirCol;
+    }
+
+    if (visited[row][col]) {
+      return false;
+    }
+
+    if (dst[0] == row && dst[1] == col) {
+      return true;
+    }
+
+    visited[row][col] = true;
+
+    return hasPath(row, col, -1, 0, grid, dst, visited) ||
+      hasPath(row, col, 1, 0, grid, dst, visited) ||
+      hasPath(row, col, 0, -1, grid, dst, visited) ||
+      hasPath(row, col, 0, 1, grid, dst, visited);
+  }
 
 
 }
