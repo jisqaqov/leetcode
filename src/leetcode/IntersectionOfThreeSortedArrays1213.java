9088b9f983566f1f619e1a710457948e48c37b49
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-10 11:13:35 +0300

    two pointers: 1213. Intersection of Three Sorted Arrays;

--- src/leetcode/IntersectionOfThreeSortedArrays1213.java
+++ src/leetcode/IntersectionOfThreeSortedArrays1213.java
@@ -53,6 +53,32 @@ public class IntersectionOfThreeSortedArrays1213 {
     return output;
   }
 
+  private static class V3 {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+      List<Integer> output = new ArrayList<>();
+
+      int i = 0, j = 0, k = 0;
+
+      while (i < arr1.length && j < arr2.length && k < arr3.length) {
+        if (arr1[i] < arr2[j] || arr1[i] < arr3[k]) {
+          i++;
+        } else if (arr2[j] < arr1[i] || arr2[j] < arr3[k]) {
+          j++;
+        } else if (arr3[k] < arr1[i] || arr3[k] < arr2[j]) {
+          k++;
+        } else {
+          output.add(arr1[i]);
+
+          i++;
+          j++;
+          k++;
+        }
+      }
+
+      return output;
+    }
+  }
+
   private static class V2 {
 
     public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
