07f46fe4269120f42a0ff4e530619e2e39b06bfd
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-10 11:04:49 +0300

    two pointers: 1213. Intersection of Three Sorted Arrays;

---
+++ src/leetcode/IntersectionOfThreeSortedArrays1213.java
@@ -0,0 +1,88 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Jandos Iskakov
+ * problem: 1213. Intersection of Three Sorted Arrays
+ * algorithm: Two Pointers
+ * time complexity: O(N+M+K)
+ * space complexity: O(MIN(N, M, K))
+ * Runtime: 1 ms, faster than 100.00% of Java online submissions
+ * Memory Usage: 39.4 MB, less than 100.00% of Java online submissions
+ */
+public class IntersectionOfThreeSortedArrays1213 {
+
+  public static void main(String[] args) {
+    IntersectionOfThreeSortedArrays1213 problem = new IntersectionOfThreeSortedArrays1213();
+    problem.test();
+  }
+
+  private void test() {
+    System.out.println(new V2().arraysIntersection(
+      new int[]{1, 2, 3, 4, 5},
+      new int[]{1, 2, 5, 7, 9},
+      new int[]{1, 3, 4, 5, 8}
+    ));
+  }
+
+  public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+    List<Integer> output = new ArrayList<>();
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < arr1.length && j < arr2.length && k < arr3.length) {
+      if (arr1[i] < arr2[j] || arr1[i] < arr3[k]) {
+        i++;
+      } else if (arr2[j] < arr1[i] || arr2[j] < arr3[k]) {
+        j++;
+      } else if (arr3[k] < arr1[i] || arr3[k] < arr2[j]) {
+        k++;
+      } else {
+        output.add(arr1[i]);
+
+        i++;
+        j++;
+        k++;
+      }
+    }
+
+    return output;
+  }
+
+  private static class V2 {
+
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+      Map<Integer, Integer> map1 = new HashMap<>();
+      for (int i = 0; i < arr1.length; i++) {
+        map1.put(arr1[i], map1.getOrDefault(arr1[i], 0) + 1);
+      }
+
+      Map<Integer, Integer> map2 = new HashMap<>();
+      for (int i = 0; i < arr2.length; i++) {
+        map2.put(arr2[i], map2.getOrDefault(arr2[i], 0) + 1);
+      }
+
+      List<Integer> output = new ArrayList<>();
+
+      for (int num : arr3) {
+        if (map1.getOrDefault(num, 0) == 0 || map2.getOrDefault(num, 0) == 0) {
+          continue;
+        }
+
+        output.add(num);
+
+        map1.put(num, map1.get(num) - 1);
+        map2.put(num, map2.get(num) - 1);
+      }
+
+      return output;
+    }
+
+
+  }
+
+}
--- src/prep/Prep.java
+++ src/prep/Prep.java
@@ -1,7 +1,6 @@
 package prep;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Prep {
@@ -12,97 +11,32 @@ public class Prep {
   }
 
   private void test() {
-    //<b>abc</b>xyz<b>123</b>
-    System.out.println(addBoldTag("abcxyz123", new String[]{"abc", "123"}));
-    //"<b>aaabbc</b>c"
-    System.out.println(addBoldTag("aaabbcc", new String[]{"aaa", "aab", "bc"}));
-    // "<b>a</b>b<b>c</b>d<b>e</b>f"
-    System.out.println(addBoldTag("abcdef", new String[] {"a","c","e","g"}));
+    System.out.println(arraysIntersection(
+      new int[]{1, 2, 3, 4, 5},
+      new int[]{1, 2, 5, 7, 9},
+      new int[]{1, 3, 4, 5, 8}
+    ));
   }
 
-  public String addBoldTag(String s, String[] dict) {
-    List<int[]> indexes = new ArrayList<>();
-    for (String t : dict) {
-      indexes.addAll(findIndexes(s, t));
-    }
+  public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+    List<Integer> output = new ArrayList<>();
 
-    List<int[]> merged = merge(indexes);
+    int i = 0, j = 0, k = 0;
 
-    StringBuilder output = new StringBuilder();
-
-    int idx = 0;
-
-    for (int i = 0; i < s.length(); i++) {
-      char ch = s.charAt(i);
-
-      String prefix = "";
-      String suffix = "";
-
-      if (idx < merged.size()) {
-        if (i == merged.get(idx)[0]) {
-          prefix = "<b>";
-        }
-
-        if (i == merged.get(idx)[1]) {
-          suffix = "</b>";
-          idx++;
-        }
-      }
-
-      if (!prefix.isEmpty()) {
-        output.append(prefix);
-      }
-
-      output.append(ch);
-
-      if (!suffix.isEmpty()) {
-        output.append(suffix);
-      }
-    }
-
-    return output.toString();
-  }
-
-  private List<int[]> merge(List<int[]> indexes) {
-    if (indexes.isEmpty()) {
-      return new ArrayList<>();
-    }
-
-    indexes.sort(Comparator.comparingInt(t -> t[0]));
-
-    List<int[]> output = new ArrayList<>();
-    output.add(indexes.get(0));
-
-    for (int i = 1; i < indexes.size(); i++) {
-      int[] index = indexes.get(i);
-      int[] index2 = output.get(output.size() - 1);
-
-      if (index[0] - index2[1] <= 1) {
-        index2[1] = Math.max(index[1], index2[1]);
+    while (i < arr1.length && j < arr2.length && k < arr3.length) {
+      if (arr1[i] < arr2[j] || arr1[i] < arr3[k]) {
+        i++;
+      } else if (arr2[j] < arr1[i] || arr2[j] < arr3[k]) {
+        j++;
+      } else if (arr3[k] < arr1[i] || arr3[k] < arr2[j]) {
+        k++;
       } else {
-        output.add(new int[]{index[0], index[1]});
+        output.add(arr1[i]);
+
+        i++;
+        j++;
+        k++;
       }
-    }
-
-    return output;
-  }
-
-  private List<int[]> findIndexes(String s, String t) {
-    List<Integer> list = new ArrayList<>();
-
-    int index = 0;
-
-    while (index != -1) {
-      index = s.indexOf(t, index);
-      if (index != -1) {
-        list.add(index);
-        index++;
-      }
-    }
-
-    List<int[]> output = new ArrayList<>();
-    for (int idx : list) {
-      output.add(new int[]{idx, idx + t.length() - 1});
     }
 
     return output;
