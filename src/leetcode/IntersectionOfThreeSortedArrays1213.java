f13ffa0492192403a6e496ff4876df7d67e3059e
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-10 11:11:54 +0300

    two pointers: 1213. Intersection of Three Sorted Arrays;

--- src/leetcode/IntersectionOfThreeSortedArrays1213.java
+++ src/leetcode/IntersectionOfThreeSortedArrays1213.java
@@ -22,7 +22,7 @@ public class IntersectionOfThreeSortedArrays1213 {
   }
 
   private void test() {
-    System.out.println(new V2().arraysIntersection(
+    System.out.println(arraysIntersection(
       new int[]{1, 2, 3, 4, 5},
       new int[]{1, 2, 5, 7, 9},
       new int[]{1, 3, 4, 5, 8}
@@ -35,17 +35,17 @@ public class IntersectionOfThreeSortedArrays1213 {
     int i = 0, j = 0, k = 0;
 
     while (i < arr1.length && j < arr2.length && k < arr3.length) {
-      if (arr1[i] < arr2[j] || arr1[i] < arr3[k]) {
-        i++;
-      } else if (arr2[j] < arr1[i] || arr2[j] < arr3[k]) {
-        j++;
-      } else if (arr3[k] < arr1[i] || arr3[k] < arr2[j]) {
-        k++;
-      } else {
+      if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
         output.add(arr1[i]);
 
         i++;
         j++;
+        k++;
+      } else if (arr1[i] < arr2[j]) {
+        i++;
+      } else if (arr2[j] < arr3[k]) {
+        j++;
+      } else {
         k++;
       }
     }
