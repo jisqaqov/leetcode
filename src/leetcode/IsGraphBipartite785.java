6dac2e750ec1fb4ae50ce307aa7545fd05365d3e
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-13 21:50:10 +0300

    refactoring;

--- src/leetcode/IsGraphBipartite785.java
+++ src/leetcode/IsGraphBipartite785.java
@@ -8,7 +8,7 @@ import java.util.Deque;
  * problem: 785. Is Graph Bipartite?
  * algorithm: DFS, Graph
  * time complexity: O(V + E)
- * space complexity: O(V + E)
+ * space complexity: O(V)
  * Runtime: 0 ms, faster than 100.00% of Java online submissions for Is Graph Bipartite?.
  * Memory Usage: 45.3 MB, less than 36.59% of Java online submissions for Is Graph Bipartite?.
  */
