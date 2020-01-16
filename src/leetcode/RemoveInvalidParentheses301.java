6ef4d8f438ae3ceb82fe2e4aed24a2ea1e7ac879
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-16 16:11:12 +0300

    dfs: 301. Remove Invalid Parentheses;

--- src/leetcode/RemoveInvalidParentheses301.java
+++ src/leetcode/RemoveInvalidParentheses301.java
@@ -1,7 +1,6 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,193 +16,163 @@ public class RemoveInvalidParentheses301 {
   public static void main(String[] args) {
     RemoveInvalidParentheses301 problem = new RemoveInvalidParentheses301();
     problem.test();
+    problem.test2();
   }
 
   private void test() {
-    System.out.println("bfs:");
+    System.out.println("v1:");
     System.out.println(removeInvalidParentheses("()())()"));
     System.out.println(removeInvalidParentheses("(a)())()"));
     System.out.println(removeInvalidParentheses(")("));
     System.out.println(removeInvalidParentheses("("));
     System.out.println(removeInvalidParentheses("(a(b(c)d)"));
-
-    System.out.println("dfs:");
-    SolutionOnDFS dfsSol = new SolutionOnDFS();
-    System.out.println(dfsSol.removeInvalidParentheses("()())()"));
-    System.out.println(dfsSol.removeInvalidParentheses("(a)())()"));
-    System.out.println(dfsSol.removeInvalidParentheses(")("));
-    System.out.println(dfsSol.removeInvalidParentheses("("));
-    System.out.println(dfsSol.removeInvalidParentheses("(a(b(c)d)"));
   }
 
-  /**
-   * https://leetcode.com/problems/remove-invalid-parentheses/discuss/75032/Share-my-Java-BFS-solution
-   * algorithm: BFS
-   * time complexity: O(n*2^(n-1))
-   * space complexity: O(N)
-   * Runtime: 49 ms, faster than 39.11% of Java online submissions for Remove Invalid Parentheses.
-   * Memory Usage: 40.1 MB, less than 80.43% of Java online submissions for Remove Invalid Parentheses.
-   */
-  public List<String> removeInvalidParentheses(String s) {
-    List<String> sol = new ArrayList<>();
+  private void test2() {
+    V2 problem = new V2();
 
-    Set<String> visited = new HashSet<>();
-    Queue<String> queue = new LinkedList<>();
-
-    queue.add(s);
-    visited.add(s);
-
-    boolean isValid = false;
-
-    while (!queue.isEmpty()) {
-      String b = queue.poll();
-
-      if (isValid(b)) {
-        sol.add(b);
-        isValid = true;
-      }
-
-      if (isValid) {
-        continue;
-      }
-
-      for (int i = 0; i < b.length(); i++) {
-        char ch = b.charAt(i);
-        if (ch != '(' && ch != ')') {
-          continue;
-        }
-
-        String t = b.substring(0, i) + b.substring(i + 1);
-
-        if (!visited.contains(t)) {
-          visited.add(t);
-          queue.add(t);
-        }
-      }
-    }
-
-    return sol;
-  }
-
-  private boolean isValid(String s) {
-    int counter = 0;
-    for (int i = 0; i < s.length(); i++) {
-      if (s.charAt(i) == '(') {
-        counter++;
-      } else if (s.charAt(i) == ')') {
-        counter--;
-      }
-
-      if (counter < 0) {
-        return false;
-      }
-    }
-
-    return counter == 0;
+    System.out.println("v2:");
+    System.out.println(problem.removeInvalidParentheses("()())()"));
+    System.out.println(problem.removeInvalidParentheses("(a)())()"));
+    System.out.println(problem.removeInvalidParentheses(")("));
+    System.out.println(problem.removeInvalidParentheses("("));
+    System.out.println(problem.removeInvalidParentheses("(a(b(c)d)"));
   }
 
   /**
    * algorithm: DFS
    * time complexity: O(2^N)
    * space complexity: O(N)
-   * Runtime: 49 ms, faster than 39.11% of Java online submissions for Remove Invalid Parentheses.
-   * Memory Usage: 40.1 MB, less than 80.43% of Java online submissions for Remove Invalid Parentheses.
+   * Runtime: 15 ms, faster than 48.12% of Java online submissions
+   * Memory Usage: 48.5 MB, less than 6.52% of Java online submissions
    */
-  private static class SolutionOnDFS {
+  public List<String> removeInvalidParentheses(String s) {
+    Set<String> output = new HashSet<>();
+
+    int remOpen = 0;
+    int remClose = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '(') {
+        remOpen++;
+      } else if (s.charAt(i) == ')') {
+        if (remOpen > 0) {
+          remOpen--;
+        } else {
+          remClose++;
+        }
+      }
+    }
+
+    int ops = remOpen + remClose;
+    int open = 0;
+
+    gen(s.toCharArray(), new StringBuilder(), ops, open, 0, output);
+
+    if (!output.isEmpty()) {
+      return new ArrayList<>(output);
+    }
+
+    return new ArrayList<>(output);
+  }
+
+  private void gen(char[] s, StringBuilder sb, int ops, int bal, int index, Set<String> output) {
+    if (ops < 0 || bal < 0) {
+      return;
+    }
+
+    if (index == s.length) {
+      if (bal == 0) {
+        output.add(sb.toString());
+      }
+
+      return;
+    }
+
+    int len = sb.length();
+
+    if (s[index] == '(') {
+      gen(s, sb, ops - 1, bal, index + 1, output);
+      gen(s, sb.append(s[index]), ops, bal + 1, index + 1, output);
+    } else if (s[index] == ')') {
+      gen(s, sb, ops - 1, bal, index + 1, output);
+      gen(s, sb.append(s[index]), ops, bal - 1, index + 1, output);
+    } else {
+      gen(s, sb.append(s[index]), ops, bal, index + 1, output);
+    }
+
+    sb.setLength(len);
+  }
+
+  private static class V2 {
+
+    /**
+     * https://leetcode.com/problems/remove-invalid-parentheses/discuss/75032/Share-my-Java-BFS-solution
+     * algorithm: BFS
+     * time complexity: O(n*2^(n-1))
+     * space complexity: O(N)
+     * Runtime: 49 ms, faster than 39.11% of Java online submissions for Remove Invalid Parentheses.
+     * Memory Usage: 40.1 MB, less than 80.43% of Java online submissions for Remove Invalid Parentheses.
+     */
     public List<String> removeInvalidParentheses(String s) {
-      if (s == null || s.length() == 0) {
-        return new ArrayList<>(Collections.singleton(""));
-      }
+      List<String> output = new ArrayList<>();
 
-      int[] temp = calc(0, 0, s.charAt(0));
-      int la = temp[0];
-      int ra = temp[1];
+      Set<String> visited = new HashSet<>();
+      Queue<String> queue = new LinkedList<>();
 
-      for (int i = 1; i < s.length(); i++) {
-        temp = calc(la, ra, s.charAt(i));
-        la = temp[0];
-        ra = temp[1];
-      }
+      queue.add(s);
+      visited.add(s);
 
-      Set<String> sol = new HashSet<>();
+      boolean isValid = false;
 
-      generate(s, "", true, -1, 0, 0, 0, 0, la, ra, sol);
+      while (!queue.isEmpty()) {
+        String b = queue.poll();
 
-      if (!sol.isEmpty()) {
-        return new ArrayList<>(sol);
-      }
-
-      return new ArrayList<>(Collections.singleton(""));
-    }
-
-    private void generate(String s, String t, boolean remove, int index, int l, int r,
-      int lm, int rm, int la, int ra, Set<String> sol) {
-
-      if (index >= 0) {
-        char currChar = s.charAt(index);
-        boolean isBracket = isBracket(currChar);
-
-        if (!isBracket && remove) {
-          return;
+        if (isValid(b)) {
+          output.add(b);
+          isValid = true;
         }
 
-        if (!remove) {
-          if (currChar == '(') {
-            l++;
-          } else if (currChar == ')') {
-            r++;
-          }
+        if (isValid) {
+          continue;
         }
 
-        if (isBracket) {
-          if (!remove) {
-            int[] temp = calc(lm, rm, currChar);
-            lm = temp[0];
-            rm = temp[1];
-            t = t + currChar;
-          } else if (currChar == '(') {
-            lm++;
-          } else if (currChar == ')') {
-            rm++;
-          }
-        } else {
-          t = t + currChar;
-        }
-
-        if (r > l) {
-          return;
-        }
-
-        if (index == s.length() - 1) {
-          if (l == r && lm == la && rm == ra) {
-            sol.add(t);
+        for (int i = 0; i < b.length(); i++) {
+          char ch = b.charAt(i);
+          if (ch != '(' && ch != ')') {
+            continue;
           }
 
-          return;
+          String t = b.substring(0, i) + b.substring(i + 1);
+
+          if (!visited.contains(t)) {
+            visited.add(t);
+            queue.add(t);
+          }
         }
       }
 
-      generate(s, t, false, index + 1, l, r, lm, rm, la, ra, sol);
-      generate(s, t, true, index + 1, l, r, lm, rm, la, ra, sol);
+      return output;
     }
 
-    private boolean isBracket(char ch) {
-      return ch == '(' || ch == ')';
-    }
+    private boolean isValid(String s) {
+      int counter = 0;
 
-    private int[] calc(int l, int r, char ch) {
-      if (ch == ')') {
-        if (l == 0) {
-          r++;
-        } else {
-          l--;
+      for (int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == '(') {
+          counter++;
+        } else if (s.charAt(i) == ')') {
+          counter--;
         }
-      } else if (ch == '(') {
-        l++;
+
+        if (counter < 0) {
+          return false;
+        }
       }
 
-      return new int[]{l, r};
+      return counter == 0;
     }
+
   }
 
 }
