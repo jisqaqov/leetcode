0dd1680208175a68948d543ac6e40770c992129b
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-18 19:13:40 +0300

    facebook: First Index Of Anagram In String;

--- src/facebook/FirstIndexOfAnagramInString.java
+++ src/facebook/FirstIndexOfAnagramInString.java
@@ -1,12 +1,34 @@
-package prep;
+package facebook;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class Prep {
+/**
+ * @author Jandos Iskakov
+ * problem:
+ *
+ * Simplified version of https://leetcode.com/problems/find-all-anagrams-in-a-string/
+ * Find the first index of anagram within a given string.
+ *
+ * Example 1:
+ *
+ * Input: s = "hello", p = "lo"
+ * Output: 3
+ * Explanation: the returned value would be 3 because the smallest index of "lo" is 3.
+ * Example 2:
+ *
+ * Input: s = "hehehe", p = "eh"
+ * Output: 0
+ * Explanation: the returned value would be 0 because "he" which is an anagram can be found in index 0.
+ *
+ * algorithm: Sliding Window
+ * time complexity: O(|S| + |P|)
+ * space complexity: O(|P|)
+ */
+public class FirstIndexOfAnagramInString {
 
   public static void main(String[] args) {
-    Prep problem = new Prep();
+    FirstIndexOfAnagramInString problem = new FirstIndexOfAnagramInString();
     problem.test();
   }
 
@@ -17,8 +39,8 @@ public class Prep {
 
   private int findFirstAnagram(char[] s, char[] p) {
     Map<Character, Integer> map = new HashMap<>();
-    for (int i = 0; i < p.length; i++) {
-      map.put(p[i], map.getOrDefault(p[i], 0) + 1);
+    for (char ch : p) {
+      map.put(ch, map.getOrDefault(ch, 0) + 1);
     }
 
     int diff = p.length;
@@ -36,7 +58,7 @@ public class Prep {
       if (i >= p.length) {
         if (map.containsKey(s[start])) {
           map.put(s[start], map.get(s[start]) + 1);
-
+          
           if (map.get(s[start]) > 0) {
             diff++;
           }
