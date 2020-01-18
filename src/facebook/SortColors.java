2fda9f410f6cd89c7a81afd88af323fdb282f2d3
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-18 18:12:43 +0300

    facebook: variation of Dutch National Flag problem;

---
+++ src/facebook/SortColors.java
@@ -0,0 +1,83 @@
+package facebook;
+
+import java.util.Arrays;
+
+/**
+ * @author Jandos Iskakov
+ * problem:
+ * https://leetcode.com/discuss/interview-question/124623/Facebook-or-Onsite-or-Variation-of-Dutch-National-Flag-problem
+ * You have an unsorted array of integers and a function string
+ * getCategory(integer), which deterministically returns 1 of three possible strings: "low",
+ * "medium", or "high", depending on the input integer. You need to output an array with all the
+ * "low" numbers at the bottom, all the "medium" numbers in the middle, and all the "high" numbers
+ * at the top. This is basically a partial sort. Within each category, the order of the numbers does
+ * not matter.
+ *
+ * For example, you might be give the array [5,7,2,9,1,14,12,10,5,3].
+ * For input integers 1 - 3, getCategory(integer) returns "low",
+ * for 4 - 10 it returns "medium," and for 11 - 15 it returns "high".
+ *
+ * You could output an array (or modify the given array) that looks like this:
+ * [3,1,2,5,5,9,7,10,14,12]
+ *
+ * algorithm: Two Pointers
+ * time complexity: O(N)
+ * space complexity: O(1)
+ */
+public class SortColors {
+
+  public static void main(String[] args) {
+    SortColors problem = new SortColors();
+    problem.test();
+  }
+
+  private void test() {
+    int[] tc1a = {5, 7, 2, 9, 1, 14, 12, 10, 5, 3};
+    sort(tc1a);
+    System.out.println(Arrays.toString(tc1a));
+  }
+
+  public void sort(int[] nums) {
+    int low = 0;
+    int high = nums.length - 1;
+    int i = 0;
+
+    while (i <= high) {
+      String cat = getCategory(nums[i]);
+
+      switch (cat) {
+        case "low":
+          swap(nums, i, low);
+
+          low++;
+          i++;
+          break;
+        case "high":
+          swap(nums, i, high);
+          high--;
+          break;
+        default:
+          i++;
+          break;
+      }
+    }
+  }
+
+  private void swap(int[] nums, int i, int j) {
+    int temp = nums[j];
+    nums[j] = nums[i];
+    nums[i] = temp;
+  }
+
+  private String getCategory(int number) {
+    if (number <= 3) {
+      return "low";
+    } else if (number <= 10) {
+      return "medium";
+    }
+
+    return "high";
+  }
+
+
+}
--- src/prep/Prep.java
+++ src/prep/Prep.java
@@ -1,9 +1,5 @@
 package prep;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 public class Prep {
 
   public static void main(String[] args) {
@@ -13,36 +9,8 @@ public class Prep {
 
   private void test() {
     //TODO:
-    System.out.println(minMeetingRooms(new int[][]{{0, 30}, {5, 10}, {15, 20}}));//2
-    System.out.println(minMeetingRooms(new int[][]{{7, 10}, {2, 4}}));//1
-    System.out.println(minMeetingRooms(new int[][]{{13, 15}, {1, 13}}));//1
-    System.out.println(minMeetingRooms(
-      new int[][]{{1293, 2986}, {848, 3846}, {4284, 5907}, {4466, 4781}, {518, 2918},
-        {300, 5870}}));//4
-    System.out.println(minMeetingRooms(new int[][]{{1, 5}, {8, 9}, {8, 9}}));//2
+
 
   }
-
-  public int minMeetingRooms(int[][] intervals) {
-    PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-    Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
-
-    int max = 0;
-
-    for (int[] interval : intervals) {
-      while (!pq.isEmpty() && pq.peek() <= interval[0]) {
-        pq.poll();
-      }
-
-      pq.offer(interval[1]);
-
-      max = Math.max(pq.size(), max);
-    }
-
-    return max;
-  }
-
-
 
 }
