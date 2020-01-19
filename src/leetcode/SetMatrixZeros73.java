075814f057a9196b67d93d37d0ecffa5731c9aaf
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-19 17:41:02 +0300

    array: 73. Set Matrix Zeroes;

--- src/leetcode/SetMatrixZeros73.java
+++ src/leetcode/SetMatrixZeros73.java
@@ -44,22 +44,13 @@ public class SetMatrixZeros73 {
     boolean zeroRow = false;
 
     for (int i = 0; i < n; i++) {
-      if (matrix[i][0] == 0) {
-        zeroCol = true;
-      }
-    }
-
-    for (int j = 0; j < m; j++) {
-      if (matrix[0][j] == 0) {
-        zeroRow = true;
-      }
-    }
-
-    for (int i = 1; i < n; i++) {
-      for (int j = 1; j < m; j++) {
+      for (int j = 0; j < m; j++) {
         if (matrix[i][j] == 0) {
           matrix[0][j] = 0;
           matrix[i][0] = 0;
+
+          if (i == 0) zeroRow = true;
+          if (j == 0) zeroCol = true;
         }
       }
     }
