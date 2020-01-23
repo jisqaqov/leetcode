eae37df445d6d6f027b2c94568cdd7f3695557db
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-23 12:47:26 +0300

    binary search: solved Facebook | Phone Screen | Peak or Valley Problem;

--- src/facebook/PeakOrValley.java
+++ src/facebook/PeakOrValley.java
@@ -1,9 +1,32 @@
-package prep;
+package facebook;
 
-public class Prep {
+/**
+ * @author Jandos Iskakov
+ * algorithm: Binary Search
+ * time complexity: O(log(N))
+ * space complexity: O(1)
+ *
+ * problem:
+ * https://leetcode.com/discuss/interview-question/473801/Facebook-or-Phone-Screen-or-Peak-or-Valley-Problem
+ * Facebook | Phone Screen | Peak or Valley Problem
+ *
+ * Determine Peak or Valley from an integer array satisfying the following properities.
+ *
+ * At any given point in the array, the difference between two elements is +/- 1.
+ * Ex. Arr[i] = A [i - 1] is +/- 1.
+ * There should be only one peak or valley.
+ * Return the index of the peak or valley from the given integer array.
+ *
+ * Examples:
+ * Valley - [3, 2, 1, 0, 1] ==> 3
+ * Peak - [4, 5, 6, 7, 8, 9, 8, 7] ==> 5
+ * Invalid - [1, 2, 3] or [1, 2, 3, 2, 1, 2]
+ *
+ */
+public class PeakOrValley {
 
   public static void main(String[] args) {
-    Prep problem = new Prep();
+    PeakOrValley problem = new PeakOrValley();
     problem.test();
   }
 
