84c7748ddc0023d738f6b477b7d9234e48a13519
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-01-29 16:45:20 +0300

    two pointers: resolved 26. Remove Duplicates from Sorted Array;

--- src/leetcode/RemoveDuplicatesFromSortedArray26.java
+++ src/leetcode/RemoveDuplicatesFromSortedArray26.java
@@ -30,16 +30,41 @@ public class RemoveDuplicatesFromSortedArray26 {
   }
 
   public int removeDuplicates(int[] nums) {
-    int p = 0;
+    if (nums.length == 0) {
+      return 0;
+    }
 
-    for (int i = 0; i < nums.length; i++) {
-      if (i == 0 || nums[i] != nums[i - 1]) {
+    int p = 1;
+
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] != nums[i - 1]) {
         nums[p] = nums[i];
         p++;
       }
     }
 
     return p;
+  }
+
+  private static class V2 {
+
+    public int removeDuplicates(int[] nums) {
+      if (nums.length == 0) {
+        return 0;
+      }
+
+      int p = 0;
+
+      for (int i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[p]) {
+          p++;
+          nums[p] = nums[i];
+        }
+      }
+
+      return p + 1;
+    }
+
   }
 
 }
