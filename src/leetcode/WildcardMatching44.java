1d218b6fb31eb65a3823f52bd70fb064f9627c75
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-02-03 20:50:48 +0300

    dp: resolved 44. Wildcard Matching;

--- src/leetcode/WildcardMatching44.java
+++ src/leetcode/WildcardMatching44.java
@@ -1,91 +1,54 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author Jandos Iskakov
  * problem: 44. Wildcard Matching
- * algorithm: DP, Greedy, Backtracking
- * time complexity:
- * space complexity:
- * notes: TLE, need to optimize
+ * algorithm: DP
+ * time complexity: O(N*M)
+ * space complexity: O(N*M)
+ * Runtime: 25 ms, faster than 26.14% of Java online submissions
+ * Memory Usage: 41.4 MB, less than 34.88% of Java online submissions
  */
 public class WildcardMatching44 {
 
-    public static void main(String[] args) {
-        WildcardMatching44 solution = new WildcardMatching44();
-        solution.test();
+  public static void main(String[] args) {
+    WildcardMatching44 problem = new WildcardMatching44();
+    problem.test();
+  }
+
+  private void test() {
+    System.out.println(isMatch("aa", "a"));//false
+    System.out.println(isMatch("aa", "*"));//true
+    System.out.println(isMatch("cb", "?a"));//false
+    System.out.println(isMatch("adceb", "*a*b"));//true
+    System.out.println(isMatch("acdcb", "a*c?b"));//false
+  }
+
+  public boolean isMatch(String s, String p) {
+    boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+    dp[0][0] = true;
+
+    for (int i = 1; i <= p.length(); i++) {
+      if (p.charAt(i - 1) == '*') {
+        dp[0][i] = dp[0][i - 1];
+      }
     }
 
-    public void test() {
-        System.out.println(isMatch("abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb",
-                "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb"));
-        System.out.println(isMatch("aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaabb", "a*******b"));
-        System.out.println(isMatch("aabcdedefghijkl", "aabc*"));
-        System.out.println(isMatch("ho", "ho**"));
-        System.out.println(isMatch("a", "a*"));
-        System.out.println(isMatch("aa", "a"));
-        System.out.println(isMatch("aa", "*"));
-        System.out.println(isMatch("cb", "?a"));
-        System.out.println(isMatch("cb", "?b"));
-        System.out.println(isMatch("adceb", "*a*b"));
-        System.out.println(isMatch("acdcb", "a*c?b"));
+    for (int i = 1; i <= s.length(); i++) {
+      for (int j = 1; j <= p.length(); j++) {
+        char sch = s.charAt(i - 1);
+        char pch = p.charAt(j - 1);
+
+        if (sch == pch || pch == '?') {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else if (pch == '*') {
+          dp[i][j] = dp[i - 1][j - 1] || dp[i][j - 1] || dp[i - 1][j];
+        }
+      }
     }
 
-    public boolean isMatch(String s, String p) {
-        Map<String, Boolean> memo = new HashMap<>();
+    return dp[s.length()][p.length()];
+  }
 
-        return isMatch(s, p, 0, 0, memo);
-    }
-
-    public boolean isMatch(String s, String p, int i, int j, Map<String, Boolean> memo) {
-        String index = i + ":" + j;
-
-        if (p.equals("*")) {
-            memo.put(index, true);
-            return true;
-        }
-
-        if (memo.containsKey(index)) {
-            return memo.get(index);
-        }
-
-        while (i < s.length() && j < p.length()) {
-            if (p.charAt(j) == '*') {
-                while (j < p.length() && p.charAt(j) == '*') {
-                    j++;
-                }
-
-                j--;
-
-                if (j == p.length() - 1) {
-                    memo.put(i + ":" + j, true);
-                    return true;
-                }
-
-                for (int k = i; k < s.length(); k++) {
-                    if (isMatch(s, p, k, j + 1, memo)) {
-                        return true;
-                    }
-                }
-            } else if (p.charAt(j) != '?' && s.charAt(i) != p.charAt(j)) {
-                memo.put(i + ":" + j, false);
-                return false;
-            }
-
-            ++i;
-            ++j;
-        }
-
-        while (j < p.length() && p.charAt(j) == '*') {
-            j++;
-        }
-
-        boolean isMatch = i == s.length() && j == p.length();
-        memo.put(i + ":" + j, isMatch);
-
-        return isMatch;
-    }
 
 }
