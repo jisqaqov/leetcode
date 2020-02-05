e6b8e0a619c17b9684c302ae3459e47bbb560c98
Zhandos Iskakov <zhandos.iskakov@iii.com>
2020-02-05 11:28:56 +0300

    stack: solved 496. Next Greater Element I;

---
+++ src/leetcode/NextGreaterElementI496.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+import utils.TestUtils;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Jandos Iskakov
+ * problem: 496. Next Greater Element I
+ * algorithm: Stack, Hash Table
+ * time complexity: O(N + M)
+ * space complexity: O(M)
+ * Runtime: 3 ms, faster than 85.79% of Java online submissions
+ * Memory Usage: 41.1 MB, less than 7.41% of Java online submissions
+ */
+public class NextGreaterElementI496 {
+
+  public static void main(String[] args) {
+    NextGreaterElementI496 prep = new NextGreaterElementI496();
+    prep.test();
+  }
+
+  private void test() {
+    TestUtils.printArray(nextGreaterElement(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2}));//-1, 3, -1
+  }
+
+  public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    Deque<Integer> stack = new ArrayDeque<>();
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = nums2.length - 1; i >= 0; i--) {
+      int g = -1;
+      if (i + 1 <= nums2.length - 1 && nums2[i] < nums2[i + 1]) {
+        g = nums2[i + 1];
+        stack.push(nums2[i + 1]);
+      } else if (!stack.isEmpty()) {
+        while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+          stack.pop();
+        }
+
+        if (!stack.isEmpty()) {
+          g = stack.peek();
+        }
+      }
+
+      map.put(nums2[i], g);
+    }
+
+    int[] output = new int[nums1.length];
+    for (int i = 0; i < nums1.length; i++) {
+      output[i] = map.getOrDefault(nums1[i], -1);
+    }
+
+    return output;
+  }
+
+}
