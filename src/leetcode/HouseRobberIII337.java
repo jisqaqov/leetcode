64d6b5326609fd6fc8150964ebc0152a9df5e0b5
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-02-29 21:20:38 +0300

    dp: resolved 337. House Robber III;

--- src/leetcode/HouseRobberIII337.java
+++ src/leetcode/HouseRobberIII337.java
@@ -9,41 +9,69 @@ import java.util.Map;
  * algorithm: Tree, DFS
  * time complexity: O(N)
  * space complexity: O(N)
- * Runtime: 2 ms, faster than 57.61% of Java online submissions
- * Memory Usage: 41.7 MB, less than 11.11% of Java online submissions
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions
+ * Memory Usage: 39.6 MB, less than 58.33% of Java online submissions
  */
 public class HouseRobberIII337 {
 
+  private static final int[] ZERO_ARR = {0, 0};
+
   public int rob(TreeNode root) {
-    Map<TreeNode, Integer> dp = new HashMap<>();
-    return helper(root, dp);
+    return helper(root)[0];
   }
 
-  private int helper(TreeNode root, Map<TreeNode, Integer> map) {
+  private int[] helper(TreeNode root) {
     if (root == null) {
-      return 0;
+      return ZERO_ARR;
     }
 
-    if (map.containsKey(root)) {
-      return map.get(root);
-    }
+    int[] left = helper(root.left);
+    int[] right = helper(root.right);
 
-    int max1 = helper(root.left, map) + helper(root.right, map);
+    int[] max = new int[2];
 
-    int max2 = root.val;
-    if (root.left != null) {
-      max2 += helper(root.left.left, map) + helper(root.left.right, map);
-    }
+    max[0] = Math.max(root.val + left[1] + right[1],
+      left[0] + right[0]);
 
-    if (root.right != null) {
-      max2 += helper(root.right.left, map) + helper(root.right.right, map);
-    }
-
-    int max = Math.max(max1, max2);
-
-    map.put(root, max);
+    max[1] = left[0] + right[0];
 
     return max;
+  }
+
+
+  private static class V2 {
+
+    public int rob(TreeNode root) {
+      Map<TreeNode, Integer> dp = new HashMap<>();
+      return helper(root, dp);
+    }
+
+    private int helper(TreeNode root, Map<TreeNode, Integer> map) {
+      if (root == null) {
+        return 0;
+      }
+
+      if (map.containsKey(root)) {
+        return map.get(root);
+      }
+
+      int max1 = helper(root.left, map) + helper(root.right, map);
+
+      int max2 = root.val;
+      if (root.left != null) {
+        max2 += helper(root.left.left, map) + helper(root.left.right, map);
+      }
+
+      if (root.right != null) {
+        max2 += helper(root.right.left, map) + helper(root.right.right, map);
+      }
+
+      int max = Math.max(max1, max2);
+
+      map.put(root, max);
+
+      return max;
+    }
   }
 
   /**
