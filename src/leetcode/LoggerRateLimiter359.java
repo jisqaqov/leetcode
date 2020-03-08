5f0b82bc2c8216899d3c8725ebab2d2a6580b494
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-03-08 18:32:04 +0300

    array: resolved 359. Logger Rate Limiter;

--- src/leetcode/LoggerRateLimiter359.java
+++ src/leetcode/LoggerRateLimiter359.java
@@ -1,7 +1,11 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * @author Jandos Iskakov
@@ -14,15 +18,54 @@ import java.util.Map;
  */
 public class LoggerRateLimiter359 {
 
-
   class Logger {
+    private class Log {
+      private String message;
+      private int timestamp;
+
+      public Log(String message, int timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+      }
+    }
+
+    private Queue<Log> queue;
+    private Set<String> set;
+
+    /** Initialize your data structure here. */
+    public Logger() {
+      queue = new LinkedList<>();
+      set = new HashSet<>();
+    }
+
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+     If this method returns false, the message will not be printed.
+     The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+      while (!queue.isEmpty() && timestamp - queue.peek().timestamp >= 10) {
+        set.remove(queue.poll().message);
+      }
+
+      if (set.contains(message)) {
+        return false;
+      }
+
+      queue.add(new Log(message, timestamp));
+      set.add(message);
+
+      return true;
+    }
+  }
+
+
+  class LoggerV2 {
 
     private Map<String, Integer> map = new HashMap<>();
 
     /**
      * Initialize your data structure here.
      */
-    public Logger() {
+    public LoggerV2() {
 
     }
 
