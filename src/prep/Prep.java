8ec215c0f664d7f8a1675edb131979936f4c33b3
Zhandos Iskakov <zhandos.iskakov@iii.com>
2020-03-12 11:50:02 +0300

    stack: resolved 227. Basic Calculator II;

--- src/prep/Prep.java
+++ src/prep/Prep.java
@@ -1,5 +1,8 @@
 package prep;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Prep {
 
   public static void main(String[] args) {
@@ -8,7 +11,75 @@ public class Prep {
   }
 
   private void test() {
+    System.out.println(calculate("3+5/2"));
+  }
 
+  public int calculate(String s) {
+    Deque<Integer> stack = new ArrayDeque<>();
+
+    char oper = '+';
+    int num = 0;
+
+    stack.push(0);
+
+    for (int i = 0; i < s.length(); i++) {
+      char ch = s.charAt(i);
+
+      if (ch == '+' || ch == '-') {
+        stack.push(calculate(stack, num, oper));
+
+        num = 0;
+        oper = ch;
+      } else if (ch == '*' || ch == '/') {
+        int val = 0;
+        if (oper == '*' || oper == '/') {
+          val = calculate(stack, num, oper);
+        } else {
+          if (oper == '-') {
+            num = -num;
+          }
+
+          val = num;
+        }
+
+        stack.push(val);
+
+        num = 0;
+        oper = ch;
+      } else if (ch != ' ') {
+        num = num * 10 + (ch - '0');
+      }
+    }
+
+    stack.push(calculate(stack, num, oper));
+
+    int output = 0;
+    while (!stack.isEmpty()) {
+      output += stack.pop();
+    }
+
+    return output;
+  }
+
+  private int calculate(Deque<Integer> stack, int num, char oper) {
+    int val = 0;
+
+    switch (oper) {
+      case '+':
+        val = stack.pop() + num;
+        break;
+      case '-':
+        val = stack.pop() - num;
+        break;
+      case '*':
+        val = stack.pop() * num;
+        break;
+      case '/':
+        val = stack.pop() / num;
+        break;
+    }
+
+    return val;
   }
 
 }
