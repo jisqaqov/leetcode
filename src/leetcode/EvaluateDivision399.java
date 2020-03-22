6f250a2b3ba94b68f9a7c374425c8ddfdd3a1fc7
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-03-22 18:15:23 +0300

    graph: solved 399. Evaluate Division;

--- src/leetcode/EvaluateDivision399.java
+++ src/leetcode/EvaluateDivision399.java
@@ -3,8 +3,10 @@ package leetcode;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 import utils.TestUtils;
 
@@ -93,21 +95,15 @@ public class EvaluateDivision399 {
       return 1;
     }
 
-    if (graph.get(a).containsKey(b)) {
-      return graph.get(a).get(b);
-    } else if (graph.get(b).containsKey(a)) {
-      return graph.get(b).get(a);
-    }
-
     used.add(a);
 
     double eval = -1;
 
-    for (String adj : graph.get(a).keySet()) {
-      if (!used.contains(adj)) {
-        double val = eval(adj, b, graph, used);
+    for (Map.Entry<String, Double> adj : graph.get(a).entrySet()) {
+      if (!used.contains(adj.getKey())) {
+        double val = eval(adj.getKey(), b, graph, used);
         if (val != -1) {
-          eval = graph.get(a).get(adj) * val;
+          eval = adj.getValue() * val;
           break;
         }
       }
@@ -116,6 +112,71 @@ public class EvaluateDivision399 {
     used.remove(a);
 
     return eval;
+  }
+
+  private static class BfsVersion {
+
+    public double[] calcEquation(List<List<String>> equations, double[] values,
+      List<List<String>> queries) {
+      Map<String, Map<String, Double>> graph = new HashMap<>();
+
+      for (int i = 0; i < equations.size(); i++) {
+        List<String> eq = equations.get(i);
+
+        graph.putIfAbsent(eq.get(0), new HashMap<>());
+        graph.get(eq.get(0)).put(eq.get(1), values[i]);
+
+        graph.putIfAbsent(eq.get(1), new HashMap<>());
+        graph.get(eq.get(1)).put(eq.get(0), 1 / values[i]);
+      }
+
+      double[] output = new double[queries.size()];
+
+      for (int i = 0; i < queries.size(); i++) {
+        List<String> query = queries.get(i);
+        output[i] = eval(query.get(0), query.get(1), graph);
+      }
+
+      return output;
+    }
+
+    private double eval(String a, String b, Map<String, Map<String, Double>> graph) {
+      if (!graph.containsKey(a) || !graph.containsKey(b)) {
+        return -1;
+      }
+
+      if (a.equals(b)) {
+        return 1;
+      }
+
+      Set<String> used = new HashSet<>();
+      used.add(a);
+
+      Queue<String> queue = new LinkedList<>();
+      queue.add(a);
+
+      Queue<Double> values = new LinkedList<>();
+      values.add(1.0);
+
+      while (!queue.isEmpty()) {
+        String node = queue.poll();
+        double value = values.poll();
+
+        if (node.equals(b)) {
+          return value;
+        }
+
+        for (Map.Entry<String, Double> adj : graph.get(node).entrySet()) {
+          if (!used.contains(adj.getKey())) {
+            queue.add(adj.getKey());
+            values.add(value * adj.getValue());
+          }
+        }
+      }
+
+      return -1;
+    }
+
   }
 
 }
