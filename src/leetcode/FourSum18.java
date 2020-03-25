366adc70cb633b51172e27f91e4c183b8e08edc1
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-03-25 21:06:49 +0300

    two pointers: solved 18. 4Sum;

--- src/leetcode/FourSum18.java
+++ src/leetcode/FourSum18.java
@@ -26,60 +26,120 @@ public class FourSum18 {
   }
 
   public List<List<Integer>> fourSum(int[] nums, int target) {
-    List<List<Integer>> output = new ArrayList<>();
-
     Arrays.sort(nums);
 
-    for (int i = 0; i <= nums.length - 4; i++) {
-      if (i > 0 && nums[i] == nums[i - 1]) {
-        continue;
+    return helper(nums, 4, 0, target);
+  }
+
+  public List<List<Integer>> helper(int[] nums, int k, int index, int target) {
+    List<List<Integer>> output = new ArrayList<>();
+
+    if (k == 2) {
+      int i = index;
+      int j = nums.length - 1;
+
+      while (i < j) {
+        int s = nums[i] + nums[j];
+        if (s == target) {
+          List<Integer> temp = new ArrayList<>();
+          temp.add(nums[i]);
+          temp.add(nums[j]);
+          output.add(temp);
+
+          i++;
+          j--;
+
+          // avoid duplicates
+          while (i < j && nums[i] == nums[i - 1]) {
+            i++;
+          }
+
+          while (i < j && nums[j] == nums[j + 1]) {
+            j--;
+          }
+        } else if (s > target) {
+          j--;
+        } else {
+          i++;
+        }
       }
 
-      List<int[]> list = threeSum(nums, i + 1, target - nums[i]);
-      for (int[] sub : list) {
-        output.add(Arrays.asList(nums[i], sub[0], sub[1], sub[2]));
+    } else {
+      for (int i = index; i <= nums.length - k; i++) {
+        if (i > index && nums[i] == nums[i - 1]) {
+          continue;
+        }
+
+        List<List<Integer>> list = helper(nums, k - 1, i + 1, target - nums[i]);
+        for (List<Integer> sub : list) {
+          sub.add(0, nums[i]);
+          output.add(sub);
+        }
       }
     }
 
     return output;
   }
 
-  private List<int[]> threeSum(int[] nums, int idx, int target) {
-    List<int[]> output = new ArrayList<>();
+  private static class V2 {
 
-    for (int i = idx; i <= nums.length - 3; i++) {
-      if (i > idx && nums[i] == nums[i - 1]) {
-        continue;
-      }
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+      List<List<Integer>> output = new ArrayList<>();
 
-      int p = target - nums[i];
+      Arrays.sort(nums);
 
-      int j = i + 1;
-      int k = nums.length - 1;
-      while (j < k) {
-        if (nums[j] + nums[k] == p) {
-          output.add(new int[]{nums[i], nums[j], nums[k]});
+      for (int i = 0; i <= nums.length - 4; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) {
+          continue;
+        }
 
-          j++;
-          k--;
-
-          while (j < k && nums[j] == nums[j - 1]) {
-            j++;
-          }
-
-          while (j < k && nums[k] == nums[k + 1]) {
-            k--;
-          }
-
-        } else if (nums[j] + nums[k] < p) {
-          j++;
-        } else {
-          k--;
+        List<int[]> list = threeSum(nums, i + 1, target - nums[i]);
+        for (int[] sub : list) {
+          output.add(Arrays.asList(nums[i], sub[0], sub[1], sub[2]));
         }
       }
+
+      return output;
     }
 
-    return output;
+    private List<int[]> threeSum(int[] nums, int idx, int target) {
+      List<int[]> output = new ArrayList<>();
+
+      for (int i = idx; i <= nums.length - 3; i++) {
+        if (i > idx && nums[i] == nums[i - 1]) {
+          continue;
+        }
+
+        int p = target - nums[i];
+
+        int j = i + 1;
+        int k = nums.length - 1;
+        while (j < k) {
+          if (nums[j] + nums[k] == p) {
+            output.add(new int[]{nums[i], nums[j], nums[k]});
+
+            j++;
+            k--;
+
+            while (j < k && nums[j] == nums[j - 1]) {
+              j++;
+            }
+
+            while (j < k && nums[k] == nums[k + 1]) {
+              k--;
+            }
+
+          } else if (nums[j] + nums[k] < p) {
+            j++;
+          } else {
+            k--;
+          }
+        }
+      }
+
+      return output;
+    }
+
   }
 
 }
