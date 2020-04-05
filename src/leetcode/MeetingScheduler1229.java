a1b9653cfb04c89d6103ec5e7abafdcd817847f0
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-04-05 18:57:18 +0300

    two pointers: solved 1229. Meeting Scheduler;

--- src/leetcode/MeetingScheduler1229.java
+++ src/leetcode/MeetingScheduler1229.java
@@ -11,8 +11,8 @@ import java.util.List;
  * algorithm: Sort, Two Pointers
  * time complexity: O(nlog(n) + mlog(m))
  * space complexity: O(1)
- * Runtime: 27 ms, faster than 49.82% of Java online submissions
- * Memory Usage: 48.9 MB, less than 100.00% of Java online submissions
+ * Runtime: 38 ms, faster than 11.14% of Java online submissions
+ * Memory Usage: 48.6 MB, less than 100.00% of Java online submissions
  */
 public class MeetingScheduler1229 {
 
@@ -22,21 +22,12 @@ public class MeetingScheduler1229 {
 
     int i = 0, j = 0;
 
-    List<Integer> output = new ArrayList<>();
-
     while (i < slots1.length && j < slots2.length) {
-      if (isOverlap(slots1[i], slots2[j])) {
-        int start = Math.max(slots1[i][0], slots2[j][0]);
-        int end = Math.min(slots1[i][1], slots2[j][1]);
+      int start = Math.max(slots1[i][0], slots2[j][0]);
+      int end = Math.min(slots1[i][1], slots2[j][1]);
 
-        int len = end - start;
-
-        if (len >= duration) {
-          output.add(start);
-          output.add(start + duration);
-
-          break;
-        }
+      if (start + duration <= end) {
+        return Arrays.asList(start, start + duration);
       }
 
       if (slots1[i][1] < slots2[j][1]) {
@@ -46,12 +37,7 @@ public class MeetingScheduler1229 {
       }
     }
 
-    return output;
-  }
-
-  private boolean isOverlap(int[] slots1, int[] slots2) {
-    return (slots2[0] >= slots1[0] && slots2[0] <= slots1[1]) ||
-      (slots1[0] >= slots2[0] && slots1[0] <= slots2[1]);
+    return new ArrayList<>();
   }
 
 }
