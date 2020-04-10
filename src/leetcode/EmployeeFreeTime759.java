f4f9999d012fd1e9ff60823fc8f03d2bf2b726c1
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-04-10 15:33:53 +0300

    sort: resolved 759. Employee Free Time;

--- src/leetcode/EmployeeFreeTime759.java
+++ src/leetcode/EmployeeFreeTime759.java
@@ -1,7 +1,8 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -11,39 +12,98 @@ import java.util.PriorityQueue;
  * algorithm: Heap
  * time complexity: O(nlog(n))
  * space complexity: O(nlog(n))
- * Runtime: 11 ms, faster than 49.71% of Java online submissions
- * Memory Usage: 43.1 MB, less than 100.00% of Java online submissions
+ * Runtime: 9 ms, faster than 79.83% of Java online submissions
+ * Memory Usage: 42 MB, less than 100.00% of Java online submissions
  */
 public class EmployeeFreeTime759 {
 
+  public static void main(String[] args) {
+    EmployeeFreeTime759 problem = new EmployeeFreeTime759();
+    problem.test();
+  }
+
+  private void test() {
+    System.out.println(employeeFreeTime(
+      Arrays.asList(
+        Arrays.asList(new Interval(1, 2), new Interval(5, 6)),
+        Arrays.asList(new Interval(2, 3), new Interval(4, 10))
+      )
+    ));
+  }
+
   public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-    List<Interval> list = new ArrayList<>();
-    for (List<Interval> sch : schedule) {
-      list.addAll(sch);
+    if (schedule.isEmpty()) {
+      return Collections.emptyList();
     }
 
-    list.sort((i1, i2) -> i1.start - i2.start);
+    List<Interval> sorted = new ArrayList<>();
+    for (List<Interval> sch : schedule) {
+      sorted.addAll(sch);
+    }
 
-    PriorityQueue<Interval> pq = new PriorityQueue<>((i1, i2) -> i1.end - i2.end);
+    sorted.sort((i1, i2) -> i1.start - i2.start);
+
+    Interval current = new Interval(sorted.get(0).start, sorted.get(0).end);
+
+    List<Interval> merged = new ArrayList<>();
+    merged.add(current);
+
+    for (int i = 1; i < sorted.size(); i++) {
+      Interval interval = sorted.get(i);
+
+      if (interval.start < current.end) {
+        current.end = Math.max(interval.end, current.end);
+      } else {
+        current = new Interval(sorted.get(i).start, sorted.get(i).end);
+        merged.add(current);
+      }
+    }
 
     List<Interval> output = new ArrayList<>();
 
-    for (int i = 0; i < list.size(); i++) {
-      Interval interval = list.get(i);
+    for (int i = 1; i < merged.size(); i++) {
+      Interval curr = merged.get(i);
+      Interval prev = merged.get(i - 1);
 
-      int last = -1;
-      while (!pq.isEmpty() && pq.peek().end < interval.start) {
-        last = pq.poll().end;
+      if (curr.end > prev.start && prev.end < curr.start) {
+        output.add(new Interval(prev.end, curr.start));
       }
-
-      if (pq.isEmpty() && last != -1) {
-        output.add(new Interval(last, interval.start));
-      }
-
-      pq.add(interval);
     }
 
     return output;
+  }
+
+  private class V2 {
+
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+      List<Interval> list = new ArrayList<>();
+      for (List<Interval> sch : schedule) {
+        list.addAll(sch);
+      }
+
+      list.sort((i1, i2) -> i1.start - i2.start);
+
+      PriorityQueue<Interval> pq = new PriorityQueue<>((i1, i2) -> i1.end - i2.end);
+
+      List<Interval> output = new ArrayList<>();
+
+      for (int i = 0; i < list.size(); i++) {
+        Interval interval = list.get(i);
+
+        int last = -1;
+        while (!pq.isEmpty() && pq.peek().end < interval.start) {
+          last = pq.poll().end;
+        }
+
+        if (pq.isEmpty() && last != -1) {
+          output.add(new Interval(last, interval.start));
+        }
+
+        pq.add(interval);
+      }
+
+      return output;
+    }
   }
 
   /**
@@ -60,6 +120,14 @@ public class EmployeeFreeTime759 {
     public Interval(int start, int end) {
       this.start = start;
       this.end = end;
+    }
+
+    @Override
+    public String toString() {
+      return "Interval{" +
+        "start=" + start +
+        ", end=" + end +
+        '}';
     }
   }
 
