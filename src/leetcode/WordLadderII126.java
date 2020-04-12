8cc9abc598b3ec0c8ee29c7aed788a9a701a3d75
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-04-12 12:12:55 +0300

    bfs: resolved 126. Word Ladder II;

--- src/leetcode/WordLadderII126.java
+++ src/leetcode/WordLadderII126.java
@@ -38,64 +38,87 @@ public class WordLadderII126 {
   }
 
   public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-    List<List<String>> solution = new ArrayList<>();
+    List<List<String>> output = new ArrayList<>();
 
     Set<String> dict = new HashSet<>(wordList);
+
     Set<String> visited = new HashSet<>();
+    visited.add(beginWord);
 
     Queue<String> queue = new LinkedList<>();
     queue.add(beginWord);
 
-    Map<String, Integer> dis = new HashMap<>();
-    dis.put(beginWord, 0);
+    Map<String, List<String>> graph = new HashMap<>();
+    for (String word : dict) {
+      graph.put(word, new ArrayList<>());
+    }
 
-    Map<String, Set<String>> adjList = new HashMap<>();
-
-    while (!queue.isEmpty() && !visited.contains(endWord)) {
+    while (!queue.isEmpty()) {
       int size = queue.size();
+
+      Set<String> level = new HashSet<>();
 
       for (int q = 0; q < size; q++) {
         String node = queue.poll();
 
-        for (int i = 0; i < node.length(); i++) {
-          char[] chars = node.toCharArray();
+        List<String> candidates = getCandidateWords(node, dict);
+        for (String candidate : candidates) {
+          if (visited.contains(candidate)) {
+            continue;
+          }
 
-          for (char c = 'a'; c <= 'z'; c++) {
-            chars[i] = c;
+          graph.get(candidate).add(node);
 
-            String word = new String(chars);
-
-            if (!dict.contains(word) || word.equals(node)) {
-              continue;
-            }
-
-            if (dis.get(node) + 1 <= dis.getOrDefault(word, Integer.MAX_VALUE)) {
-              dis.put(word, dis.get(node) + 1);
-
-              adjList.putIfAbsent(word, new HashSet<>());
-              adjList.get(word).add(node);
-
-              if (!word.equals(endWord) && !visited.contains(word)) {
-                queue.add(word);
-              }
-
-              visited.add(word);
-            }
+          if (!level.contains(candidate)) {
+            queue.add(candidate);
           }
         }
+
+        level.addAll(candidates);
+      }
+
+      visited.addAll(level);
+
+      if (visited.contains(endWord)) {
+        break;
       }
     }
 
     if (visited.contains(endWord)) {
-      backtrack(endWord, beginWord, adjList,
+      buildPath(endWord, beginWord, graph,
         new LinkedList<>(Collections.singletonList(endWord)),
-        solution);
+        output);
     }
 
-    return solution;
+    return output;
   }
 
-  private void backtrack(String node, String beginWord, Map<String, Set<String>> adjList,
+  private List<String> getCandidateWords(String word, Set<String> dict) {
+    List<String> output = new ArrayList<>();
+
+    char[] chs = word.toCharArray();
+
+    for (int i = 0; i < word.length(); i++) {
+      char oldChar = chs[i];
+
+      for (char ch = 'a'; ch <= 'z'; ch++) {
+        chs[i] = ch;
+
+        if (ch != oldChar) {
+          String candidate = String.valueOf(chs);
+          if (dict.contains(candidate)) {
+            output.add(candidate);
+          }
+        }
+      }
+
+      chs[i] = oldChar;
+    }
+
+    return output;
+  }
+
+  private void buildPath(String node, String beginWord, Map<String, List<String>> adjList,
     LinkedList<String> list, List<List<String>> solution) {
 
     if (node.equals(beginWord)) {
@@ -105,7 +128,7 @@ public class WordLadderII126 {
 
     for (String adj : adjList.get(node)) {
       list.addFirst(adj);
-      backtrack(adj, beginWord, adjList, list, solution);
+      buildPath(adj, beginWord, adjList, list, solution);
       list.removeFirst();
     }
   }
