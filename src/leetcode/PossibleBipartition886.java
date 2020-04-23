7c699e54a0bf983aa82dfb34cb60b1abd61c1179
Zhandos Iskakov <zhandos.iskakov@iii.com>
2020-04-23 17:52:06 +0300

    bfs: solved 886. Possible Bipartition;

--- src/leetcode/PossibleBipartition886.java
+++ src/leetcode/PossibleBipartition886.java
@@ -1,38 +1,61 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 /**
  * @author Jandos Iskakov
  * problem: 886. Possible Bipartition
  * algorithm: Graph, DFS
- * time complexity: O(N)
- * space complexity: O(N)
- * Runtime: 24 ms, faster than 54.55% of Java online submissions
- * Memory Usage: 49.1 MB, less than 100.00% of Java online submissions
+ * time complexity: O(N + E)
+ * space complexity: O(N + E)
+ * Runtime: 11 ms, faster than 95.97% of Java online submissions
+ * Memory Usage: 47.1 MB, less than 100.00% of Java online submissions
  */
 public class PossibleBipartition886 {
 
   public boolean possibleBipartition(int n, int[][] dislikes) {
-    Set<Integer>[] graph = new HashSet[n + 1];
-    for (int i = 1; i <= n; i++) {
-      graph[i] = new HashSet<>();
+    List<Integer>[] graph = new ArrayList[n];
+    for (int i = 0; i < n; i++) {
+      graph[i] = new ArrayList<>();
     }
 
     for (int[] dislike : dislikes) {
-      graph[dislike[0]].add(dislike[1]);
-      graph[dislike[1]].add(dislike[0]);
+      int u = dislike[0] - 1;
+      int v = dislike[1] - 1;
+
+      graph[u].add(v);
+      graph[v].add(u);
     }
 
-    int[] groups = new int[n + 1];
+    int[] groups = new int[n];
 
-    for (int p = 1; p <= n; p++) {
-      if (groups[p] == 0) {
-        groups[p] = 1;
+    for (int i = 0; i < n; i++) {
+      if (groups[i] != 0) {
+        continue;
+      }
 
-        if (!checkPossibility(groups, p, graph)) {
-          return false;
+      groups[i] = 1;
+
+      Queue<Integer> queue = new LinkedList<>();
+      queue.add(i);
+
+      while (!queue.isEmpty()) {
+        int person = queue.poll();
+
+        for (int adj : graph[person]) {
+          if (groups[adj] == groups[person]) {
+            return false;
+          }
+
+          if (groups[adj] == 0) {
+            groups[adj] = -groups[person];
+            queue.add(adj);
+          }
         }
       }
     }
@@ -40,22 +63,52 @@ public class PossibleBipartition886 {
     return true;
   }
 
-  private boolean checkPossibility(int[] groups, int person, Set<Integer>[] dislikes) {
-    for (int p : dislikes[person]) {
-      if (groups[person] == groups[p]) {
-        return false;
+  private static class DfsVersion {
+
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+      Set<Integer>[] graph = new HashSet[n + 1];
+      for (int i = 1; i <= n; i++) {
+        graph[i] = new HashSet<>();
       }
 
-      if (groups[p] == 0) {
-        groups[p] = -groups[person];
+      for (int[] dislike : dislikes) {
+        graph[dislike[0]].add(dislike[1]);
+        graph[dislike[1]].add(dislike[0]);
+      }
 
-        if (!checkPossibility(groups, p, dislikes)) {
-          return false;
+      int[] groups = new int[n + 1];
+
+      for (int p = 1; p <= n; p++) {
+        if (groups[p] == 0) {
+          groups[p] = 1;
+
+          if (!checkPossibility(groups, p, graph)) {
+            return false;
+          }
         }
       }
+
+      return true;
     }
 
-    return true;
+    private boolean checkPossibility(int[] groups, int person, Set<Integer>[] dislikes) {
+      for (int p : dislikes[person]) {
+        if (groups[person] == groups[p]) {
+          return false;
+        }
+
+        if (groups[p] == 0) {
+          groups[p] = -groups[person];
+
+          if (!checkPossibility(groups, p, dislikes)) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    }
+
   }
 
 }
