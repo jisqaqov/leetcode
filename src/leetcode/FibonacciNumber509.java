88051478d0ba9448dd9634da8244a66cc2182018
Zhandos Iskakov <zhandos.iskakov@iii.com>
2020-04-30 15:48:28 +0300

    dp: solved 509. Fibonacci Number;

--- src/leetcode/FibonacciNumber509.java
+++ src/leetcode/FibonacciNumber509.java
@@ -33,20 +33,20 @@ public class FibonacciNumber509 {
 
   private static class RecursionVersion {
 
-    private Map<Integer, Integer> dp = new HashMap<>();
+    private int[] dp = new int[31];
 
     public int fib(int n) {
       if (n <= 1) {
         return n;
       }
 
-      if (dp.containsKey(n)) {
-        return dp.get(n);
+      if (dp[n] != 0) {
+        return dp[n];
       }
 
       int fn = fib(n - 1) + fib(n - 2);
 
-      dp.put(n, fn);
+      dp[n] = fn;
 
       return fn;
     }
