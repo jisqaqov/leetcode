e7e7323b4ed63bae45b1620707506bda6f736864
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-05-13 17:25:32 +0300

    two pointers: solved 259. 3Sum Smaller;

--- src/leetcode/ThreeSumSmaller259.java
+++ src/leetcode/ThreeSumSmaller259.java
@@ -14,15 +14,11 @@ import java.util.Arrays;
 public class ThreeSumSmaller259 {
 
   public int threeSumSmaller(int[] nums, int target) {
-    if (nums.length < 3) {
-      return 0;
-    }
-
     Arrays.sort(nums);
 
     int count = 0;
 
-    for (int i = 0; i < nums.length; i++) {
+    for (int i = 0; i < nums.length - 2; i++) {
       int j = i + 1;
       int k = nums.length - 1;
 
