a18e43d64a369685438fb525de39787f69ea6605
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-05-13 18:12:44 +0300

    stack: solved 1209. Remove All Adjacent Duplicates in String II;

--- src/leetcode/RemoveAllAdjacentDuplicatesInStringII1209.java
+++ src/leetcode/RemoveAllAdjacentDuplicatesInStringII1209.java
@@ -15,32 +15,73 @@ import java.util.Deque;
 public class RemoveAllAdjacentDuplicatesInStringII1209 {
 
   public String removeDuplicates(String s, int k) {
-    Deque<Integer> counts = new ArrayDeque<>();
-    Deque<Character> letters = new ArrayDeque<>();
+    Deque<Node> stack = new ArrayDeque<>();
 
     for (int i = 0; i < s.length(); i++) {
-      if (letters.isEmpty() || letters.peek() != s.charAt(i)) {
-        letters.push(s.charAt(i));
-        counts.push(1);
-      } else if (letters.peek() == s.charAt(i)) {
-        counts.push(counts.poll() + 1);
-        letters.push(s.charAt(i));
+      if (stack.isEmpty() || stack.peek().ch != s.charAt(i)) {
+        stack.push(new Node(s.charAt(i), 1));
+      } else if (stack.peek().ch == s.charAt(i)) {
+        stack.peek().count++;
       }
 
-      if (counts.peek() == k) {
-        counts.poll();
-        while (!letters.isEmpty() && letters.peek() == s.charAt(i)) {
-          letters.poll();
-        }
+      if (stack.peek().count == k) {
+        stack.poll();
       }
     }
 
     StringBuilder sb = new StringBuilder();
-    while (!letters.isEmpty()) {
-      sb.append(letters.poll());
+    while (!stack.isEmpty()) {
+      Node node = stack.poll();
+      for (int i = 0; i < node.count; i++) {
+        sb.append(node.ch);
+      }
     }
 
     return sb.reverse().toString();
   }
+
+  private class Node {
+    char ch;
+    int count;
+
+    Node(char ch, int count) {
+      this.ch = ch;
+      this.count = count;
+    }
+  }
+
+  private class V2 {
+
+    public String removeDuplicates(String s, int k) {
+      Deque<Integer> counts = new ArrayDeque<>();
+      Deque<Character> letters = new ArrayDeque<>();
+
+      for (int i = 0; i < s.length(); i++) {
+        if (letters.isEmpty() || letters.peek() != s.charAt(i)) {
+          counts.push(1);
+        } else if (letters.peek() == s.charAt(i)) {
+          counts.push(counts.poll() + 1);
+        }
+
+        letters.push(s.charAt(i));
+
+        if (counts.peek() == k) {
+          counts.poll();
+          while (!letters.isEmpty() && letters.peek() == s.charAt(i)) {
+            letters.poll();
+          }
+        }
+      }
+
+      StringBuilder sb = new StringBuilder();
+      while (!letters.isEmpty()) {
+        sb.append(letters.poll());
+      }
+
+      return sb.reverse().toString();
+    }
+
+  }
+
 
 }
