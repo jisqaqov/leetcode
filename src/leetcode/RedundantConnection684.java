6f025ad6aff4b1b4b8816adb0a63cafc06e9113a
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-05-18 16:41:48 +0300

    union find: 684. Redundant Connection;

--- src/leetcode/RedundantConnection684.java
+++ src/leetcode/RedundantConnection684.java
@@ -1,20 +1,15 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import utils.TestUtils;
 
 /**
  * @author Jandos Iskakov
  * problem: 684. Redundant Connection
  * algorithm: Graph
- * time complexity: O(Elog(E) + V + E)
- * space complexity: O(Elog(E) + V + E)
- * Runtime: 5 ms, faster than 21.98% of Java online submissions
- * Memory Usage: 40 MB, less than 63.64% of Java online submissions
+ * time complexity: O(Nlog(N))
+ * space complexity: O(N)
+ * Runtime: 0 ms, faster than 100.00%
+ * Memory Usage: 39.4 MB, less than 72.73%
  */
 public class RedundantConnection684 {
 
@@ -29,95 +24,52 @@ public class RedundantConnection684 {
   }
 
   public int[] findRedundantConnection(int[][] edges) {
-    int n = edges.length;
-
-    List<Integer>[] adjList = new List[n + 1];
-    for (int i = 0; i <= n; i++) {
-      adjList[i] = new ArrayList();
-    }
+    DisjointSet ds = new DisjointSet(edges.length);
 
     for (int[] edge : edges) {
-      adjList[edge[0]].add(edge[1]);
-      adjList[edge[1]].add(edge[0]);
-    }
+      int u = edge[0] - 1;
+      int v = edge[1] - 1;
 
-    Set<Integer> cycles = new HashSet<>();
-
-    int[] time = new int[n + 1];
-    int[] vertex = new int[2];
-
-    boolean[] visited = new boolean[n + 1];
-
-    findCycle(adjList, visited, 1, -1, cycles, time, vertex);
-
-    int minTime = time[vertex[0]];
-    int maxTime = time[vertex[1]];
-
-    for (int v : new ArrayList<>(cycles)) {
-      if (time[v] < minTime || time[v] > maxTime) {
-        cycles.remove(v);
+      if (!ds.union(u, v)) {
+        return edge;
       }
     }
 
-    List<Integer> temp = new ArrayList<>(cycles);
-    temp.sort(Comparator.comparingInt(a -> time[a]));
-
-    boolean[][] candidates = new boolean[n + 1][n + 1];
-
-    for (int i = 0; i < temp.size() - 1; i++) {
-      int u = Math.min(temp.get(i), temp.get(i + 1));
-      int v = Math.max(temp.get(i), temp.get(i + 1));
-
-      candidates[u][v] = true;
-    }
-
-    int u = Math.min(vertex[0], vertex[1]);
-    int v = Math.max(vertex[0], vertex[1]);
-
-    candidates[u][v] = true;
-
-    int output = -1;
-    for (int i = 0; i < edges.length; i++) {
-      int[] edge = edges[i];
-
-      if (candidates[edge[0]][edge[1]]) {
-        output = Math.max(output, i);
-      }
-    }
-
-    return edges[output];
+    return edges[0];
   }
 
-  private boolean findCycle(List<Integer>[] adjList, boolean[] visited, int src, int prev,
-    Set<Integer> cycles, int[] time, int[] vertex) {
+  private class DisjointSet {
+    private int[] ds;
 
-    visited[src] = true;
-
-    cycles.add(src);
-
-    time[0]++;
-    time[src] = time[0];
-
-    for (int adj : adjList[src]) {
-      if (adj == prev) {
-        continue;
-      }
-
-      if (visited[adj]) {
-        vertex[0] = adj;
-        vertex[1] = src;
-
-        return true;
-      }
-
-      if (findCycle(adjList, visited, adj, src, cycles, time, vertex)) {
-        return true;
+    DisjointSet(int n) {
+      ds = new int[n];
+      for (int i = 0; i < n; i++) {
+        ds[i] = i;
       }
     }
 
-    cycles.remove(src);
+    int root(int x) {
+      while (ds[x] != x) {
+        ds[x] = ds[ds[x]];
+        x = ds[x];
+      }
 
-    return false;
+      return ds[x];
+    }
+
+    boolean union(int x, int y) {
+      int rootX = root(x);
+      int rootY = root(y);
+
+      if (rootX != rootY) {
+        ds[rootX] = rootY;
+
+        return true;
+      }
+
+      return false;
+    }
+
   }
 
 }
