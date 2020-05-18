e925e77a654bd528c79f8517fd84aae0ca730db4
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-05-18 19:08:17 +0300

    heap: resolved 373. Find K Pairs with Smallest Sums;

--- src/leetcode/FindKPairsWithSmallestSums373.java
+++ src/leetcode/FindKPairsWithSmallestSums373.java
@@ -9,11 +9,11 @@ import java.util.PriorityQueue;
 /**
  * @author Jandos Iskakov
  * problem: 373. Find K Pairs with Smallest Sums
- * algorithm: Graph, Topological Sort
+ * algorithm: Heap
  * time complexity: O(klog(k))
  * space complexity: O(k)
- * Runtime: 4 ms, faster than 79.90% of Java online submissions
- * Memory Usage: 41.5 MB, less than 48.15% of Java online submissions
+ * Runtime: 4 ms, faster than 77.46%
+ * Memory Usage: 40.5 MB, less than 66.67%
  */
 public class FindKPairsWithSmallestSums373 {
 
@@ -28,36 +28,26 @@ public class FindKPairsWithSmallestSums373 {
   }
 
   public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-    int n = nums1.length;
-    int m = nums2.length;
-
-    if (n == 0 || m == 0) {
+    if (nums1.length == 0 || nums2.length == 0) {
       return Collections.emptyList();
     }
 
     List<List<Integer>> output = new ArrayList<>();
 
     PriorityQueue<int[]> pq = new PriorityQueue<>(
-      (p1, p2) -> nums1[p1[0]] + nums2[p1[1]] - (nums1[p2[0]] + nums2[p2[1]]));
-    pq.add(new int[]{0, 0});
+      (p1, p2) -> Integer.compare(p1[2], p2[2]));
 
-    boolean[][] used = new boolean[n][m];
+    for (int i = 0; i < Math.min(nums1.length, k); i++) {
+      pq.add(new int[] {i, 0, nums1[i] + nums2[0]});
+    }
 
     for (; k > 0 && !pq.isEmpty(); k--) {
-      int[] idx = pq.poll();
+      int[] vals = pq.poll();
 
-      output.add(Arrays.asList(nums1[idx[0]], nums2[idx[1]]));
+      output.add(Arrays.asList(nums1[vals[0]], nums2[vals[1]]));
 
-      int[] idx1 = {idx[0] + 1, idx[1]};
-      if (idx1[0] < n && !used[idx1[0]][idx1[1]]) {
-        pq.add(idx1);
-        used[idx1[0]][idx1[1]] = true;
-      }
-
-      int[] idx2 = {idx[0], idx[1] + 1};
-      if (idx2[1] < m && !used[idx2[0]][idx2[1]]) {
-        pq.add(idx2);
-        used[idx2[0]][idx2[1]] = true;
+      if (vals[1] < nums2.length - 1) {
+        pq.add(new int[] {vals[0], vals[1] + 1, nums1[vals[0]] + nums2[vals[1] + 1]});
       }
     }
 
