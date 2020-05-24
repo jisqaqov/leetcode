2b3f54e5aa01d40f7b0c9382e7780acb8372fce9
Zhandos Iskakov <j.isqaqov@gmail.com>
2020-05-24 18:58:48 +0300

    recursion: resolved 95. Unique Binary Search Trees II;

--- src/leetcode/p0020/UniqueBinarySearchTreesII95.java
+++ src/leetcode/p0020/UniqueBinarySearchTreesII95.java
@@ -10,7 +10,7 @@ import java.util.Map;
 /**
  * @author Jandos Iskakov
  * problem: 95. Unique Binary Search Trees II
- * algorithm: DP
+ * algorithm: Recursion
  * time complexity:
  * space complexity:
  * Runtime: 1 ms, faster than 95.97% of Java online submissions
@@ -19,145 +19,32 @@ import java.util.Map;
 public class UniqueBinarySearchTreesII95 {
 
   public List<TreeNode> generateTrees(int n) {
-    Map<Integer, List<TreeNode>> dp = new HashMap<>();
-
-    dp.put(0, Collections.emptyList());
-    dp.put(1, Arrays.asList(new TreeNode(1)));
-
-    for (int i = 2; i <= n; i++) {
-      dp.put(i, generate(i, dp));
+    if (n == 0) {
+      return new ArrayList<>();
     }
 
-    return dp.get(n);
+    return generate(1, n);
   }
 
-  private List<TreeNode> generate(int n, Map<Integer, List<TreeNode>> dp) {
-    List<TreeNode> list = new ArrayList<>();
-
-    for (int k = 1; k <= n; k++) {
-      List<TreeNode> left = cloneTrees(dp.get(k - 1), 0);
-      List<TreeNode> right = cloneTrees(dp.get(n - k), k);
-
-      list.addAll(buildTrees(left, right, k));
+  private List<TreeNode> generate(int start, int end) {
+    if (start > end) {
+      return Collections.singletonList(null);
     }
 
-    return list;
-  }
-
-  private List<TreeNode> buildTrees(List<TreeNode> leftList, List<TreeNode> rightList, int k) {
     List<TreeNode> list = new ArrayList<>();
 
-    if (rightList.isEmpty()) {
+    for (int val = start; val <= end; val++) {
+      List<TreeNode> leftList = generate(start, val - 1);
+      List<TreeNode> rightList = generate(val + 1, end);
+
       for (TreeNode left : leftList) {
-        list.add(new TreeNode(k, left, null));
-      }
-    } else if (leftList.isEmpty()) {
-      for (TreeNode right : rightList) {
-        list.add(new TreeNode(k, null, right));
-      }
-    }
-
-    for (TreeNode left : leftList) {
-      for (TreeNode right : rightList) {
-        list.add(new TreeNode(k, left, right));
+        for (TreeNode right : rightList) {
+          list.add(new TreeNode(val, left, right));
+        }
       }
     }
 
     return list;
-  }
-
-  private List<TreeNode> cloneTrees(List<TreeNode> list, int inc) {
-    List<TreeNode> clones = new ArrayList<>();
-    for (TreeNode root : list) {
-      clones.add(cloneTree(root, inc));
-    }
-
-    return clones;
-  }
-
-  private TreeNode cloneTree(TreeNode root, int inc) {
-    if (root == null) {
-      return null;
-    }
-
-    TreeNode clone = new TreeNode(root.val + inc);
-    clone.left = cloneTree(root.left, inc);
-    clone.right = cloneTree(root.right, inc);
-
-    return clone;
-  }
-
-  private class TopDownVersion {
-
-    public List<TreeNode> generateTrees(int n) {
-      Map<Integer, List<TreeNode>> dp = new HashMap<>();
-
-      dp.put(0, Collections.emptyList());
-      dp.put(1, Arrays.asList(new TreeNode(1)));
-
-      return generate(n, dp);
-    }
-
-    private List<TreeNode> generate(int n, Map<Integer, List<TreeNode>> dp) {
-      if (dp.containsKey(n)) {
-        return dp.get(n);
-      }
-
-      List<TreeNode> list = new ArrayList<>();
-
-      for (int k = 1; k <= n; k++) {
-        List<TreeNode> left = cloneTrees(generate(k - 1, dp), 0);
-        List<TreeNode> right = cloneTrees(generate(n - k, dp), k);
-
-        list.addAll(buildTrees(left, right, k));
-      }
-
-      return list;
-    }
-
-    private List<TreeNode> buildTrees(List<TreeNode> leftList, List<TreeNode> rightList, int k) {
-      List<TreeNode> list = new ArrayList<>();
-
-      if (rightList.isEmpty()) {
-        for (TreeNode left : leftList) {
-          list.add(new TreeNode(k, left, null));
-        }
-      } else if (leftList.isEmpty()) {
-        for (TreeNode right : rightList) {
-          list.add(new TreeNode(k, null, right));
-        }
-      }
-
-      for (TreeNode left : leftList) {
-        for (TreeNode right : rightList) {
-          list.add(new TreeNode(k, left, right));
-        }
-      }
-
-      return list;
-    }
-
-    private List<TreeNode> cloneTrees(List<TreeNode> list, int inc) {
-      List<TreeNode> clones = new ArrayList<>();
-      for (TreeNode root : list) {
-        clones.add(cloneTree(root, inc));
-      }
-
-      return clones;
-    }
-
-    private TreeNode cloneTree(TreeNode root, int inc) {
-      if (root == null) {
-        return null;
-      }
-
-      TreeNode clone = new TreeNode(root.val + inc);
-      clone.left = cloneTree(root.left, inc);
-      clone.right = cloneTree(root.right, inc);
-
-      return clone;
-    }
-
   }
 
   public class TreeNode {
